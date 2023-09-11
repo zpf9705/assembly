d65a0d3113197aff274874259ba0d2c1ee14d04b
zhangpengfei <929160069@qq.com>
2023-09-11 17:48:05 +0800

    ---init

--- src/main/java/top/osjf/assembly/sdk/client/HttpClient.java
+++ src/main/java/top/osjf/assembly/sdk/client/HttpClient.java
@@ -3,24 +3,19 @@ package top.osjf.assembly.sdk.client;
 import com.alibaba.fastjson.JSON;
 import copy.cn.hutool.v_5819.core.exceptions.ExceptionUtil;
 import copy.cn.hutool.v_5819.core.io.IoUtil;
-import copy.cn.hutool.v_5819.http.ContentType;
 import copy.cn.hutool.v_5819.logger.StaticLog;
-import org.springframework.util.CollectionUtils;
+import org.springframework.lang.NonNull;
 import org.springframework.util.StopWatch;
 import top.osjf.assembly.sdk.SdkException;
-import top.osjf.assembly.utils.HttpUtils;
+import top.osjf.assembly.sdk.SdkUtils;
+import top.osjf.assembly.sdk.process.DefaultResponse;
+import top.osjf.assembly.sdk.process.Request;
+import top.osjf.assembly.sdk.process.Response;
 
 import java.util.Map;
-import java.util.Objects;
 
 /**
- * The HTTP based request client supports four types of request methods: get, post, put, and del.
- * <p>
- * The parameter selection is a common and standardized form, and the default request body type is JSON.
- * <p>
- * Currently, it also supports {@code application/x-www-form-urlencoded} type request methods.
- * <p>
- * More specific support for request body types will be added when encountering them
+ * Implementation class of http mode for {@link Client}.
  *
  * @author zpf
  * @since 1.1.0
@@ -37,7 +32,7 @@ public class HttpClient<R extends Response> extends AbstractClient<R> {
 
     @Override
     public R request() {
-        Request<R> request = getCurrentParam();
+        Request<R> request = getCurrentRequest();
         StopWatch stopWatch = new StopWatch();
         //Request Timer
         stopWatch.start();
@@ -50,39 +45,34 @@ public class HttpClient<R extends Response> extends AbstractClient<R> {
             //Verification of necessary parameters
             request.validate();
             //Obtain request body map
-            Map<String, Object> paramsMap = request.toParamsMap();
+            Object requestParam = request.getRequestParam();
             //Get Request Header
             Map<String, String> headers = request.getHeadMap();
-            /*
-             * Here, different plans will be made for the request
-             * body string based on the type of response context set
-             */
-            body = getBodyStringWithContentType(request, headers);
             //requested action
-            responseStr = caseRequestMethodDoThat(request, headers, paramsMap, body);
+            responseStr = doRequest(request, headers, requestParam);
             /*
              * This step requires special conversion
              * requirements for response data Final shift to JSON data
              */
-            responseStr = this.convert(request, responseStr);
+            responseStr = this.preResponseStrHandler(request, responseStr);
             /*
              * This step is mainly to convert the encapsulated response class,
              * which is uniformly converted by json Type conversion
              * This requires that the interaction in the response must meet the
              * requirements of JSON return, or the innermost layer should be JSON data
              */
-            response = this.JsonToConvertResponse(request, responseStr);
+            response = this.convertToResponse(request, responseStr);
 
         } catch (SdkException e) {
             StaticLog.error("Client request fail, apiName={}, error=[{}]",
-                    request.matchApi().name(), ExceptionUtil.stacktraceToOneLineString(e));
+                    request.matchSdk().name(), ExceptionUtil.stacktraceToOneLineString(e));
             throwable = e;
             errorMsg = ExceptionUtil.stacktraceToOneLineString(throwable);
-            String jsonData = JSON.toJSONString(DefaultResponse.buildResponse(e.getCode(), e.getMsg()));
+            String jsonData = JSON.toJSONString(DefaultResponse.buildResponse(e.getMessage()));
             response = JSON.parseObject(jsonData, request.getResponseCls());
         } catch (Exception e) {
             StaticLog.error("Client request fail, apiName={}, error=[{}]",
-                    request.matchApi().name(), ExceptionUtil.stacktraceToOneLineString(e));
+                    request.matchSdk().name(), ExceptionUtil.stacktraceToOneLineString(e));
             throwable = e;
             errorMsg = ExceptionUtil.stacktraceToOneLineString(throwable);
             String jsonData = JSON.toJSONString(DefaultResponse.buildUnknownResponse(e.getMessage()));
@@ -94,11 +84,11 @@ public class HttpClient<R extends Response> extends AbstractClient<R> {
             //logger console
             if (throwable == null) {
                 String msgFormat = "Request end, name={}, request={}, response={}, time={}ms";
-                StaticLog.info(msgFormat, request.matchApi().name(), body, responseStr,
+                StaticLog.info(msgFormat, request.matchSdk().name(), body, responseStr,
                         totalTimeMillis);
             } else {
                 String msgFormat = "Request fail, name={}, request={}, response={}, error={}, time={}ms";
-                StaticLog.info(msgFormat, request.matchApi().name(), body, responseStr,
+                StaticLog.info(msgFormat, request.matchSdk().name(), body, responseStr,
                         errorMsg, totalTimeMillis);
             }
         }
@@ -109,65 +99,16 @@ public class HttpClient<R extends Response> extends AbstractClient<R> {
     }
 
     /**
-     * Make different plans for the request body based on different context types
+     * HTTP requests based on different methods.
      *
-     * @param request Request parameter encapsulation
-     * @param headers Header information
-     * @return body value
-     */
-    private String getBodyStringWithContentType(Request<R> request, Map<String, String> headers) {
-        String body;
-        String contentType = headers.get("Content-Type");
-        //application/json
-        if (CollectionUtils.isEmpty(headers) || Objects.equals(contentType, ContentType.JSON.getValue())) {
-            body = JSON.toJSONString(request.toParamsMap());
-            //application/x-www-form-urlencoded
-        } else if (Objects.equals(contentType, ContentType.FORM_URLENCODED.getValue())) {
-            body = (String) request.getBody();
-        } else {
-            //default to json
-            body = JSON.toJSONString(request.toParamsMap());
-
-            //other any content-type if meet need to add
-        }
-        return body;
-    }
-
-    /**
-     * HTTP requests based on different methods
-     *
-     * @param request   Request parameter encapsulation
-     * @param headers   Header information
-     * @param paramsMap Parameter map
-     * @param body      Certified body
+     * @param request      The actual request ,must not be {@literal null}.
+     * @param headers      Header information map.
+     * @param requestParam Request parameters.
      * @return return value
      */
-    public String caseRequestMethodDoThat(Request<R> request,
-                                          Map<String, String> headers,
-                                          Map<String, Object> paramsMap,
-                                          String body) {
-        String responseStr;
-        //get request url
-        String url = getUrl();
-        //with request method switch to request any
-        switch (request.matchApi().getRequestMethod()) {
-            case GET:
-                responseStr = HttpUtils.get(url, headers, paramsMap);
-                break;
-            case POST:
-                responseStr = HttpUtils.post(url, headers, body);
-                break;
-            case PUT:
-                responseStr = HttpUtils.put(url, headers, body);
-                break;
-            case DELETE:
-                responseStr = HttpUtils.delete(url, headers);
-                break;
-            default:
-                //default to empty map string
-                responseStr = "{}";
-                break;
-        }
-        return responseStr;
+    public String doRequest(@NonNull Request<R> request, Map<String, String> headers, Object requestParam) {
+        //Check if there is a context type, if not, it defaults to JSON
+        SdkUtils.checkContentType(headers);
+        return request.matchSdk().getRequestMethod().action(getUrl(), headers, requestParam);
     }
 }
