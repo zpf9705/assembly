917861a6b111c718b28c83106cf1b11ddcd7fa98
zhangpengfei <929160069@qq.com>
2023-09-11 17:07:33 +0800

    ---init

--- src/main/java/top/osjf/assembly/sdk/SdkException.java
+++ src/main/java/top/osjf/assembly/sdk/SdkException.java
@@ -2,39 +2,28 @@ package top.osjf.assembly.sdk;
 
 
 /**
- * Sdk uniformly throws exceptions, including status and status information, as shown in {@link SdkAssert}
+ * The exception thrown by the body parameter verification of SDK is usually the judgment
+ * of business logic parameters, which is thrown during the request.
  *
+ * @see top.osjf.assembly.sdk.process.Validated
  * @author zpf
  * @since 1.1.0
  */
 public class SdkException extends IllegalArgumentException {
 
-    public static final String format
-            = "{\"note\":\"Custom exception\",\"code\":\"%s\",\"msg\":\"%s\"}";
-
-    private static final long serialVersionUID = 5913991753884029933L;
-
-    private final Integer code;
-
-    private final String msg;
-
-    /**
-     * Construction method with code and message
-     *
-     * @param code Request Status
-     * @param msg  Request Result Information
-     */
-    public SdkException(Integer code, String msg) {
-        super(String.format(format, code, msg));
-        this.code = code;
-        this.msg = msg;
+    public SdkException() {
+        super();
     }
 
-    public Integer getCode() {
-        return this.code;
+    public SdkException(String s) {
+        super(s);
     }
 
-    public String getMsg() {
-        return this.msg;
+    public SdkException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SdkException(Throwable cause) {
+        super(cause);
     }
 }
