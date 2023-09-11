1c904616511c841c4b0036046af53183f5e8e88c
zhangpengfei <929160069@qq.com>
2023-09-11 17:12:26 +0800

    ---init

---
+++ src/main/java/top/osjf/assembly/sdk/process/RequestParamCapable.java
@@ -0,0 +1,16 @@
+package top.osjf.assembly.sdk.process;
+
+
+/**
+ * Request parameter acquisition interface, uniformly return {@link Object}, compatible with all return values.
+ *
+ * @author zpf
+ * @since 1.1.0
+ */
+public interface RequestParamCapable {
+
+    /**
+     * @return Return any of {@link Object}.
+     */
+    Object getRequestParam();
+}
