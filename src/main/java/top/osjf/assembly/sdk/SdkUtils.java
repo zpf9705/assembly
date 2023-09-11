3100a17cc498130bacd08f3aa51dc5ec14e7d391
zhangpengfei <929160069@qq.com>
2023-09-11 17:51:28 +0800

    ---init

--- src/main/java/top/osjf/assembly/sdk/SdkUtils.java
+++ src/main/java/top/osjf/assembly/sdk/SdkUtils.java
@@ -1,46 +1,19 @@
 package top.osjf.assembly.sdk;
 
-import copy.cn.hutool.v_5819.core.bean.BeanUtil;
-import copy.cn.hutool.v_5819.core.bean.copier.CopyOptions;
 import copy.cn.hutool.v_5819.core.collection.CollectionUtil;
-import copy.cn.hutool.v_5819.core.util.ArrayUtil;
-import copy.cn.hutool.v_5819.core.util.ReflectUtil;
 import copy.cn.hutool.v_5819.http.ContentType;
-import top.osjf.assembly.sdk.annotation.MapFiled;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 /**
+ * About sdk helper tool class.
+ *
  * @author zpf
  * @since 1.1.0
  */
 public abstract class SdkUtils {
 
     static final String named = "Content-Type";
-
-    //MapFiled
-    public static Map<String, Object> parseObjectToMap(Object obj) {
-        if (obj == null) {
-            return Collections.emptyMap();
-        }
-        //get all fields include self and parent private and public
-        Field[] allFields = ReflectUtil.getFields(obj.getClass());
-        if (ArrayUtil.isEmpty(allFields)) {
-            return BeanUtil.beanToMap(obj);
-        } else {
-            Map<String, String> fieldMapping = Arrays.stream(allFields)
-                    //at MapFiled
-                    .filter(f -> Objects.nonNull(f.getAnnotation(MapFiled.class)) &&
-                            //no static
-                            !Modifier.isStatic(f.getModifiers()))
-                    .collect(Collectors.toMap(Field::getName,
-                            field -> field.getAnnotation(MapFiled.class).name()));
-            return BeanUtil.beanToMap(obj, new LinkedHashMap<>(), CopyOptions.create().setFieldMapping(fieldMapping));
-        }
-    }
 
     public static void checkContentType(Map<String, String> headers) {
         if (CollectionUtil.isEmpty(headers)) {
