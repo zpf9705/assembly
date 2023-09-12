572ea94ff50f59b96fea4d0cf46a5e01e2a765e2
zhangpengfei <929160069@qq.com>
2023-09-12 16:03:35 +0800

    ---init

--- src/main/java/top/osjf/assembly/sdk/process/AbstractRequestParams.java
+++ src/main/java/top/osjf/assembly/sdk/process/AbstractRequestParams.java
@@ -1,6 +1,8 @@
 package top.osjf.assembly.sdk.process;
 
 
+import com.alibaba.fastjson.JSON;
+import org.springframework.lang.Nullable;
 import top.osjf.assembly.sdk.SdkException;
 import top.osjf.assembly.sdk.client.Client;
 import top.osjf.assembly.sdk.client.HttpClient;
@@ -58,8 +60,40 @@ public abstract class AbstractRequestParams<R extends AbstractResponse> implemen
     }
 
     @Override
+    public Object getRequestParam() {
+        Object param = getParam();
+        if (param == null) {
+            return null;
+        }
+        if (defaultToJson()) {
+            param = JSON.toJSONString(param);
+        }
+        return param;
+    }
+
+    @Override
     @SuppressWarnings("rawtypes")
     public Class<? extends Client> getClientCls() {
         return HttpClient.class;
+    }
+
+    /**
+     * The abstract method for obtaining the actual request body. If {@link #defaultToJson()} requires
+     * JSON serialization, it will be done in {@link #getRequestParam()}. If it is not a JSON request
+     * parameter, this method needs to be rewritten to directly convert the input parameter format and
+     * set {@link #defaultToJson()} to {@literal true}.
+     *
+     * @return Returns an input parameter object, which may have multiple forms of existence or may be {@literal null}.
+     */
+    @Nullable
+    public abstract Object getParam();
+
+    /**
+     * Mark whether to use input parameters as JSON serial numbers.
+     *
+     * @return True is required, while false is not required.
+     */
+    public boolean defaultToJson() {
+        return true;
     }
 }
