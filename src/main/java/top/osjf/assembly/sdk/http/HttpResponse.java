c4dc583b3ba63d3130e6a8bf9514b6fac901e780
zhangpengfei <929160069@qq.com>
2023-09-13 17:30:36 +0800

    --- update to best

---
+++ src/main/java/top/osjf/assembly/sdk/http/HttpResponse.java
@@ -0,0 +1,13 @@
+package top.osjf.assembly.sdk.http;
+
+import top.osjf.assembly.sdk.process.Response;
+
+/**
+ * Occupy in HTTP mode, inherit {@link Response}.
+ * <p>There are currently no extension methods available.</p>
+ *
+ * @author zpf
+ * @since 1.1.1
+ */
+public interface HttpResponse extends Response {
+}
