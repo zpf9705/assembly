a8ee04f0b6b1b8688fe77f30c1108ed0ba4581de
zhangpengfei <929160069@qq.com>
2023-09-13 17:45:19 +0800

    --- update to best

--- src/main/java/top/osjf/assembly/sdk/process/AbstractRequestParams.java
+++ src/main/java/top/osjf/assembly/sdk/process/AbstractRequestParams.java
@@ -1,49 +1,14 @@
 package top.osjf.assembly.sdk.process;
 
 
-import com.alibaba.fastjson.JSON;
-import org.springframework.lang.Nullable;
 import top.osjf.assembly.sdk.SdkException;
-import top.osjf.assembly.sdk.client.Client;
-import top.osjf.assembly.sdk.client.HttpClient;
 
 import java.util.Collections;
 import java.util.Map;
 
 /**
- * Request abstract node class, used to define the public parameters or methods of the real request parameter class.
- * <p>
- * You can check the example code:
- * <pre>
- * {@code
- * public class TestP extends AbstractRequestParams<TestR> {
- *
- *     public SdkEnum matchSdk() {
- *         return new SdkEnum() {
- *
- *             public String getUlr(String host) {
- *                 return "";
- *             }
- *
- *             public RequestMethod getRequestMethod() {
- *                 return RequestMethod.POST;
- *             }
- *
- *             public String name() {
- *                 return "null";
- *             }
- *         };
- *     }
- *
- *     public Object getRequestParam() {
- *         return "";
- *     }
- *
- *     public Class<TestR> getResponseCls() {
- *         return TestR.class;
- *     }
- * }}
- * </pre>
+ * <p>The abstract implementation of {@link Request} mainly focuses on default implementation of some
+ * rules and methods of {@link Request}.</p>
  *
  * @author zpf
  * @since 1.1.0
@@ -57,43 +22,5 @@ public abstract class AbstractRequestParams<R extends AbstractResponse> implemen
 
     @Override
     public void validate() throws SdkException {
-    }
-
-    @Override
-    public Object getRequestParam() {
-        Object param = getParam();
-        if (param == null) {
-            return null;
-        }
-        if (defaultToJson()) {
-            param = JSON.toJSONString(param);
-        }
-        return param;
-    }
-
-    @Override
-    @SuppressWarnings("rawtypes")
-    public Class<? extends Client> getClientCls() {
-        return HttpClient.class;
-    }
-
-    /**
-     * The abstract method for obtaining the actual request body. If {@link #defaultToJson()} requires
-     * JSON serialization, it will be done in {@link #getRequestParam()}. If it is not a JSON request
-     * parameter, this method needs to be rewritten to directly convert the input parameter format and
-     * set {@link #defaultToJson()} to {@literal true}.
-     *
-     * @return Returns an input parameter object, which may have multiple forms of existence or may be {@literal null}.
-     */
-    @Nullable
-    public abstract Object getParam();
-
-    /**
-     * Mark whether to use input parameters as JSON serial numbers.
-     *
-     * @return True is required, while false is not required.
-     */
-    public boolean defaultToJson() {
-        return true;
     }
 }
