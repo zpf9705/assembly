cd12e167fa42ab407f7633e2e0a81b7880bb31ca
zhangpengfei <929160069@qq.com>
2023-09-13 18:03:13 +0800

    --- update to best

--- src/main/java/top/osjf/assembly/sdk/process/AbstractResponse.java
+++ src/main/java/top/osjf/assembly/sdk/process/AbstractResponse.java
@@ -1,42 +1,19 @@
 package top.osjf.assembly.sdk.process;
 
 
-import org.apache.http.HttpStatus;
-
 /**
- * Response abstract node class, used to define common states, unknown error messages, success plans, etc.
- * <p>
- * You can check the example code:
- * <pre>
- * {@code
- * public class TestR extends AbstractResponse {
- *
- *     private Boolean success;
- *
- *     private Integer code;
- *
- *     private String message;
- *
- *     private Object errors;
- *
- *     private List<Supplier> data;
- * }}
- * </pre>
+ * <p>The abstract implementation of {@link Response} mainly focuses on default implementation of some
+ * rules and methods of {@link Response}.</p>
+ * <p>The default implementation is to convert the format when {@link DefaultResponse} users encounter exceptions.</p>
  *
  * @author zpf
  * @since 1.1.0
  */
 public abstract class AbstractResponse implements Response {
 
-    private static final long serialVersionUID = 6922151145018976148L;
-
-    public static final Integer DATA_ERROR_CODE = 600558;
-
-    private static final Integer UNKNOWN_ERROR_CODE = 500358;
-
     private static final boolean DEFAULT_IS_SUCCESS = false;
 
-    private static final String DEFAULT_MESSAGE = "Please inherited [AbstractResponse]";
+    private static final String DEFAULT_MESSAGE = "UNKNOWN";
 
     @Override
     public boolean isSuccess() {
@@ -45,20 +22,6 @@ public abstract class AbstractResponse implements Response {
 
     @Override
     public String getMessage() {
-        return buildUnknownResponse(DEFAULT_MESSAGE).getMessage();
-    }
-
-    public static DefaultResponse buildResponse(String message) {
-        return new DefaultResponse(HttpStatus.SC_BAD_REQUEST, message);
-    }
-
-    public static DefaultResponse buildUnknownResponse(String message) {
-        return new DefaultResponse(UNKNOWN_ERROR_CODE, String
-                .format("happen unknown exception,message=[%s]", message));
-    }
-
-    public static DefaultResponse buildDataErrorResponse(String message) {
-        return new DefaultResponse(DATA_ERROR_CODE, String
-                .format("happen data_error exception,message=[%s]", message));
+        return DEFAULT_MESSAGE;
     }
 }
