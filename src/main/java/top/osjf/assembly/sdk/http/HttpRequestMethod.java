5f5618593737f87a80c664b310b502ad7448c6c5
zhangpengfei <929160069@qq.com>
2023-09-19 11:38:02 +0800

    --- update to best

--- src/main/java/top/osjf/assembly/sdk/http/HttpRequestMethod.java
+++ src/main/java/top/osjf/assembly/sdk/http/HttpRequestMethod.java
@@ -1,7 +1,7 @@
 package top.osjf.assembly.sdk.http;
 
 import org.springframework.lang.NonNull;
-import top.osjf.assembly.utils.HttpUtils;
+import top.osjf.assembly.utils.ApacheHttpUtils;
 
 import java.util.Map;
 
@@ -11,34 +11,34 @@ import java.util.Map;
  * <p>Implement the {@link HttpSdkEnum.Action} interface to define the action methods for each request method.</p>
  *
  * @author zpf
- * @see HttpUtils
+ * @see ApacheHttpUtils
  * @since 1.1.1
  */
 public enum HttpRequestMethod implements HttpSdkEnum.Action {
 
     GET {
         @Override
-        public String apply(@NonNull String url, Map<String, String> headers, Object requestParam, Boolean montage)
-                throws Exception {
-            return HttpUtils.get(url, headers, requestParam, montage);
+        public String doRequest(@NonNull Type type, @NonNull String url, Map<String, String> headers, Object requestParam,
+                                Boolean montage) throws Exception {
+            return type.getInstance().get(url, headers, requestParam, montage);
         }
     }, POST {
         @Override
-        public String apply(@NonNull String url, Map<String, String> headers, Object requestParam, Boolean montage)
-                throws Exception {
-            return HttpUtils.post(url, headers, requestParam, montage);
+        public String doRequest(@NonNull Type type, @NonNull String url, Map<String, String> headers, Object requestParam,
+                                Boolean montage) throws Exception {
+            return type.getInstance().post(url, headers, requestParam, montage);
         }
     }, PUT {
         @Override
-        public String apply(@NonNull String url, Map<String, String> headers, Object requestParam, Boolean montage)
-                throws Exception {
-            return HttpUtils.put(url, headers, requestParam, montage);
+        public String doRequest(@NonNull Type type, @NonNull String url, Map<String, String> headers, Object requestParam,
+                                Boolean montage) throws Exception {
+            return type.getInstance().put(url, headers, requestParam, montage);
         }
     }, DELETE {
         @Override
-        public String apply(@NonNull String url, Map<String, String> headers, Object requestParam, Boolean montage)
-                throws Exception {
-            return HttpUtils.delete(url, headers, requestParam, montage);
+        public String doRequest(@NonNull Type type, @NonNull String url, Map<String, String> headers, Object requestParam,
+                                Boolean montage) throws Exception {
+            return type.getInstance().delete(url, headers, requestParam, montage);
         }
     }
 }
