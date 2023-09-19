0c6aeb7f9c086df48b5b3327553f7300e622f7fc
zhangpengfei <929160069@qq.com>
2023-09-19 11:34:14 +0800

    --- update to best

---
+++ src/main/java/top/osjf/assembly/sdk/http/ApacheHttpClient.java
@@ -0,0 +1,28 @@
+package top.osjf.assembly.sdk.http;
+
+import java.util.Map;
+
+/**
+ * HTTP tool request client class based on Apache Open Source Foundation.
+ * <p>Please refer to {@link CommonsHttpClient} for the specific request process.</p>
+ *
+ * @author zpf
+ * @since 1.1.0
+ */
+@SuppressWarnings("serial")
+@HttpClient(type = Type.APACHE_HTTP)
+public class ApacheHttpClient<R extends HttpResponse> extends CommonsHttpClient<R> {
+
+    /* ******* super Constructs ***********/
+
+    public ApacheHttpClient(String url) {
+        super(url);
+    }
+
+    @Override
+    public String doRequest(HttpRequestMethod method, Map<String, String> headers, Object requestParam,
+                            Boolean montage) throws Exception {
+        super.doRequest(method, headers, requestParam, montage);
+        return method.doRequest(getUrl(), headers, requestParam, montage);
+    }
+}
