e65a0a02b342515880fc3c47240adc7228d39ae0
zhangpengfei <929160069@qq.com>
2023-09-19 16:10:47 +0800

    --- update to best

--- src/main/java/top/osjf/assembly/sdk/http/OkHttpMethod.java
+++ src/main/java/top/osjf/assembly/sdk/http/OkHttpMethod.java
@@ -1,11 +1,9 @@
 package top.osjf.assembly.sdk.http;
 
-import top.osjf.assembly.utils.OkHttpUtils;
-
 import java.util.Map;
 
 /**
- * One of the implementation classes of {@link HttpMethod}, please refer to {@link OkHttpUtils} for implementation.
+ * One of the implementation classes of {@link HttpMethod}, please refer to {@link OkHttpSimpleRequestUtils} for implementation.
  *
  * @author zpf
  * @since 1.1.1
@@ -19,21 +17,21 @@ public class OkHttpMethod implements HttpMethod {
 
     @Override
     public String get(String url, Map<String, String> headers, Object requestParam, boolean montage) throws Exception {
-        return OkHttpUtils.get(url, headers, requestParam, montage);
+        return OkHttpSimpleRequestUtils.get(url, headers, requestParam, montage);
     }
 
     @Override
     public String post(String url, Map<String, String> headers, Object requestParam, boolean montage) throws Exception {
-        return OkHttpUtils.post(url, headers, requestParam, montage);
+        return OkHttpSimpleRequestUtils.post(url, headers, requestParam, montage);
     }
 
     @Override
     public String put(String url, Map<String, String> headers, Object requestParam, boolean montage) throws Exception {
-        return OkHttpUtils.put(url, headers, requestParam, montage);
+        return OkHttpSimpleRequestUtils.put(url, headers, requestParam, montage);
     }
 
     @Override
     public String delete(String url, Map<String, String> headers, Object requestParam, boolean montage) throws Exception {
-        return OkHttpUtils.delete(url, headers, requestParam, montage);
+        return OkHttpSimpleRequestUtils.delete(url, headers, requestParam, montage);
     }
 }
