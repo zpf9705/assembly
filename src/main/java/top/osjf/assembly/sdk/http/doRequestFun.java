15d6c95a7d79543f9d267e5ce10aa39095bb48d4
zhangpengfei <929160069@qq.com>
2023-09-19 11:36:46 +0800

    --- update to best

---
+++ src/main/java/top/osjf/assembly/sdk/http/doRequestFun.java
@@ -0,0 +1,29 @@
+
+package top.osjf.assembly.sdk.http;
+
+/**
+ * A functional interface (callback) that computes a value based on multiple input values.
+ *
+ * @param <T1> the first value type
+ * @param <T2> the second value type
+ * @param <T3> the third value type
+ * @param <T4> the fourth value type
+ * @param <R>  the result type
+ * @author zpf
+ * @since 1.1.1
+ */
+@FunctionalInterface
+public interface doRequestFun<T1, T2, T3, T4, R> {
+
+    /**
+     * Calculate a value based on the input values.
+     *
+     * @param t1 the first value.
+     * @param t2 the second value.
+     * @param t3 the third value.
+     * @param t4 the fourth value.
+     * @return the result value.
+     * @throws Exception if the implementation wishes to throw any type of exception.
+     */
+    R doRequest(T1 t1, T2 t2, T3 t3, T4 t4) throws Exception;
+}
