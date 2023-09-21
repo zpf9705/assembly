47ef31416f337950c9f2a41ac1cdb596bd62c0a4
zhangpengfei <929160069@qq.com>
2023-09-21 16:33:31 +0800

    --- update to many models

---
+++ assembly-cache/src/main/java/top/osjf/assembly/cache/persistence/CachePersistenceReductionSelector.java
@@ -0,0 +1,30 @@
+package top.osjf.assembly.cache.persistence;
+
+import org.springframework.util.Assert;
+import top.osjf.assembly.util.SpiLoads;
+
+/**
+ * Use {@link SpiLoads} to provide query services for the {@link CachePersistenceReduction} interface.
+ *
+ * @author zpf
+ * @since 1.0.0
+ */
+public final class CachePersistenceReductionSelector {
+
+    private CachePersistenceReductionSelector() {
+    }
+
+    public static CachePersistenceReduction getReductionByClass(Class<?> reductionClass) {
+        if (reductionClass == null) {
+            return null;
+        }
+        return getReductionByClass0(reductionClass);
+    }
+
+    private static CachePersistenceReduction getReductionByClass0(Class<?> reductionClass) {
+        CachePersistenceReduction reduction = SpiLoads.findSpi(CachePersistenceReduction.class)
+                .getSpecifiedServiceBySubClassName(reductionClass.getName());
+        Assert.notNull(reduction, "No found named [" + reductionClass.getName() + "] in serviceLoad");
+        return reduction;
+    }
+}
