16c6abcd946cf83ae496f6a631c7bdb838957642
zhangpengfei <929160069@qq.com>
2023-09-21 13:27:16 +0800

    --- update to many models

---
+++ assembly-cache/src/main/java/top/osjf/assembly/cache/factory/CacheFactory.java
@@ -0,0 +1,22 @@
+package top.osjf.assembly.cache.factory;
+
+import top.osjf.assembly.util.annotation.NotNull;
+
+/**
+ * Thread safe cache executor acquisition interface.
+ * <p>
+ * In order to ensure the cache distribution problem caused by multiple generic initialization
+ * {@link top.osjf.assembly.cache.operations.CacheTemplate}, the concept of a factory is specifically
+ * introduced here to ensure the only way to set the cache.
+ * <p>
+ * Obtain a unique {@link CacheExecutor} on this interface, ensuring that multiple operations call
+ * the same environment cache.
+ *
+ * @author zpf
+ * @since 1.0.0
+ */
+public interface CacheFactory {
+
+    @NotNull
+    CacheExecutor executor();
+}
