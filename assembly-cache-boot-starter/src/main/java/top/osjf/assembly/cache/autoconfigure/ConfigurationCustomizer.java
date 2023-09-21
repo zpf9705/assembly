6d8b2c65ae471ded13a3592501587bee9e3c533e
zhangpengfei <929160069@qq.com>
2023-09-21 10:05:44 +0800

    --- update to many models

--- assembly-cache-boot-starter/src/main/java/top/osjf/assembly/cache/autoconfigure/ConfigurationCustomizer.java
+++ assembly-cache-boot-starter/src/main/java/top/osjf/assembly/cache/autoconfigure/ConfigurationCustomizer.java
@@ -1,22 +1,17 @@
 package top.osjf.assembly.cache.autoconfigure;
 
 /**
- * Multiple configurations of the facial interface are added to the interface for {@link ExpireProperties}.
- * <p>
- * Multiple items related to this interface can be configured in combination with the
- * class marked in {@link org.springframework.context.annotation.Bean}, obtained through
- * the {@link org.springframework.context.annotation.Configuration} method function,
- * and finally regressed to the main configuration {@link ExpireProperties}.
+ * Callback interface that can be customized a {@link AssemblyCacheProperties} object generated on auto-configuration.
  *
  * @author zpf
- * @since 1.1.0
+ * @since 1.0.0
  */
 public interface ConfigurationCustomizer {
 
     /**
-     * Setting for {@link ExpireProperties} Configuration
+     * Customize the given a {@link AssemblyCacheProperties} object.
      *
-     * @param properties {@link ExpireProperties}
+     * @param properties the properties object to customize.
      */
-    void customize(ExpireProperties properties);
+    void customize(AssemblyCacheProperties properties);
 }
