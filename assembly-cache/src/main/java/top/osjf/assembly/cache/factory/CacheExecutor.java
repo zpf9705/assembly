b1fc1794d7418209f5547c78e528f75c431deddb
zhangpengfei <929160069@qq.com>
2023-09-21 14:06:07 +0800

    --- update to many models

---
+++ assembly-cache/src/main/java/top/osjf/assembly/cache/factory/CacheExecutor.java
@@ -0,0 +1,28 @@
+package top.osjf.assembly.cache.factory;
+
+import top.osjf.assembly.cache.command.CacheCommands;
+import top.osjf.assembly.cache.command.CacheKeyCommands;
+import top.osjf.assembly.cache.command.CachePairCommands;
+
+/**
+ * As the name suggests, it is a cache executor interface that provides the implementation of cache methods.
+ * <p>
+ * It inherits the relevant instruction methods defined by the cache, and publicizes it with the data
+ * type in the form of a unified {@link Byte} array of data, commonly known as serialization. <br>
+ * When taking out the data, the {@link Byte} array is deserialized into real objects,
+ * which are the values of key and value.
+ * <p>{@link top.osjf.assembly.util.SerialUtils}</p>
+ *
+ * @author zpf
+ * @since 1.0.0
+ */
+public interface CacheExecutor extends CacheCommands {
+
+    default CachePairCommands pairCommands() {
+        return this;
+    }
+
+    default CacheKeyCommands keyCommands() {
+        return this;
+    }
+}
