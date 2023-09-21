6f7a68a3a13ad2a4b4fb3eaad99fb389fb93b90e
zhangpengfei <929160069@qq.com>
2023-09-21 16:22:43 +0800

    --- update to many models

---
+++ assembly-cache/src/main/java/top/osjf/assembly/cache/persistence/ByteCachePersistence.java
@@ -0,0 +1,117 @@
+package top.osjf.assembly.cache.persistence;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
+import org.springframework.util.Assert;
+import top.osjf.assembly.cache.exceptions.CachePersistenceException;
+import top.osjf.assembly.util.annotation.NotNull;
+
+import java.util.List;
+
+/**
+ * The only Special extend for {@link AbstractCachePersistence} with generic {@code byte[]}
+ *
+ * @author zpf
+ * @since 1.0.0
+ */
+public class ByteCachePersistence extends AbstractCachePersistence<byte[], byte[]> {
+
+    private static final long serialVersionUID = 5518995337588214891L;
+
+    public ByteCachePersistence() {
+        super();
+    }
+
+    public ByteCachePersistence(BytePersistence persistence, String writePath) {
+        super(persistence, writePath);
+    }
+
+    /**
+     * Set in cache map {@code ByteCachePersistence} with {@code Entry<byte[], byte[]>} and {@code FactoryBeanName}.<br>
+     * Whether you need to query in the cache.
+     *
+     * @param entry must not be {@literal null}
+     * @return {@link ByteCachePersistence}
+     */
+    public static ByteCachePersistence ofSetBytes(@NotNull Entry<byte[], byte[]> entry) {
+        return ofSet(ByteCachePersistence.class, BytePersistence.class, entry);
+    }
+
+    /**
+     * Set in cache map {@code ByteCachePersistence} with {@code Persistence<byte[],byte[]>}.<br>
+     * Don't query is in the cache.
+     *
+     * @param persistence must not be {@literal null}
+     * @return {@link ByteCachePersistence}
+     */
+    public static ByteCachePersistence ofSetPersistenceBytes(@NotNull BytePersistence persistence) {
+        return ofSetPersistence(ByteCachePersistence.class, persistence);
+    }
+
+    /**
+     * Get an {@code ByteCachePersistence} with {@code key} and {@code value} in cache map.
+     *
+     * @param key   must not be {@literal null}.
+     * @param value must not be {@literal null}.
+     * @return {@link ByteCachePersistence}
+     */
+    public static ByteCachePersistence ofGetBytes(byte[] key, byte[] value) {
+        return ofGet(key, value, ByteCachePersistence.class);
+    }
+
+    /**
+     * Get an {@code ByteCachePersistence} with {@code key}  in cache map.
+     *
+     * @param key must not be {@literal null}.
+     * @return {@link ByteCachePersistence}.
+     */
+    public static ByteCachePersistence ofGetBytes(byte[] key) {
+        return ofGet(key);
+    }
+
+    /**
+     * Get any {@code ByteCachePersistence} with similar {@code key}  in cache map.
+     *
+     * @param key must not be {@literal null}.
+     * @return {@link ByteCachePersistence}.
+     */
+    public static List<ByteCachePersistence> ofGetSimilarBytes(byte[] key) {
+        return ofGetSimilar(key);
+    }
+
+    @Override
+    public String getReduction() {
+        return ByteCachePersistence.class.getName();
+    }
+
+    @Override
+    public void reductionUseString(@NotNull StringBuilder buffer) {
+        super.reductionUseString(buffer);
+        //parse json
+        BytePersistence persistence;
+        try {
+            persistence = JSONObject.parseObject(buffer.toString(), new TypeReference<BytePersistence>() {
+            });
+        } catch (Exception e) {
+            throw new CachePersistenceException("Buffer data [" + buffer + " ] parse Persistence error " +
+                    "[" + e.getMessage() + "]");
+        }
+        //No cache in the cache
+        ByteCachePersistence globePersistence = ofSetPersistence(this.getClass(), persistence);
+        Assert.notNull(globePersistence, "GlobePersistence no be null");
+        this.deserializeWithEntry(globePersistence);
+    }
+
+    public static class BytePersistence extends Persistence<byte[], byte[]> {
+
+        private static final long serialVersionUID = -368192386828860022L;
+
+        public BytePersistence() {
+            super();
+        }
+
+        public BytePersistence(Entry<byte[], byte[]> entry) {
+            super(entry);
+        }
+    }
+}
