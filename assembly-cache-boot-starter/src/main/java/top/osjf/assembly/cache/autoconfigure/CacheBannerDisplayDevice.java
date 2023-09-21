93c4496be026a56a123e76d79658d2f183f7424c
zhangpengfei <929160069@qq.com>
2023-09-21 10:15:46 +0800

    --- update to many models

---
+++ assembly-cache-boot-starter/src/main/java/top/osjf/assembly/cache/autoconfigure/CacheBannerDisplayDevice.java
@@ -0,0 +1,51 @@
+package top.osjf.assembly.cache.autoconfigure;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.Banner;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.EnvironmentCapable;
+import top.osjf.assembly.util.annotation.NotNull;
+
+import java.io.PrintStream;
+
+/**
+ * Define the method for caching component logo output, implement {@link Banner}, define relevant
+ * execution parameter acquisition methods, and perform logo output after the initialization of
+ * the corresponding bean is completed.
+ *
+ * @author zpf
+ * @since 1.0.0
+ */
+public interface CacheBannerDisplayDevice extends InitializingBean, Banner, EnvironmentCapable {
+
+    @Override
+    default void afterPropertiesSet() {
+        //print client banner
+        printBanner(getEnvironment(), getSourceClass(), System.out);
+    }
+
+    void printBanner(Environment environment, Class<?> sourceClass, PrintStream out);
+
+    @Override
+    @NotNull
+    Environment getEnvironment();
+
+    /**
+     * Obtain a resource class object that can obtain the version.<br>
+     * If it is the original, it defaults to {@link CacheVersion} to obtain the version of this project.
+     *
+     * @return must not be {@literal null}.
+     */
+    @NotNull
+    default Class<?> getSourceClass() {
+        return CacheVersion.class;
+    }
+
+    /**
+     * Obtain a {@link StartUpBanner} implementation class.
+     *
+     * @return Return the {@link StartUpBanner} associated with this assembly.
+     */
+    @NotNull
+    StartUpBanner getStartUpBanner();
+}
