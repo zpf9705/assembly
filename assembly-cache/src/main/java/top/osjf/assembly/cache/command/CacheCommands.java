4fe9c9bf4db561d4db32528a947245e57014bca5
zhangpengfei <929160069@qq.com>
2023-09-21 13:39:51 +0800

    --- update to many models

---
+++ assembly-cache/src/main/java/top/osjf/assembly/cache/command/CacheCommands.java
@@ -0,0 +1,10 @@
+package top.osjf.assembly.cache.command;
+
+/**
+ * Cache instruction aggregation interface, as a unified implementation.
+ *
+ * @author zpf
+ * @since 1.0.0
+ */
+public interface CacheCommands extends CacheKeyCommands, CachePairCommands {
+}
