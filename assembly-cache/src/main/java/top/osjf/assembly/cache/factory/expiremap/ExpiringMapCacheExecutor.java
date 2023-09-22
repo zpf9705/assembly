7161508e6f7dd9af43a22d050d04a943274a2fef
zhangpengfei <929160069@qq.com>
2023-09-22 09:22:11 +0800

    --- update to many models

--- assembly-cache/src/main/java/top/osjf/assembly/cache/factory/expiremap/ExpiringMapCacheExecutor.java
+++ assembly-cache/src/main/java/top/osjf/assembly/cache/factory/expiremap/ExpiringMapCacheExecutor.java
@@ -16,104 +16,104 @@ import java.util.concurrent.TimeUnit;
 public interface ExpiringMapCacheExecutor extends CacheExecutor {
 
     /**
-     * Proxy for {@link net.jodah.expiringmap.ExpiringMap#put(Object, Object)}
+     * Execute on {@link net.jodah.expiringmap.ExpiringMap#put(Object, Object)}.
      *
      * @param key   must not be {@literal null}
      * @param value must not be {@literal null}
-     * @return set result
+     * @return Returns result value.
      */
     Boolean put(byte[] key, byte[] value);
 
     /**
-     * Proxy for {@link net.jodah.expiringmap.ExpiringMap#put(Object, Object, long, TimeUnit)}}
+     * Execute on {@link net.jodah.expiringmap.ExpiringMap#put(Object, Object, long, TimeUnit)}}.
      *
      * @param key      must not be {@literal null}
      * @param value    must not be {@literal null}
      * @param duration must not be {@literal null}
      * @param unit     must not be {@literal null}
-     * @return set result
+     * @return Returns result value.
      */
     Boolean putDuration(byte[] key, byte[] value, Long duration, TimeUnit unit);
 
     /**
-     * Proxy for {@link net.jodah.expiringmap.ExpiringMap#putIfAbsent(Object, Object)}}
+     * Execute on {@link net.jodah.expiringmap.ExpiringMap#putIfAbsent(Object, Object)}}.
      *
      * @param key   must not be {@literal null}
      * @param value must not be {@literal null}
-     * @return set result
+     * @return Returns result value.
      */
     Boolean putIfAbsent(byte[] key, byte[] value);
 
     /**
-     * Proxy for {@link net.jodah.expiringmap.ExpiringMap#put(Object, Object, long, TimeUnit)}}
+     * Execute on {@link net.jodah.expiringmap.ExpiringMap#put(Object, Object, long, TimeUnit)}}.
      *
      * @param key      must not be {@literal null}
      * @param value    must not be {@literal null}
      * @param duration must not be {@literal null}
      * @param unit     must not be {@literal null}
-     * @return set result
+     * @return Returns result value.
      */
     Boolean putIfAbsentDuration(byte[] key, byte[] value, Long duration, TimeUnit unit);
 
     /**
-     * Proxy for {@link net.jodah.expiringmap.ExpiringMap#get(Object)}
+     * Execute on {@link net.jodah.expiringmap.ExpiringMap#get(Object)}.
      *
      * @param key must not be {@literal null}
-     * @return key in value
+     * @return Returns result value.
      */
     byte[] getVal(byte[] key);
 
     /**
-     * Proxy for {@link net.jodah.expiringmap.ExpiringMap#get(Object)}
+     * Execute on {@link net.jodah.expiringmap.ExpiringMap#get(Object)}.
      *
      * @param key must not be {@literal null}
-     * @return Similar keys with a key
+     * @return Returns result value.
      */
     List<byte[]> getKeysByKeys(byte[] key);
 
     /**
-     * Proxy for {@link net.jodah.expiringmap.ExpiringMap#replace(Object, Object)}
+     * Execute on {@link net.jodah.expiringmap.ExpiringMap#replace(Object, Object)}.
      *
      * @param key      must not be {@literal null}
      * @param newValue must not be {@literal null}
-     * @return The old value is replaced
+     * @return Returns result value.
      */
     byte[] replace(byte[] key, byte[] newValue);
 
     /**
-     * Proxy for {@link net.jodah.expiringmap.ExpiringMap#remove(Object)}
+     * Execute on {@link net.jodah.expiringmap.ExpiringMap#remove(Object)}.
      *
      * @param keys must not be {@literal null}
-     * @return deleted number
+     * @return Returns result value.
      */
     @CanNull
     Long deleteReturnSuccessNum(byte[]... keys);
 
     /**
-     * Proxy for {@link net.jodah.expiringmap.ExpiringMap#remove(Object)}}
+     * Execute on {@link net.jodah.expiringmap.ExpiringMap#remove(Object)}}.
      *
      * @param key must not be {@literal null}
-     * @return removed key/value
+     * @return Returns result value.
      */
     Map<byte[], byte[]> deleteSimilarKey(byte[] key);
 
     /**
-     * Proxy for {@link net.jodah.expiringmap.ExpiringMap#clear()}
+     * Execute on {@link net.jodah.expiringmap.ExpiringMap#clear()}.
      *
-     * @return clear result
+     * @return Returns result value.
      */
     Boolean reboot();
 
     /**
-     * Proxy for {@link net.jodah.expiringmap.ExpiringMap#containsKey(Object)}
+     * Execute on {@link net.jodah.expiringmap.ExpiringMap#containsKey(Object)}.
      *
      * @param key must not be {@literal null}
-     * @return contains result
+     * @return Returns result value.
      */
     Boolean containsKey(byte[] key);
 
     /**
-     * Proxy for {@link net.jodah.expiringmap.ExpiringMap#containsValue(Object)}}
+     * Execute on {@link net.jodah.expiringmap.ExpiringMap#containsValue(Object)}}.
      *
      * @param value must not be {@literal null}
      * @return contains result
@@ -121,54 +121,54 @@ public interface ExpiringMapCacheExecutor extends CacheExecutor {
     Boolean containsValue(byte[] value);
 
     /**
-     * Proxy for {@link net.jodah.expiringmap.ExpiringMap#getExpiration(Object)}
+     * Execute on {@link net.jodah.expiringmap.ExpiringMap#getExpiration(Object)}.
      *
      * @param key must not be {@literal null}
-     * @return Set the time with key
+     * @return Returns result value.
      */
     Long getExpirationWithKey(byte[] key);
 
     /**
-     * Proxy for {@link net.jodah.expiringmap.ExpiringMap#getExpiration(Object)}}
+     * Execute on {@link net.jodah.expiringmap.ExpiringMap#getExpiration(Object)}}.
      *
      * @param key  must not be {@literal null}
      * @param unit must not be {@literal null}
-     * @return Set the time with unit
+     * @return Returns result value.
      */
     Long getExpirationWithUnit(byte[] key, TimeUnit unit);
 
     /**
-     * Proxy for {@link net.jodah.expiringmap.ExpiringMap#getExpectedExpiration(Object)}}
+     * Execute on {@link net.jodah.expiringmap.ExpiringMap#getExpectedExpiration(Object)}}.
      *
      * @param key must not be {@literal null}
-     * @return For the rest with key
+     * @return Returns result value.
      */
     Long getExpectedExpirationWithKey(byte[] key);
 
     /**
-     * Proxy for {@link net.jodah.expiringmap.ExpiringMap#getExpectedExpiration(Object)}}
+     * Execute on {@link net.jodah.expiringmap.ExpiringMap#getExpectedExpiration(Object)}}.
      *
      * @param key  must not be {@literal null}
      * @param unit must not be {@literal null}
-     * @return For the rest with unit
+     * @return Returns result value.
      */
     Long getExpectedExpirationWithUnit(byte[] key, TimeUnit unit);
 
     /**
-     * Proxy for {@link net.jodah.expiringmap.ExpiringMap#setExpiration(Object, long, TimeUnit)}
+     * Execute on {@link net.jodah.expiringmap.ExpiringMap#setExpiration(Object, long, TimeUnit)}.
      *
      * @param key      must not be {@literal null}
      * @param duration must not be {@literal null}
      * @param timeUnit must not be {@literal null}
-     * @return replace duration result
+     * @return Returns result value.
      */
     Boolean setExpirationDuration(byte[] key, Long duration, TimeUnit timeUnit);
 
     /**
-     * Proxy for {@link net.jodah.expiringmap.ExpiringMap#resetExpiration(Object)}
+     * Execute on {@link net.jodah.expiringmap.ExpiringMap#resetExpiration(Object)}.
      *
      * @param key must not be {@literal null}
-     * @return rest result
+     * @return Returns result value.
      */
     Boolean resetExpirationWithKey(byte[] key);
 }
