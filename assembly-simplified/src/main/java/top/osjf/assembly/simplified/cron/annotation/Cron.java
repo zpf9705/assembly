6f26bd2eb24ca0f0a4d30c590d1e541a4a48237e
zhangpengfei <929160069@qq.com>
2023-09-22 16:01:19 +0800

    --- update to many models

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/cron/annotation/Cron.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/cron/annotation/Cron.java
@@ -8,8 +8,8 @@ import java.lang.annotation.*;
  * Flag annotation for timed task method registration, only supports starting with cron expression,
  * supports spring specified environment {@link org.springframework.core.env.Environment}registration,
  * and provides fixed initialization parameters.
- * <p>
- * Relying on {@link CronTaskRegisterImport} to scan, obtain, and register corresponding annotation methods.
+ *
+ * <p>Relying on {@link CronTaskRegisterImport} to scan, obtain, and register corresponding annotation methods.
  *
  * @author zpf
  * @since 1.1.0
@@ -20,17 +20,16 @@ import java.lang.annotation.*;
 public @interface Cron {
 
     /**
-     * Fill in a planned cron expression that represents the frequency of task execution
+     * Fill in a planned cron expression that represents the frequency of task execution.
      *
-     * @return corn express , must no be {@literal null}
+     * @return corn express , must no be {@literal null}.
      */
     String express();
 
     /**
-     * Limited spring startup environment parameters,
-     * only when registering the task in the specified environment
+     * Limited spring startup environment parameters,only when registering the task in the specified environment.
      *
-     * @return If left blank, register directly
+     * @return If left blank, register directly.
      */
     String[] profiles() default {};
 }
