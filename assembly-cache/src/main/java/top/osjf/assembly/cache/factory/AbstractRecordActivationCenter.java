680845f57aae987a77bff852e9073787c26b62b6
zhangpengfei <929160069@qq.com>
2023-09-22 15:25:43 +0800

    --- update to many models

--- assembly-cache/src/main/java/top/osjf/assembly/cache/factory/AbstractRecordActivationCenter.java
+++ assembly-cache/src/main/java/top/osjf/assembly/cache/factory/AbstractRecordActivationCenter.java
@@ -1,13 +1,11 @@
 package top.osjf.assembly.cache.factory;
 
 import org.springframework.util.Assert;
-import top.osjf.assembly.util.ByteContain;
 
 import java.io.Serializable;
 
 /**
- * This abstract class is used to record the currently activated central class
- * and provide it to the corresponding implementation method of the central class.
+ * Abstract function class: used to record the currently activated cache center, providing static storage and retrieval.
  *
  * @author zpf
  * @since 1.0.0
@@ -15,12 +13,10 @@ import java.io.Serializable;
 @SuppressWarnings({"rawtypes", "serial"})
 public abstract class AbstractRecordActivationCenter<C, K, V> implements Center<C, K, V>, Serializable {
 
-    protected static final ByteContain contain = new ByteContain(16);
-
     protected static Center center0;
 
     /**
-     * Placing a single instance cache.
+     * Place it in a global cache center.
      *
      * @param center Must not be {@literal null}.
      */
@@ -33,7 +29,7 @@ public abstract class AbstractRecordActivationCenter<C, K, V> implements Center<
     }
 
     /**
-     * Obtain the currently activated center.
+     * Obtain the global cache center.
      *
      * @return Returns a unique {@link Center} entity.
      */
