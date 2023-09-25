c443dcafbe57d49a396e41b16eb6dad44b1f5d0f
zhangpengfei <929160069@qq.com>
2023-09-25 10:24:31 +0800

    --- update to many models

--- assembly-util/src/main/java/top/osjf/assembly/util/data/ByteIdentify.java
+++ assembly-util/src/main/java/top/osjf/assembly/util/data/ByteIdentify.java
@@ -1,68 +1,33 @@
 package top.osjf.assembly.util.data;
 
-import cn.hutool.core.lang.hash.CityHash;
 import top.osjf.assembly.util.annotation.NotNull;
 
-import java.io.Serializable;
 import java.util.Arrays;
 
 /**
- * The authentication object of byte numbers mainly solves the problem of byte serialization address
- * values changing.<br>
+ * The generic type of {@link Identify} is the {@code byte[]} implementation class.
  *
- * Rewrite the {@link #hashCode()} method to mask address value verification and instead add byte
- * array values to obtain a new {@code hashCode} value.<br>
- *
- * Rewritten the {@link #equals(Object)} method to determine that the value of {@link ByteIdentify}
- * remains consistent only when the tangent is lower than {@code hashcode}.
+ * <p>The authentication object of byte numbers mainly solves the problem of byte
+ * serialization address values changing.
  *
  * @author zpf
  * @since 1.0.0
  */
-public class ByteIdentify implements ComparableBool<ByteIdentify>, Serializable {
+public class ByteIdentify extends Identify<byte[], ByteIdentify> {
 
     private static final long serialVersionUID = -851217802015189183L;
 
-    private byte[] var;
-
     public ByteIdentify(byte[] var) {
-        if (var == null) {
-            throw new IllegalArgumentException("ByteIdentify parse var must no be null");
-        }
-        this.var = var;
-    }
-
-    public byte[] getVar() {
-        return var;
-    }
-
-    public void setVar(byte[] var) {
-        this.var = var;
-    }
-
-    @Override
-    public int hashCode() {
-        return CityHash.hash32(var);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (!(obj instanceof ByteIdentify)) {
-            return false;
-        }
-        return this.hashCode() == obj.hashCode();
+        super(var);
     }
 
     @Override
     public int compareTo(@NotNull ByteIdentify o) {
         int compare;
-        String s0 = Arrays.toString(o.var)
+        String s0 = Arrays.toString(o.getData())
                 .replace("[", "")
                 .replace("]", "");
-        String s01 = Arrays.toString(this.var)
+        String s01 = Arrays.toString(getData())
                 .replace("[", "")
                 .replace("]", "");
         int s0l = s0.length();
@@ -86,7 +51,7 @@ public class ByteIdentify implements ComparableBool<ByteIdentify>, Serializable 
     @Override
     public String toString() {
         return "ByteContain{" +
-                "var=" + Arrays.toString(var) +
+                "var=" + Arrays.toString(getData()) +
                 '}';
     }
 }
