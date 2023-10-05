a844d236e621717e973e99cb74816a4c31e39e0b
zhangpengfei <929160069@qq.com>
2023-10-05 09:42:11 +0800

    --- update to many models for service collection

---
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/service/annotation/ServiceCollection.java
@@ -0,0 +1,21 @@
+package top.osjf.assembly.simplified.service.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * The annotation that needs to be annotated for service interfaces/abstract classes
+ * is mainly used to tell you that once the class is annotated, its implementation
+ * class or subclass can be automatically collected, provided that it has been added
+ * to the Spring container and can be collected.
+ *
+ * <p>For specific collection methods, please refer to the implementation class
+ * {@link top.osjf.assembly.simplified.service.context.ClassesServiceContext}.</p>
+ *
+ * @author zpf
+ * @since 2.0.4
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface ServiceCollection {
+}
