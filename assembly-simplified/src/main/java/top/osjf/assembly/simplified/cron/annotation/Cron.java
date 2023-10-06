3b2b834087757c0f407c33d66fcb4c36a5d47e13
zhangpengfei <929160069@qq.com>
2023-10-06 21:21:10 +0800

    --- update to many models for service collection

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/cron/annotation/Cron.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/cron/annotation/Cron.java
@@ -1,14 +1,17 @@
 package top.osjf.assembly.simplified.cron.annotation;
 
+import top.osjf.assembly.simplified.cron.CronTaskRegister;
+
 import java.lang.annotation.*;
 
 /**
- * Flag annotation for timed task method registration, only supports starting with cron expression,
- * supports spring specified environment {@link org.springframework.core.env.Environment}registration,
+ * Flag annotation for timed task method registration, only supports
+ * starting with cron expression, supports spring specified environment
+ * {@link org.springframework.core.env.Environment} registration,
  * and provides fixed initialization parameters.
  *
- * <p>Relying on {@link top.osjf.assembly.simplified.cron.CronTaskRegisterBootProcessor} to scan,
- * obtain, and register corresponding annotation methods.
+ * <p>Relying on {@link CronTaskRegister} to scan, obtain, and register
+ * corresponding annotation methods.
  *
  * @author zpf
  * @since 1.1.0
@@ -19,16 +22,26 @@ import java.lang.annotation.*;
 public @interface Cron {
 
     /**
-     * Provide the correct formatted cron expression for timing.
+     * Provide a cron expression that conforms to the rules.
+     * <p>This parameter will determine the execution interval of the
+     * registered scheduled task.
+     * <p>Please understand the writing conventions of cron expressions
+     * before use to avoid unnecessary errors.
      *
-     * @return corn express , must no be {@literal null}.
+     * @return Must be provided and cannot be {@literal null}.
      */
     String express();
 
     /**
-     * Specify the set of environment names for the spring framework to start and run for scheduled tasks.
+     * Since the implementation of this annotation needs to be in the
+     * springboot environment, the purpose of this parameter is to restrict
+     * the environment for registering scheduled tasks.
+     * <p>Referring to the configuration parameters of
+     * <pre>{@code Spring.profiles.active}</pre>, this parameter can be
+     * registered to the scheduled thread pool after being configured as above.
      *
-     * @return If left blank, register directly.
+     * @return The array of registered environment parameters needs to be restricted.
+     * If it is empty, it will be directly registered to the task thread pool.
      */
     String[] profiles() default {};
 }
--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/cron/annotation/EnableCronTaskRegister.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/cron/annotation/EnableCronTaskRegister.java
@@ -4,18 +4,20 @@ import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.AliasFor;
 import top.osjf.assembly.simplified.cron.AbstractCronListener;
 import top.osjf.assembly.simplified.cron.CronListener;
-import top.osjf.assembly.simplified.cron.CronTaskRegisterBootProcessor;
+import top.osjf.assembly.simplified.cron.CronRegister;
+import top.osjf.assembly.simplified.cron.CronTaskRegister;
 
 import java.lang.annotation.*;
 
 /**
  * Opening annotations for timed task registration.
  *
- * <p>This annotation will scan the methods that carry {@link CronTaskRegisterBootProcessor} through {@link Cron},
- * register them with the timed thread pool using {@link top.osjf.assembly.simplified.cron.CronRegister},
+ * <p>This annotation will scan the methods that carry {@link CronTaskRegister} through
+ * {@link Cron}, register them with the timed thread pool using {@link CronRegister},
  * and trigger these operations.
  *
- * <p>It only needs to be standardized in the class header of the spring container to be implemented.</p>
+ * <p>It only needs to be standardized in the class header of the spring
+ * container to be implemented.</p>
  *
  * @author zpf
  * @since 1.1.0
@@ -23,44 +25,40 @@ import java.lang.annotation.*;
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Import({CronTaskRegisterBootProcessor.class})
+@Import({CronTaskRegister.class})
 public @interface EnableCronTaskRegister {
 
     /**
      * Carrying the path where the {@link Cron} class is located.
+     * <p>At the same time, task listeners can also be added to implement
+     * {@link CronListener} or {@link AbstractCronListener}.
+     * <p>If it is null, the default is to use springboot to start the
+     * package path where the main class is located.
      *
-     * <p>At the same time, task listeners can also be added to implement.
-     * {@link CronListener} or
-     * {@link AbstractCronListener}
-     *
-     * <p>If it is null, the default is to use springboot to start the package path where the main class is located.
-     *
-     * @return alias for {{@link #basePackages()}}
+     * @return Alias for {{@link #basePackages()}}.
      */
     @AliasFor("basePackages")
     String[] value() default {};
 
     /**
      * His value shifts to {@link #value()}, consistent with it.
+     * <p>At the same time, task listeners can also be added to implement
+     * {@link CronListener} or {@link AbstractCronListener}.
+     * <p>If it is null, the default is to use springboot to start the
+     * package path where the main class is located.
      *
-     * <p>At the same time, task listeners can also be added to implement.
-     * {@link CronListener} or
-     * {@link AbstractCronListener}
-     *
-     * <p>If it is null, the default is to use springboot to start the package path where the main class is located.
-     *
-     * @return alias for {{@link #value()}}
+     * @return Alias for {{@link #value()}}.
      */
     @AliasFor("value")
     String[] basePackages() default {};
 
     /**
-     * Choose whether to start the timed thread pool even if no registration method is found.
-     *
+     * Choose whether to start the timed thread pool even if no
+     * registration method is found.
      * <p>But I won't actively add listeners for {@link CronListener},
      * unless a timing method with {@link Cron} annotation is added.
      *
-     * @return if {@code true} , defaults to enabled.
+     * @return If {@code true} , defaults to enable.
      */
     boolean noMethodDefaultStart() default false;
 }
