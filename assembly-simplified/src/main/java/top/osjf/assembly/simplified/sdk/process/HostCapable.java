7e19c29065ac1517c0c8d82e6d1c22c66896f0b5
zhangpengfei <929160069@qq.com>
2023-10-06 21:30:57 +0800

    --- update to many models for service collection

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/process/HostCapable.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/process/HostCapable.java
@@ -1,17 +1,15 @@
 package top.osjf.assembly.simplified.sdk.process;
 
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.core.annotation.AnnotationAttributes;
-import org.springframework.core.type.AnnotationMetadata;
-import top.osjf.assembly.simplified.sdk.annotation.SdkProxyBeanDefinitionProcessor;
+import top.osjf.assembly.simplified.sdk.annotation.SdkProxyBeanRegister;
 import top.osjf.assembly.util.annotation.NotNull;
 
 /**
- * When registering a proxy class to the spring container at the interface level,
- * the property fields placed in the host address acquisition interface are completed in
- * {@link SdkProxyBeanDefinitionProcessor#beanRegister(AnnotationAttributes, BeanDefinitionRegistry, AnnotationMetadata)}.
+ * When registering a proxy class to the spring container at the interface
+ * level, the property fields placed in the host address acquisition interface
+ * are completed in {@link SdkProxyBeanRegister#beanRegister}.
  *
- * <p>When dynamic SDK calls are made, they are retrieved at any time for calling operations.</p>
+ * <p>When dynamic SDK calls are made, they are retrieved at
+ * any time for calling operations.
  *
  * @author zpf
  * @since 1.1.0
