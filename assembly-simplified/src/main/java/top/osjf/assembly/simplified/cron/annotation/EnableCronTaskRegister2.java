48f1700739d71cd8c6254711820060d01b293383
zhangpengfei <929160069@qq.com>
2023-10-11 15:18:45 +0800

    --- update to many utils

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/cron/annotation/EnableCronTaskRegister2.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/cron/annotation/EnableCronTaskRegister2.java
@@ -1,7 +1,6 @@
 package top.osjf.assembly.simplified.cron.annotation;
 
 import org.springframework.context.annotation.Import;
-import top.osjf.assembly.simplified.cron.CronListener;
 import top.osjf.assembly.simplified.cron.CronTaskRegisterConfiguration;
 
 import java.lang.annotation.*;
@@ -14,8 +13,12 @@ import java.lang.annotation.*;
  * formulation of a scanning path and instead chooses to wear annotations
  * {@link Cron} method on all registered beans, which is more convenient.
  *
- * @see top.osjf.assembly.simplified.cron.CronTaskRegisterPostProcessor
+ * <p>At the same time, the docking {@link EnableCronTaskRegister} also lacks
+ * default startup. When changing the annotation configuration to default task
+ * less registration, it will also start the timed thread.
+ *
  * @author zpf
+ * @see top.osjf.assembly.simplified.cron.CronTaskRegisterPostProcessor
  * @since 2.0.6
  */
 @Target({ElementType.TYPE})
@@ -23,14 +26,4 @@ import java.lang.annotation.*;
 @Documented
 @Import({CronTaskRegisterConfiguration.class})
 public @interface EnableCronTaskRegister2 {
-
-    /**
-     * Choose whether to start the timed thread pool even if no
-     * registration method is found.
-     * <p>But I won't actively add listeners for {@link CronListener},
-     * unless a timing method with {@link Cron} annotation is added.
-     *
-     * @return If {@code true} , defaults to enable.
-     */
-    boolean noMethodDefaultStart() default false;
 }
