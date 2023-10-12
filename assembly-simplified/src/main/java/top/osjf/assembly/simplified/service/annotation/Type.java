ba7c97721ab7a8efa498373693fddde646d36461
zhangpengfei <929160069@qq.com>
2023-10-12 09:47:25 +0800

    --- update to many utils

---
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/service/annotation/Type.java
@@ -0,0 +1,20 @@
+package top.osjf.assembly.simplified.service.annotation;
+
+/**
+ * Enumeration of types selected for service context configuration loading.
+ *
+ * @author zpf
+ * @since 2.0.6
+ */
+public enum Type {
+
+    /**
+     * @see top.osjf.assembly.simplified.service.context.ClassesServiceContext
+     */
+    CLASSES,
+
+    /**
+     * @see top.osjf.assembly.simplified.service.context.SimpleServiceContext
+     */
+    SIMPLE
+}
