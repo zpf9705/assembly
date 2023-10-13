641ce11e66f6c4222fc5fd75c10dbeef80a877a1
zhangpengfei <929160069@qq.com>
2023-10-13 15:33:58 +0800

    --- update to many utils

---
+++ assembly-util/src/main/java/top/osjf/assembly/util/lang/ClassUtils.java
@@ -0,0 +1,10 @@
+package top.osjf.assembly.util.lang;
+
+/**
+ * Class utils comes from {@link ClassUtils}, and this category points out the expansion function.
+ *
+ * @author zpf
+ * @since 1.0.4
+ */
+public class ClassUtils extends org.apache.commons.lang3.ClassUtils {
+}
