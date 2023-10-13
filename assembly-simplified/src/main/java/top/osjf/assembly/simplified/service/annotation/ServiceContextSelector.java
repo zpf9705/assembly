e410f693672f05762b4fdea9383ecd26ea0713e4
zhangpengfei <929160069@qq.com>
2023-10-13 13:05:53 +0800

    --- update to many utils

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/service/annotation/ServiceContextSelector.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/service/annotation/ServiceContextSelector.java
@@ -3,21 +3,23 @@ package top.osjf.assembly.simplified.service.annotation;
 import org.springframework.context.annotation.ImportSelector;
 import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.type.AnnotationMetadata;
-import top.osjf.assembly.simplified.service.context.ClassesServiceContext;
-import top.osjf.assembly.simplified.service.context.SimpleServiceContext;
+import top.osjf.assembly.simplified.service.context.ClassesServiceContextConfiguration;
+import top.osjf.assembly.simplified.service.context.SimpleServiceContextConfiguration;
 import top.osjf.assembly.util.annotation.NotNull;
 
 import java.util.Objects;
 
 /**
  * Select the processor for service context configuration.
+ * <ul>
+ *     <li>{@link ClassesServiceContextConfiguration}
+ *     <pre>&#064;EnableServiceCollection2@type=SIMPLE.</pre></li>
+ *     <li>{@link SimpleServiceContextConfiguration}
+ *     <pre>&#064;EnableServiceCollection2@type=CLASSES.</li>
+ * </ul>
  *
- * <p>Select {@link top.osjf.assembly.simplified.service.context.ServiceContext} injection
- * {@link ClassesServiceContext} or {@link SimpleServiceContext} based on the type of annotation {},
- * and select {@link ClassesServiceContext} by default.
- *
- * @see top.osjf.assembly.simplified.service.context.ServiceContext
  * @author zpf
+ * @see Type
  * @since 2.0.6
  */
 public class ServiceContextSelector implements ImportSelector {
@@ -32,9 +34,9 @@ public class ServiceContextSelector implements ImportSelector {
         Type type = attributes.getEnum("type");
         String name;
         if (Type.CLASSES.equals(type)) {
-            name = ClassesServiceContext.class.getName();
+            name = ClassesServiceContextConfiguration.class.getName();
         } else if (Type.SIMPLE.equals(type)) {
-            name = SimpleServiceContext.class.getName();
+            name = SimpleServiceContextConfiguration.class.getName();
         } else {
             name = null;
         }
