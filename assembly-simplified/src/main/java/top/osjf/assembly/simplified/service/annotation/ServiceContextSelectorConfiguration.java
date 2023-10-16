6920a0b7d1fd0c9110d0449e723e36958e5cb058
zhangpengfei <929160069@qq.com>
2023-10-16 14:13:56 +0800

    --- update to many utils

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/service/annotation/ServiceContextSelectorConfiguration.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/service/annotation/ServiceContextSelectorConfiguration.java
@@ -1,12 +1,14 @@
 package top.osjf.assembly.simplified.service.annotation;
 
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportSelector;
+import org.springframework.context.annotation.ImportAware;
 import org.springframework.context.annotation.Role;
 import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.type.AnnotationMetadata;
+import top.osjf.assembly.simplified.service.ServiceContextUtils;
 import top.osjf.assembly.simplified.service.context.ClassesServiceContext;
 import top.osjf.assembly.simplified.service.context.ServiceContext;
 import top.osjf.assembly.simplified.service.context.SimpleServiceContext;
@@ -16,57 +18,46 @@ import java.util.Objects;
 
 /**
  * Select the processor for service context configuration.
+ * <ul>
+ *     <li>{@link ClassesServiceContext}
+ *     <pre>&#064;EnableServiceCollection2@type=SIMPLE.</pre></li>
+ *     <li>{@link SimpleServiceContext}
+ *     <pre>&#064;EnableServiceCollection2@type=CLASSES.</pre></li>
+ * </ul>
  *
- * @author zpf
  * @see EnableServiceCollection2
+ * @author zpf
  * @since 2.0.6
  */
 @Configuration(proxyBeanMethods = false)
 @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
-public class ServiceContextSelectorConfiguration {
+public class ServiceContextSelectorConfiguration implements ImportAware {
 
-    private static SimpleServiceContext context;
+    /**
+     * The initialization object set when the type is simple is to call an
+     * empty construct to add it to the collection of {@link BeanPostProcessor},
+     * collect service objects, and then hand them over to the spring container for management.
+     */
+    private SimpleServiceContext context;
 
-    @Bean("TYPE-CHOOSE-CONTEXT")
-    public ServiceContext serviceContext() {
-        Type type = Selector.type;
-        if (type == null || type.equals(Type.CLASSES)) {
-            return new ClassesServiceContext();
-        } else {
-            context.addBeanPostProcessor();
-            return context;
+    private Type type;
+
+    @Override
+    public void setImportMetadata(@NotNull AnnotationMetadata metadata) {
+        AnnotationAttributes attributes =
+                AnnotationAttributes.fromMap(metadata
+                        .getAnnotationAttributes(EnableServiceCollection2.class.getName()));
+        Objects.requireNonNull(attributes, EnableServiceCollection2.class.getName()
+                + " analysis failed.");
+        type = attributes.getEnum("type");
+        if (Objects.equals(type, Type.SIMPLE)) {
+            //Initialize the bean listener.
+            context = new SimpleServiceContext();
         }
     }
 
-    /**
-     * <ul>
-     *     <li>{@link ClassesServiceContext}
-     *     <pre>&#064;EnableServiceCollection2@type=SIMPLE.</pre></li>
-     *     <li>{@link SimpleServiceContext}
-     *     <pre>&#064;EnableServiceCollection2@type=CLASSES.</pre></li>
-     * </ul>
-     *
-     * @author zpf
-     * @see Type
-     * @since 2.0.6
-     */
-    public static class Selector implements ImportSelector {
-
-        static Type type;
-
-        @NotNull
-        @Override
-        public String[] selectImports(@NotNull AnnotationMetadata metadata) {
-            AnnotationAttributes attributes =
-                    AnnotationAttributes.fromMap(metadata
-                            .getAnnotationAttributes(EnableServiceCollection2.class.getName()));
-            Objects.requireNonNull(attributes, EnableServiceCollection2.class.getName()
-                    + " analysis failed.");
-            type = attributes.getEnum("type");
-            if (Objects.equals(type, Type.SIMPLE)) {
-                context = new SimpleServiceContext();
-            }
-            return new String[0];
-        }
+    @Bean(ServiceContextUtils.SERVICE_CONTEXT_NAME)
+    public ServiceContext serviceContext() {
+        return ServiceContextUtils.newOrDefault(type, context);
     }
 }
