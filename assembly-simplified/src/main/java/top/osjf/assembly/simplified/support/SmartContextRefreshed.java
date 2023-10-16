b09521655f5d10e4d3cf98c243fec7952fce12fe
zhangpengfei <929160069@qq.com>
2023-10-16 15:24:50 +0800

    --- update to many utils

---
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/support/SmartContextRefreshed.java
@@ -0,0 +1,27 @@
+package top.osjf.assembly.simplified.support;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.ContextRefreshedEvent;
+import top.osjf.assembly.util.annotation.NotNull;
+import top.osjf.assembly.util.lang.Asserts;
+
+/**
+ * Smart State checker for after Context refresh.
+ *
+ * @author zpf
+ * @since 2.0.6
+ */
+public abstract class SmartContextRefreshed extends AbstractApplicationContextListener {
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        Asserts.state(nowApplicationContext() == event.getApplicationContext(),
+                "Inconsistency before and after application startup.");
+    }
+
+    /**
+     * @return Currently supported application context information.
+     */
+    @NotNull
+    public abstract ApplicationContext nowApplicationContext();
+}
