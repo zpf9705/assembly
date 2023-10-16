0ba97cfb7e1f0685420ed1e6d61eb8abc63df206
zhangpengfei <929160069@qq.com>
2023-10-16 14:44:17 +0800

    --- update to many utils

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/service/context/SimpleServiceContext.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/service/context/SimpleServiceContext.java
@@ -2,17 +2,24 @@ package top.osjf.assembly.simplified.service.context;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.support.MergedBeanDefinitionPostProcessor;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.core.Ordered;
+import top.osjf.assembly.simplified.service.ServiceContextUtils;
 import top.osjf.assembly.simplified.service.annotation.ServiceCollection;
 import top.osjf.assembly.util.annotation.CanNull;
 import top.osjf.assembly.util.annotation.NotNull;
 import top.osjf.assembly.util.data.ClassMap;
 import top.osjf.assembly.util.lang.CollectionUtils;
+import top.osjf.assembly.util.lang.StringUtils;
 import top.osjf.assembly.util.logger.Console;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Support implementation class for service context {@link ServiceContext}.
@@ -37,7 +44,19 @@ import java.util.Map;
  * @author zpf
  * @since 2.0.6
  */
-public class SimpleServiceContext extends AbstractServiceContext implements BeanPostProcessor, Ordered {
+public class SimpleServiceContext extends AbstractServiceContext implements MergedBeanDefinitionPostProcessor, Ordered {
+
+    private final Map<String, String> loaded = new HashMap<>();
+
+    public SimpleServiceContext() {
+        //Manually add to the post processor collection of the bean.
+        getConfigurableApplicationContext().getBeanFactory().addBeanPostProcessor(this);
+    }
+
+    @Override
+    public void postProcessMergedBeanDefinition(@NotNull RootBeanDefinition beanDefinition, @NotNull Class<?> beanType,
+                                                @NotNull String beanName) {
+    }
 
     @CanNull
     @Override
@@ -51,10 +70,15 @@ public class SimpleServiceContext extends AbstractServiceContext implements Bean
         return Integer.MIN_VALUE + 17;
     }
 
-    public void addBeanPostProcessor(){
-        getConfigurableApplicationContext()
-                .getBeanFactory()
-                .addBeanPostProcessor(this);
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        super.onApplicationEvent(event);
+    }
+
+    @Override
+    public void close() {
+        super.close();
+        this.loaded.clear();
     }
 
     private void preServiceCollection(Object bean, String beanName) {
@@ -68,24 +92,39 @@ public class SimpleServiceContext extends AbstractServiceContext implements Bean
         //If this bean directly contains service collection annotations and satisfies
         // the requirements of an interface or abstract class, then service collection
         // is directly carried out.
-        if (ServiceContextUtils.isServiceCollectionParent(dela)) {
+        if (loadCheck(dela.getName())
+                &&
+                ServiceContextUtils.isServiceCollectionParent(dela)) {
             classes.add(dela);
         } else {
             //Collect the interface collection and inheritance classes of this bean,
             // and search for service collection identification annotations.
             classes = ServiceContextUtils.getFilterServices(dela);
-            if (CollectionUtils.isEmpty(classes)) return;
-            Console.info("Service collection : {}", classes);
-            for (Class<?> clazz : classes) {
-                Map<String, ?> beansOfType;
-                try {
-                    //The collection of final services still relies on contextual objects.
-                    beansOfType = getApplicationContext().getBeansOfType(clazz);
-                } catch (BeansException e) {
-                    return;
-                }
-                beansOfType.forEach(contextMap::putIfAbsent);
-            }
         }
+        if (CollectionUtils.isEmpty(classes)) return;
+        //Avoid repeated loading.
+        classes = classes.stream()
+                .filter(c -> loadCheck(c.getName())).collect(Collectors.toList());
+        if (CollectionUtils.isEmpty(classes)) return;
+        Console.info("Service collection : {}", classes);
+        for (Class<?> clazz : classes) {
+            Map<String, ?> beansMap;
+            try {
+                //The collection of final services still relies on contextual objects.
+                beansMap = getApplicationContext().getBeansOfType(clazz);
+            } catch (BeansException e) {
+                return;
+            }
+            beansMap.forEach(contextMap::putIfAbsent);
+            loadSet(clazz.getName());
+        }
+    }
+
+    private void loadSet(String name) {
+        loaded.putIfAbsent(name, name);
+    }
+
+    private boolean loadCheck(String name) {
+        return StringUtils.isBlank(loaded.get(name));
     }
 }
