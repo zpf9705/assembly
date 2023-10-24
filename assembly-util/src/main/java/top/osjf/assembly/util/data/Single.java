71c7b9a0a784fd5e9ce98095aad34e30eb77bbb8
zhangpengfei <929160069@qq.com>
2023-10-24 11:17:25 +0800

    --- update to many utils

---
+++ assembly-util/src/main/java/top/osjf/assembly/util/data/Single.java
@@ -0,0 +1,30 @@
+package top.osjf.assembly.util.data;
+
+import java.io.Serializable;
+
+/**
+ * The encapsulation mode of a single object.
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.4
+ */
+public class Single<V1> implements Serializable {
+    private static final long serialVersionUID = 3978229730331192998L;
+
+    private V1 v1;
+
+    public Single() {
+        this(null);
+    }
+
+    public Single(V1 v1) {
+        this.v1 = v1;
+    }
+
+    public V1 getV1() {
+        return v1;
+    }
+
+    public void setV1(V1 v1) {
+        this.v1 = v1;
+    }
+}
