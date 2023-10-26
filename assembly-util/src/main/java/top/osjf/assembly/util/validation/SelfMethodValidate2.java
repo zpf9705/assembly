fc1a34c963294513dcb617405e45d169f6a91922
zhangpengfei <929160069@qq.com>
2023-10-26 17:50:21 +0800

    --- update to many utils

---
+++ assembly-util/src/main/java/top/osjf/assembly/util/validation/SelfMethodValidate2.java
@@ -0,0 +1,67 @@
+package top.osjf.assembly.util.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Another verification method different from {@link SelfMethodValidate} annotation.
+ * <p>This annotation needs to directly provide the name of the validation method,
+ * specify the parameter types that must be returned, and perform validation through
+ * reflection, which requires you to fill in the correct method name.
+ * <p>Provided method level information acquisition that takes precedence over
+ * {@link #message()} throwing, you can provide a correct method name and return
+ * value method, which can be obtained through reflection calls.
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.5
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = MethodValidateConstraintValidator2.class)
+@Repeatable(value = SelfMethodValidate2.List.class)
+public @interface SelfMethodValidate2 {
+
+    /**
+     * To support multiple validations, it is necessary to specify the
+     * name of the validation method and the format of the return value as
+     * <pre>{@code Supplier<Boolean>}.</pre>
+     * @return Cannot be empty, item must be provided.
+     */
+    String validateMethodName();
+
+    /**
+     * Provide the method {@link #validateMethodName()} above, and when
+     * there is no result returned from {@literal true}, the method that
+     * obtains the thrown information has a high priority than {@link #message()}.
+     * @return The name of the method for obtaining failure information.
+     */
+    String errorReplyMethod() default "";
+
+    /**
+     * If not provided {@link #errorReplyMethod()}, default information
+     * will be used.
+     * <p>Of course, if you edit it well, your failed statements will
+     * still be prioritized.
+     * @return failed msg.
+     */
+    String message() default "An unknown error occurred during model validation.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    /**
+     * Defines several {@code @SelfMethodValidate2} constraints on the same element.
+     *
+     * @see SelfMethodValidate2
+     */
+    @Target(ElementType.TYPE)
+    @Retention(RUNTIME)
+    @Documented
+    @interface List {
+        SelfMethodValidate2[] value();
+    }
+}
