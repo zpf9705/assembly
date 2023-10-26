5a8e643c72e52719aff4b4606a6e084e29b30f73
zhangpengfei <929160069@qq.com>
2023-10-26 11:41:40 +0800

    --- update to many utils

--- assembly-util/src/main/java/top/osjf/assembly/util/data/Triple.java
+++ assembly-util/src/main/java/top/osjf/assembly/util/data/Triple.java
@@ -2,6 +2,7 @@ package top.osjf.assembly.util.data;
 
 /**
  * The encapsulation mode of three objects.
+ *
  * @param <V1> One parameter.
  * @param <V2> Two parameter.
  * @param <V3> Three parameter.
@@ -12,13 +13,24 @@ public class Triple<V1, V2, V3> extends Double<V1, V2> {
 
     private static final long serialVersionUID = 2558111638535260492L;
 
+    private Double<V1, V2> doubles;
+
     private V3 v3;
 
     public Triple() {
         super();
     }
 
+    public Triple(Double<V1, V2> doubles) {
+        this(doubles, null);
+    }
+
     public Triple(V3 v3) {
+        this.v3 = v3;
+    }
+
+    public Triple(Double<V1, V2> doubles, V3 v3) {
+        this.doubles = doubles;
         this.v3 = v3;
     }
 
@@ -44,7 +56,15 @@ public class Triple<V1, V2, V3> extends Double<V1, V2> {
         return v3 != null;
     }
 
+    public void setDouble(Double<V1, V2> doubles) {
+        this.doubles = doubles;
+    }
+
     public Double<V1, V2> getDouble() {
+        return doubles;
+    }
+
+    public Double<V1, V2> getSelfDouble() {
         return this;
     }
 
@@ -54,6 +74,14 @@ public class Triple<V1, V2, V3> extends Double<V1, V2> {
 
     public static <V1, V2, V3> Triple<V1, V2, V3> ofTriple(V1 v1, V2 v2, V3 v3) {
         return new Triple<>(v1, v2, v3);
+    }
+
+    public static <V1, V2, V3> Triple<V1, V2, V3> ofTriple(Double<V1, V2> doubles, V3 v3) {
+        return new Triple<>(doubles, v3);
+    }
+
+    public static <V1, V2, V3> Triple<V1, V2, V3> ofTripleWithDouble(V1 v1, V2 v2, V3 v3) {
+        return ofTriple(ofDoubleWithSingle(v1, v2), v3);
     }
 
     @Override
