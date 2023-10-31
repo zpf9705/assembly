2034cc23c4526708b1cd037676a2048b1c032b7b
zhangpengfei <929160069@qq.com>
2023-10-31 16:43:25 +0800

    --- update to many utils

---
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/aspect/MethodInterceptorImpl.java
@@ -0,0 +1,30 @@
+package top.osjf.assembly.simplified.aspect;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import top.osjf.assembly.util.annotation.CanNull;
+import top.osjf.assembly.util.annotation.NotNull;
+
+/**
+ * A simple implementation class for {@link MethodInterceptor}, mainly used
+ * to pass {@link MethodInvocation} parameters before and after method execution
+ * into the current thread, and obtain them through {@link InvocationContext}
+ * operations, simplifying the operation of AOP.
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 2.0.7
+ */
+public class MethodInterceptorImpl implements MethodInterceptor {
+
+    @CanNull
+    @Override
+    public Object invoke(@NotNull MethodInvocation invocation) throws Throwable {
+        InvocationContext.setCurrentInvocation(invocation);
+        Object proceed;
+        try {
+            proceed = invocation.proceed();
+        } finally {
+            InvocationContext.setCurrentInvocation(null);
+        }
+        return proceed;
+    }
+}
