1c37b22b3482228672fcc8352d843e6b7615f26f
zhangpengfei <929160069@qq.com>
2023-10-31 17:06:31 +0800

    --- update to many utils

---
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/aspect/InvocationContext.java
@@ -0,0 +1,31 @@
+package top.osjf.assembly.simplified.aspect;
+
+import org.aopalliance.intercept.MethodInvocation;
+
+/**
+ * Temporarily store {@link MethodInvocation} during method interceptor
+ * execution for elegant retrieval during method execution.
+ * <p>Clear the {@link MethodInvocation} of the current thread at
+ * the end of method execution or when an exception occurs.
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 2.0.7
+ */
+public final class InvocationContext {
+
+    private static final ThreadLocal<MethodInvocation> invocation_local = new ThreadLocal<>();
+
+    private InvocationContext() {
+    }
+
+    static void setCurrentInvocation(MethodInvocation invocation) {
+        if (invocation == null) {
+            invocation_local.remove();
+        } else {
+            invocation_local.set(invocation);
+        }
+    }
+
+    public static MethodInvocation currentInvocation() {
+        return invocation_local.get();
+    }
+}
