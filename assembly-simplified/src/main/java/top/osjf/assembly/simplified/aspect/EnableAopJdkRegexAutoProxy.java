7c365a9c6e112a591857578bbfe079d985f1839d
zhangpengfei <929160069@qq.com>
2023-10-31 16:51:31 +0800

    --- update to many utils

---
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/aspect/EnableAopJdkRegexAutoProxy.java
@@ -0,0 +1,33 @@
+package top.osjf.assembly.simplified.aspect;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+/**
+ * Add this annotation to configure a spring AOP, directly using
+ * {@link org.springframework.aop.support.JdkRegexpMethodPointcut} with
+ * parameter {@link #patten()} as its method matching criterion.
+ *
+ * <p>After adding this annotation, the method information of the current
+ * thread can be obtained in real-time in {@link InvocationContext}.
+ *
+ * <p>The implementation method is similar to {@link org.springframework.aop.framework.AopContext},
+ * and the real-time method information is deleted after the method
+ * execution is completed.
+ *
+ * @see org.springframework.aop.support.StaticMethodMatcher
+ * @see org.springframework.aop.support.StaticMethodMatcherPointcut
+ * @see org.springframework.aop.support.AbstractRegexpMethodPointcut
+ * @see org.springframework.aop.support.JdkRegexpMethodPointcut
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 2.0.7
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Import(AopJdkRegexAutoProxyConfiguration.class)
+public @interface EnableAopJdkRegexAutoProxy {
+
+    String patten();
+}
