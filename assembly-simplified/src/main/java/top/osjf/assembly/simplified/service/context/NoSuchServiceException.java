9ca77c5899886ccafc02535c9dc7702bd6c1b207
zhangpengfei <929160069@qq.com>
2023-11-08 15:51:39 +0800

    --- update to many utils

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/service/context/NoSuchServiceException.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/service/context/NoSuchServiceException.java
@@ -1,6 +1,6 @@
 package top.osjf.assembly.simplified.service.context;
 
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.BeansException;
 
 /**
  * An exception thrown when the service is not queried.
@@ -8,11 +8,18 @@ import org.springframework.beans.factory.NoSuchBeanDefinitionException;
  * @author zpf
  * @since 2.0.4
  */
-public class NoSuchServiceException extends NoSuchBeanDefinitionException {
+public class NoSuchServiceException extends BeansException {
 
     private static final long serialVersionUID = 922435911357767431L;
 
-    public NoSuchServiceException(String name) {
-        super(name);
+    private final String serviceName;
+
+    public NoSuchServiceException(String serviceName) {
+        super("No service named " + serviceName + " was found from the service context");
+        this.serviceName = serviceName;
+    }
+
+    public String getServiceName() {
+        return serviceName;
     }
 }
