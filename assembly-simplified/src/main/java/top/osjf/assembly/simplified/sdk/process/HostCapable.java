9d7e60ece425cfc6018a634870c918083e0e0dd3
zhangpengfei <929160069@qq.com>
2023-11-23 15:25:38 +0800

    --- update to many utils

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/process/HostCapable.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/process/HostCapable.java
@@ -6,10 +6,11 @@ import top.osjf.assembly.util.annotation.NotNull;
 /**
  * When registering a proxy class to the spring container at the interface
  * level, the property fields placed in the host address acquisition interface
- * are completed in {@link SdkProxyBeanRegister#beanRegister}.
+ * are completed in {@link SdkProxyBeanRegister#getBeanDefinitionHolder
+ * (AnnotationAttributes, AnnotationMetadata)}.
  *
- * <p>When dynamic SDK calls are made, they are retrieved at
- * any time for calling operations.
+ * <p>When dynamic SDK calls are made, they are retrieved at any time for \
+ * calling operations.
  *
  * @author zpf
  * @since 1.1.0
@@ -18,7 +19,6 @@ public interface HostCapable {
 
     /**
      * Obtain the requested host address for each SDK.
-     *
      * @return Must not be {@literal  null}.
      */
     @NotNull
