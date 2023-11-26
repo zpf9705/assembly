6dbe152df5d450d39b0bb71928af9e48f8e50586
zhangpengfei <929160069@qq.com>
2023-11-26 11:23:59 +0800

    --- update to many utils

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/client/ClientExecutors.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/client/ClientExecutors.java
@@ -3,6 +3,7 @@ package top.osjf.assembly.simplified.sdk.client;
 import top.osjf.assembly.simplified.sdk.process.Request;
 import top.osjf.assembly.simplified.sdk.process.Response;
 
+import java.io.IOException;
 import java.util.function.Supplier;
 
 /**
@@ -20,9 +21,9 @@ public final class ClientExecutors extends ClientAccess {
     }
 
     /**
-     * The static method executed by SDK is functionally placed in the host name {@link Supplier}
-     * and executed through the host name and {@link Request} parameters.
-     *
+     * The static method executed by SDK is functionally placed in
+     * the host name {@link Supplier} and executed through the host
+     * name and {@link Request} parameters.
      * @param request {@link Request} class model parameters of API.
      * @param host    The host name of the SDK link to {@link Supplier}.
      * @param <R>     Data Generics for {@link Response}.
@@ -33,14 +34,18 @@ public final class ClientExecutors extends ClientAccess {
     }
 
     /**
-     * The static method executed by SDK is executed through the host name and {@link Request} parameters.
-     *
+     * The static method executed by SDK is executed through the
+     * host name and {@link Request} parameters.
      * @param request {@link Request} class model parameters of API.
      * @param host    The host name of the SDK.
      * @param <R>     Data Generics for {@link Response}.
      * @return Returns a defined {@link Response} class object.
      */
     public static <R extends Response> R executeRequestClient(String host, Request<R> request) {
-        return getAndSetClient(request.getUrl(host), request).request();
+        try (Client<R> client = getAndSetClient(request.getUrl(host), request)) {
+            return client.request();
+        } catch (/*update since 3.0.0*/IOException e) {
+            throw new ClientRuntimeCloseException(e);
+        }
     }
 }
