9ebf65ea9249257fe1f0428c052eaf07a12040b9
zhangpengfei <929160069@qq.com>
2023-11-26 11:24:07 +0800

    --- update to many utils

---
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/client/ClientRuntimeCloseException.java
@@ -0,0 +1,16 @@
+package top.osjf.assembly.simplified.sdk.client;
+
+/**
+ * Possible issues that may arise when attempting to
+ * solve {@link Client}'s {@link java.io.Closeable}
+ * with {@link java.io.IOException} problem.
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 3.0.0
+ */
+public class ClientRuntimeCloseException extends RuntimeException {
+    private static final long serialVersionUID = 4730988924158862453L;
+
+    public ClientRuntimeCloseException(Throwable cause) {
+        super("Client close Io error .", cause);
+    }
+}
