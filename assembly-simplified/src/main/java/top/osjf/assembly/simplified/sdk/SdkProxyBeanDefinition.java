4558775eaf77b7d45c744ed19704a5e072df8a88
zhangpengfei <929160069@qq.com>
2023-11-27 09:31:21 +0800

    --- update to many utils

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/SdkProxyBeanDefinition.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/SdkProxyBeanDefinition.java
@@ -1,28 +1,33 @@
 package top.osjf.assembly.simplified.sdk;
 
-import top.osjf.assembly.util.annotation.NotNull;
+import top.osjf.assembly.simplified.sdk.annotation.EnableSdkProxyRegister;
+import top.osjf.assembly.simplified.sdk.annotation.Sdk;
+import top.osjf.assembly.simplified.sdk.client.ClientExecutors;
+import top.osjf.assembly.simplified.support.BeanDefinitionRegisterBeforeRefresh;
 import top.osjf.assembly.util.lang.StringUtils;
 
 import java.io.Serializable;
 
 /**
- * This class contains all the information about the composition of the SDK, including the host address,
- * the base of the Spring proxy bean implementation class, and the final parameter convergence point of
- * the JDK method dynamic proxy. It can be said to be a fusion point between the Spring framework and
- * our custom {@link top.osjf.assembly.simplified.sdk.client.Client} scheme.
+ * This class contains all the information about the composition of the SDK,
+ * including the host address, the base of the Spring proxy bean implementation
+ * class, and the final parameter convergence point of the JDK method dynamic
+ * proxy.
  *
- * <p>Here is an explanation of the main implementation idea: we first scan the interface classes wearing
- * {@link top.osjf.assembly.simplified.sdk.annotation.EnableSdkProxyRegister} in
- * {@link top.osjf.assembly.simplified.support.BeanDefinitionRegisterBeforeRefresh} through
- * {@link top.osjf.assembly.simplified.sdk.annotation.Sdk}'s switch annotations,
+ * <p>It can be said to be a fusion point between the Spring framework and
+ * our custom {@link top.osjf.assembly.simplified.sdk.client.Client} scheme.
+ * <p>Here is an explanation of the main implementation idea: we first scan the
+ * interface classes wearing {@link EnableSdkProxyRegister} in
+ * {@link BeanDefinitionRegisterBeforeRefresh} through{@link Sdk}'s switch annotations,
  * and then create dynamic implementation classes for these interfaces through {}.
  *
- * <p>At this point, the implementation class is given to the proxy objects created by the jdk dynamic
- * proxy, and the proxy objects are handed over to spring as the virtual implementation classes for these interfaces.
+ * <p>At this point, the implementation class is given to the proxy objects
+ * created by the jdk dynamic proxy, and the proxy objects are handed over
+ * to spring as the virtual implementation classes for these interfaces.
  *
- * <p>When these interfaces are called through the spring container, We will uniformly bring the parameters
- * to the proxy object and connect them to our {@link top.osjf.assembly.simplified.sdk.client.ClientExecutors}
- * processing through this class.
+ * <p>When these interfaces are called through the spring container, We will
+ * uniformly bring the parameters to the proxy object and connect them to our
+ * {@link ClientExecutors} processing through this class.
  * @param <T> The data type of the proxy class.
  * @author zpf
  * @since 1.1.0
@@ -33,18 +38,13 @@ public class SdkProxyBeanDefinition<T> extends AbstractSdkProxyInvoker<T> implem
 
     private String host;
 
-    /**
-     * Place the current request host address.
-     *
-     * @param host Host address no be {@literal null}.
-     */
+    @Override
     public void setHost(String host) {
         if (StringUtils.isBlank(host)) throw new IllegalArgumentException(
                 "The calling host address of the SDK proxy class cannot be empty");
         this.host = host;
     }
 
-    @NotNull
     @Override
     public String getHost() {
         return host;
