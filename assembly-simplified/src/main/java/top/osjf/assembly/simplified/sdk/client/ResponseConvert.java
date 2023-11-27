166278b5692e4ebebbb1ec92ee03c849f745ac83
zhangpengfei <929160069@qq.com>
2023-11-27 10:17:47 +0800

    --- update to many utils

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/client/ResponseConvert.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/client/ResponseConvert.java
@@ -6,6 +6,7 @@ import top.osjf.assembly.util.annotation.NotNull;
 
 /**
  * A custom processing scheme for response string conversion {@link Response}.
+ *
  * @param <R> Implement a unified response class data type.
  * @author zpf
  * @since 1.1.0
@@ -14,16 +15,18 @@ import top.osjf.assembly.util.annotation.NotNull;
 public interface ResponseConvert<R extends Response> {
 
     /**
-     * Convert the response type {@link Request} of the request class record to the specified {@link Response}
-     * using the following custom method.
-     * <p>
-     * You can customize the conversion method based on the pre defined return values of the API.
-     * <p>{@link top.osjf.assembly.simplified.sdk.http.AbstractHttpClient#convertToResponse(Request, String)}Default to
-     * JSON format conversion.</p>
+     * Convert the response type {@link Request} of the request
+     * class record to the specified {@link Response} using the
+     * following custom method.
+     * <p>You can customize the conversion method based on the
+     * predefined return values of the API.
+     * <p>{@code top.osjf.assembly.simplified.sdk.http.AbstractHttpClient
+     * #convertToResponse(Request, String)}Default to json format conversion.
      *
      * @param request     {@link Request} class model parameters of API.
      * @param responseStr String data for API response.
-     * @return The converted response model data is implemented in {@link Response} and cannot be {@literal null}.
+     * @return The converted response model data is implemented in
+     * {@link Response} and cannot be {@literal null}.
      */
     @NotNull
     R convertToResponse(Request<R> request, String responseStr);
