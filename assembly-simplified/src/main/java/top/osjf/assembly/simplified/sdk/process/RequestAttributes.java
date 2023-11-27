b74e4ae15916a713425f172da34544c4d2eac9c9
zhangpengfei <929160069@qq.com>
2023-11-27 09:31:33 +0800

    --- update to many utils

---
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/process/RequestAttributes.java
@@ -0,0 +1,25 @@
+package top.osjf.assembly.simplified.sdk.process;
+
+/**
+ * Regarding the definition property of {@link Request},
+ * the aware interface needs to be implemented to set and
+ * obtain the corresponding request properties.
+ * @see top.osjf.assembly.simplified.sdk.SdkProxyBeanDefinition
+ * @author zpf
+ * @since 1.1.0
+ */
+public interface RequestAttributes {
+
+    /**
+     * Set the host address required for the SDK request
+     * domain, which can be a domain name or an IP address
+     * containing a good port.
+     * @param host Given request a host address.
+     */
+    void setHost(String host);
+
+    /**
+     * @return A request builder within a host.
+     */
+    String getHost();
+}
