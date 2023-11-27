d000f528a0652cc85aefed87092a8b30243f17e5
zhangpengfei <929160069@qq.com>
2023-11-27 09:37:09 +0800

    --- update to many utils

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/process/RequestParamCapable.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/process/RequestParamCapable.java
@@ -1,11 +1,10 @@
 package top.osjf.assembly.simplified.sdk.process;
 
-
-import org.springframework.lang.Nullable;
+import top.osjf.assembly.util.annotation.CanNull;
 
 /**
- * Request parameter acquisition interface, uniformly return {@link Object},
- * compatible with all return values.
+ * Request parameter acquisition interface, uniformly
+ * return {@link Object},compatible with all return values.
  *
  * @author zpf
  * @since 1.1.0
@@ -15,6 +14,6 @@ public interface RequestParamCapable {
     /**
      * @return Return any of {@link Object}.
      */
-    @Nullable
+    @CanNull
     Object getRequestParam();
 }
