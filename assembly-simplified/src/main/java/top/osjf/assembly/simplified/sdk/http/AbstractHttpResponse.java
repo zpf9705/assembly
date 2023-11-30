c69dc09c159f2d1c34b787d28de4152e9f1ea44b
zhangpengfei <929160069@qq.com>
2023-11-30 16:10:24 +0800

    --- update to many utils

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/http/AbstractHttpResponse.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/http/AbstractHttpResponse.java
@@ -1,7 +1,10 @@
 package top.osjf.assembly.simplified.sdk.http;
 
+import org.apache.http.HttpStatus;
 import top.osjf.assembly.simplified.sdk.process.AbstractResponse;
 import top.osjf.assembly.simplified.sdk.process.DefaultErrorResponse;
+
+import java.util.Objects;
 
 /**
  * Http response abstract node class, used to define common states,
@@ -41,4 +44,29 @@ import top.osjf.assembly.simplified.sdk.process.DefaultErrorResponse;
  */
 @SuppressWarnings("serial")
 public abstract class AbstractHttpResponse extends AbstractResponse implements HttpResponse {
+
+    //update for 2.1.2
+    //.......
+
+    public static final Integer SUCCESS_CODE = HttpStatus.SC_OK;
+
+    public static final Integer FAILED_CODE = HttpStatus.SC_INTERNAL_SERVER_ERROR;
+
+    public static final String SUCCESS_MESSAGE = "Congratulations";
+
+    public static final String FAILED_MESSAGE = "Internal system error";
+
+    @Override
+    public boolean isSuccess() {
+        return Objects.equals(getCode(), SUCCESS_CODE);
+    }
+
+    @Override
+    public String getMessage() {
+        return SUCCESS_MESSAGE;
+    }
+
+    public Integer getCode() {
+        return SUCCESS_CODE;
+    }
 }
