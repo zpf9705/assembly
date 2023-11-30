4c34c3f4bab16c67528cd714dca6bb4a005d0b2a
zhangpengfei <929160069@qq.com>
2023-11-30 13:50:05 +0800

    --- update to many utils

---
+++ assembly-cache/src/main/java/top/osjf/assembly/cache/listener/expiringmap/DefaultExpiringmapExpirationListener.java
@@ -0,0 +1,25 @@
+package top.osjf.assembly.cache.listener.expiringmap;
+
+import net.jodah.expiringmap.ExpirationListener;
+import top.osjf.assembly.cache.listener.AbstractExpirationHandler;
+import top.osjf.assembly.cache.listener.DefaultMessage;
+import top.osjf.assembly.util.data.ByteIdentify;
+
+/**
+ * The default expiration synchronization listener for {@link net.jodah.expiringmap.ExpiringMap}.
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.8
+ */
+public class DefaultExpiringmapExpirationListener extends AbstractExpirationHandler implements
+        ExpirationListener<ByteIdentify, ByteIdentify> {
+
+    public static final DefaultExpiringmapExpirationListener LISTENER = new DefaultExpiringmapExpirationListener();
+
+    private DefaultExpiringmapExpirationListener() {
+    }
+
+    @Override
+    public void expired(ByteIdentify key, ByteIdentify value) {
+        expiredMessageCapable(new DefaultMessage(key.getData(), value.getData()));
+    }
+}
