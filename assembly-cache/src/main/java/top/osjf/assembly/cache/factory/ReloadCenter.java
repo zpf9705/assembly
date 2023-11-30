0ee905b670b839a1865fcb7a30c16c16a1258830
zhangpengfei <929160069@qq.com>
2023-11-30 13:54:02 +0800

    --- update to many utils

--- assembly-cache/src/main/java/top/osjf/assembly/cache/factory/ReloadCenter.java
+++ assembly-cache/src/main/java/top/osjf/assembly/cache/factory/ReloadCenter.java
@@ -1,13 +1,14 @@
 package top.osjf.assembly.cache.factory;
 
-import top.osjf.assembly.cache.listener.MessageCapable;
+import top.osjf.assembly.cache.listener.ByteMessage;
 import top.osjf.assembly.cache.persistence.Entry;
 import top.osjf.assembly.util.annotation.NotNull;
 
 import java.util.concurrent.TimeUnit;
 
 /**
- * Mainly applied to restart will persist the value of the recovery by using the method of the interface.
+ * Mainly applied to restart will persist the value of the
+ * recovery by using the method of the interface.
  * @param <K> The type of key.
  * @param <V> The type of value.
  * @author zpf
@@ -23,14 +24,12 @@ public interface ReloadCenter<K, V> {
      * @param duration must not be {@literal null}.
      * @param unit     must not be {@literal null}.
      */
-
     void reload(@NotNull K key, @NotNull V value, @NotNull Long duration, @NotNull TimeUnit unit);
 
 
     /**
      * Remove expired keys of auxiliary elements.
-     *
-     * @param capable must not be {@literal null}.
+     * @param message must not be {@literal null}.
      */
-    void cleanSupportingElements(@NotNull MessageCapable capable);
+    void cleanSupportingElements(@NotNull ByteMessage message);
 }
