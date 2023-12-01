b51875e4c97ee93105b8c9bdc3a819a424bd727b
zhangpengfei <929160069@qq.com>
2023-12-01 12:53:35 +0800

    --- update to many utils

--- assembly-util/src/main/java/top/osjf/assembly/util/json/FastJsonUtils.java
+++ assembly-util/src/main/java/top/osjf/assembly/util/json/FastJsonUtils.java
@@ -1,8 +1,10 @@
 package top.osjf.assembly.util.json;
 
-import com.alibaba.fastjson.*;
+import com.alibaba.fastjson2.*;
 import top.osjf.assembly.util.lang.StringUtils;
 
+import java.lang.reflect.Type;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -11,16 +13,72 @@ import java.util.Map;
  * @author zpf
  * @since 1.0.4
  */
-public final class FastJsonUtils extends JSON {
+public final class FastJsonUtils {
 
     private FastJsonUtils() {
+    }
+
+    public static <T> String toJSONString(T obj) {
+        return JSON.toJSONString(obj);
+    }
+
+    public static JSONObject parseObject(String jsonStr) {
+        return JSON.parseObject(jsonStr);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T parseObject(String jsonStr, Object requiredType) {
+        if (requiredType == null) {
+            return null;
+        }
+        T result;
+        if (requiredType instanceof Class) {
+            result = parseObject(jsonStr, (Class<T>) requiredType);
+        } else if (requiredType instanceof Type) {
+            result = parseObject(jsonStr, (Type) requiredType);
+        } else {
+            result = null;
+        }
+        return result;
+    }
+
+    public static <T> T parseObject(String jsonStr, Class<T> requiredType) {
+        return JSON.parseObject(jsonStr, requiredType);
+    }
+
+    public static <T> T parseObject(String jsonStr, Type requiredType) {
+        return JSON.parseObject(jsonStr, requiredType);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> List<T> parseArray(String jsonStr, Object requiredType) {
+        if (requiredType == null) {
+            return null;
+        }
+        List<T> result;
+        if (requiredType instanceof Class) {
+            result = parseArray(jsonStr, (Class<T>) requiredType);
+        } else if (requiredType instanceof Type) {
+            result = parseArray(jsonStr, (Type) requiredType);
+        } else {
+            result = null;
+        }
+        return result;
+    }
+
+    public static <T> List<T> parseArray(String jsonStr, Class<T> requiredType) {
+        return JSON.parseArray(jsonStr, requiredType);
+    }
+
+    public static <T> List<T> parseArray(String jsonStr, Type requiredType) {
+        return JSON.parseArray(jsonStr, requiredType);
     }
 
     public static class TypeReferences<T> extends TypeReference<T> {
     }
 
     //Standard JSON object format or JSON array format.
-    public static boolean effectiveDataStructure(String jsonStr) {
+    public static boolean isValidObjectOrArray(String jsonStr) {
         if (StringUtils.isBlank(jsonStr)) {
             return false;
         }
@@ -29,80 +87,34 @@ public final class FastJsonUtils extends JSON {
 
     //The standard JSON object format does not include values.
     public static boolean isValidObject(String jsonStr) {
-        if (StringUtils.isBlank(jsonStr)) {
-            return false;
-        }
-        try {
-            JSONObject.parseObject(jsonStr);
-        } catch (Exception e) {
-            return false;
-        }
-        return true;
+        return JSON.isValidObject(jsonStr);
     }
 
     //Standard JSON array format.
     public static boolean isValidArray(String jsonStr) {
-        if (StringUtils.isBlank(jsonStr)) {
-            return false;
-        }
-        try {
-            JSONArray.parseArray(jsonStr);
-        } catch (Exception e) {
-            return false;
-        }
-        return true;
+        return JSON.isValidArray(jsonStr);
     }
 
     public static <T> T toObject(String str, TypeReferences<T> references) {
         return JSON.parseObject(str, references);
     }
 
+    public static <T> T toEmptyObj(Object type) {
+        return parseObject("{}", type);
+    }
+
     public static <T> T toEmptyObj(Class<T> clazz) {
         return JSON.parseObject("{}", clazz);
     }
 
-    public static Map<String, Object> getInnerMapByJsonStr(String str) {
-        if (isValidObject(str)) {
-            return parseObject(str).getInnerMap();
+    public static <T> T toEmptyObj(Type type) {
+        return JSON.parseObject("{}", type);
+    }
+
+    public static Map<String, Object> getInnerMapByJsonStr(String jsonStr) {
+        if (isValidObject(jsonStr)) {
+            return parseObject(jsonStr);
         }
         return null;
-    }
-
-    public static JSONObject toJSONObject(Object arg) {
-        if (arg == null) return null;
-        JSONObject obj;
-        if (arg instanceof JSONObject) {
-            obj = (JSONObject) arg;
-        } else {
-            if (arg instanceof String) {
-                if (isValidObject((String) arg)) {
-                    obj = parseObject((String) arg);
-                } else {
-                    throw new JSONException("Not a valid json String");
-                }
-            } else {
-                obj = parseObject(toJSONString(arg));
-            }
-        }
-        return obj;
-    }
-
-    public static <T> T toObject(Object pojo, Class<T> clazz) {
-        String jsonStr;
-        if (pojo instanceof String) {
-            if (isValidObject((String) pojo)) {
-                jsonStr = (String) pojo;
-            } else {
-                throw new JSONException("Not a valid json String");
-            }
-        } else {
-            String string = pojo.toString();
-            if (isValidObject(string)) {
-                jsonStr = string;
-            } else {
-                jsonStr = toJSONString(pojo);
-            }
-        }
-        return parseObject(jsonStr, clazz);
     }
 }
