c7c1c6716b9bd39422642a87ef1f29559e557912
zhangpengfei <929160069@qq.com>
2023-12-01 09:50:07 +0800

    --- update to many utils

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/process/Request.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/process/Request.java
@@ -1,7 +1,11 @@
 package top.osjf.assembly.simplified.sdk.process;
 
+import com.google.common.reflect.TypeToken;
 import top.osjf.assembly.simplified.sdk.SdkException;
+import top.osjf.assembly.simplified.sdk.client.AbstractClient;
 import top.osjf.assembly.simplified.sdk.client.Client;
+import top.osjf.assembly.simplified.sdk.http.ApacheHttpClient;
+import top.osjf.assembly.simplified.sdk.http.HttpResultResponse;
 import top.osjf.assembly.util.annotation.CanNull;
 
 import java.io.Serializable;
@@ -26,7 +30,7 @@ import java.util.Map;
  * @author zpf
  * @since 1.1.0
  */
-public interface Request<R extends Response> extends RequestParamCapable, Serializable {
+public interface Request<R extends Response> extends RequestParamCapable<Object>, Serializable {
 
     /**
      * Obtain the true address value of the request based on the given host name.
@@ -42,19 +46,43 @@ public interface Request<R extends Response> extends RequestParamCapable, Serial
 
     /**
      * Method for verifying request parameters, fixed throw {@link SdkException}.
-     * <p>Taking {@link top.osjf.assembly.simplified.sdk.http.ApacheHttpClient} as an example, you can take
-     * a look at its {@link top.osjf.assembly.simplified.sdk.http.ApacheHttpClient#request()} method. You
-     * need to throw {@link SdkException} for validation parameters in order to perform specialized
-     * exception capture.</p>
+     * <p>Taking {@link ApacheHttpClient} as an example, you can take
+     * a look at its {@link ApacheHttpClient#request()} method. You
+     * need to throw {@link SdkException} for validation parameters in
+     * order to perform specialized exception capture.</p>
      */
     void validate() throws SdkException;
 
     /**
-     * Obtain the class object of the response transformation entity, implemented in {@link Response}.
-     *
+     * Obtain the class object of the response transformation entity,
+     * implemented in {@link Response}.
+     * <p>If you need a {@link Response} transformation of composite
+     * generics, please refer to {@link #getResponseTypeToken()}.
      * @return Return implementation for {@link Response}.
      */
-    Class<R> getResponseCls();
+    @CanNull
+    default Class<R> getResponseCls(){
+        return null;
+    }
+
+    /**
+     * Introduced a generic help class from Google to solve the problem
+     * of composite generics.
+     * <p>Calling {@link TypeToken#getType()} can obtain the {@link java.lang.reflect.Type}
+     * of composite generics and perform subsequent conversion operations.
+     * <p>He can help you no longer need to define the corresponding class,
+     * you just need to define the corresponding data based on {@link HttpResultResponse},
+     * but the specific state set must comply with {@link HttpResultResponse}'s
+     * regulations.
+     * <p>If you need to customize {@link Response}, it will be more convenient
+     * for you to use {@link #getResponseCls()}.
+     * @return Composite paradigm.
+     * @since 2.1.2
+     */
+    default TypeToken<R> getResponseTypeToken() {
+        return new TypeToken<R>() {
+        };
+    }
 
     /**
      * Obtain the header information that needs to be set, and return a map.
@@ -64,11 +92,10 @@ public interface Request<R extends Response> extends RequestParamCapable, Serial
     Map<String, String> getHeadMap();
 
     /**
-     * Obtain the implementation class object of {@link Client} and you can define it yourself.
-     * <p>
-     * Currently, there are {@link top.osjf.assembly.simplified.sdk.http.ApacheHttpClient} in HTTP format and
-     * some default methods provided in {@link top.osjf.assembly.simplified.sdk.client.AbstractClient}.
-     *
+     * Obtain the implementation class object of {@link Client} and you
+     * can define it yourself.
+     * <p>Currently, there are {@link ApacheHttpClient} in HTTP format and
+     * some default methods provided in {@link AbstractClient}.
      * @return Implementation class of {@link Client}.
      */
     @SuppressWarnings("rawtypes")
