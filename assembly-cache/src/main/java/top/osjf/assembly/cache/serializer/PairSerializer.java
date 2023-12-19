b179395e4ebf39f8a798f0b2ca9c490b5a0eaec9
zhangpengfei <929160069@qq.com>
2023-12-19 14:50:48 +0800

    --- update to many utils

--- assembly-cache/src/main/java/top/osjf/assembly/cache/serializer/PairSerializer.java
+++ assembly-cache/src/main/java/top/osjf/assembly/cache/serializer/PairSerializer.java
@@ -1,37 +1,45 @@
 package top.osjf.assembly.cache.serializer;
 
+import java.io.Serializable;
+
 /**
- * The serialization method interface is mainly a method
- * definition for byte array serialization and deserialization,
- * with a generic definition being the converted object type after
- * deserialization.
+ * A method should be developed to serialize all cache key values into
+ * byte arrays for data consistency to ensure a unique container.
+ *
+ * <p>Therefore, serialization and deserialization methods need to be
+ * provided.
+ *
+ * <p>In this interface, {@link #serialize(Object)} and {@link
+ * #deserialize(byte[])} methods should be developed to solve the
+ * above problems, and the class types for obtaining and deserializing
+ * can also be selected.
  *
  * @param <T> Object type.
  * @author zpf
  * @since 1.0.0
  **/
-public interface PairSerializer<T> {
+public interface PairSerializer<T> extends Serializable {
 
     /**
-     * Serialize object to byte array.
+     * Returns a serialized byte array.
      *
-     * @param t Serialize target object
-     * @return no be {@literal  null}
+     * @param obj The object value to be serialized.
+     * @return serialized byte array.
      */
-    byte[] serialize(T t);
+    byte[] serialize(T obj);
 
     /**
-     * Deserialize byte group numbers into real objects.
+     * Returns an object entity deserialized from a byte array.
      *
-     * @param bytes byte array
-     * @return {@link Object}
+     * @param bytes The byte array to be deserialized.
+     * @return entity deserialized from a byte array.
      */
     T deserialize(byte[] bytes);
 
     /**
-     * Obtain a class object for deserializing generics
+     * Returns a type object of the deserialized object.
      *
-     * @return Target class
+     * @return a type object of the deserialized object.
      */
     Class<T> serializerType();
 }
