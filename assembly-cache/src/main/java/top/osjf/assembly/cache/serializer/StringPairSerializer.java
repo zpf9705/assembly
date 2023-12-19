7b716974d8269a37ec0f0d87d303c548d35852cb
zhangpengfei <929160069@qq.com>
2023-12-19 14:47:24 +0800

    --- update to many utils

--- assembly-cache/src/main/java/top/osjf/assembly/cache/serializer/StringPairSerializer.java
+++ assembly-cache/src/main/java/top/osjf/assembly/cache/serializer/StringPairSerializer.java
@@ -8,6 +8,8 @@ package top.osjf.assembly.cache.serializer;
  **/
 public class StringPairSerializer extends SerializerAdapter<String> {
 
+    private static final long serialVersionUID = -4700820192449082314L;
+
     public StringPairSerializer() {
         super(String.class);
     }
