206f2d1de6b1ece0b244df11ff830297d10f1fd1
zhangpengfei <929160069@qq.com>
2023-12-19 14:47:58 +0800

    --- update to many utils

--- assembly-cache/src/main/java/top/osjf/assembly/cache/serializer/SerializerAdapter.java
+++ assembly-cache/src/main/java/top/osjf/assembly/cache/serializer/SerializerAdapter.java
@@ -5,11 +5,14 @@ import top.osjf.assembly.util.serial.SerialUtils;
 
 /**
  * Key/value pairs serialized adapter , direct implementation {@link PairSerializer}.
+ * @see SerialUtils
  * @param <T> Object type.
  * @author zpf
  * @since 1.0.0
  **/
 public class SerializerAdapter<T> implements PairSerializer<T> {
+
+    private static final long serialVersionUID = 1275598508990368957L;
 
     private final Class<T> type;
 
