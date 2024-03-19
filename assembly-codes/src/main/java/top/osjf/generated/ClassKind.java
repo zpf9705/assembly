65857b063b521c00dfc29ceebd9760b608939366
zhangpengfei <929160069@qq.com>
2024-03-19 09:03:24 +0800

    --- update to many utils

--- assembly-codes/src/main/java/top/osjf/generated/ClassKind.java
+++ assembly-codes/src/main/java/top/osjf/generated/ClassKind.java
@@ -1,6 +1,8 @@
 package top.osjf.generated;
 
 /**
+ * Provide keyword descriptions for generating types, including
+ * Java interfaces, enumerations, regular classes, and abstract classes.
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.1.0
  */
