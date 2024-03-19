9c61d1ffc4bf0a739dc03e1ff18d22e66541c6c6
zhangpengfei <929160069@qq.com>
2024-03-19 16:50:34 +0800

    --- update to many utils

--- assembly-codes/src/main/java/top/osjf/generated/mybatisplus/MybatisPlusCodeGenerateInvocation.java
+++ assembly-codes/src/main/java/top/osjf/generated/mybatisplus/MybatisPlusCodeGenerateInvocation.java
@@ -3,12 +3,21 @@ package top.osjf.generated.mybatisplus;
 import top.osjf.generated.CodeGenerateInvocation;
 
 /**
+ * Method extension for mybatis plus framework's special support.
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.1.0
  */
 public interface MybatisPlusCodeGenerateInvocation extends CodeGenerateInvocation {
 
+    /**
+     * @see MybatisPlusCodeGenerate#join()
+     * @return MybatisPlusCodeGenerate#join()
+     */
     boolean getJoin();
 
+    /**
+     * @see MybatisPlusCodeGenerate#tableChineseName()
+     * @return MybatisPlusCodeGenerate#tableChineseName()
+     */
     String getTableChineseName();
 }
