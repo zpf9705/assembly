cda70b695765b91915fbf678ad9b4bc030f3d57f
zhangpengfei <929160069@qq.com>
2024-03-19 16:13:41 +0800

    --- update to many utils

--- assembly-codes/src/main/java/top/osjf/generated/mybatisplus/MapperCodeGenerateInvocationImpl.java
+++ assembly-codes/src/main/java/top/osjf/generated/mybatisplus/MapperCodeGenerateInvocationImpl.java
@@ -3,7 +3,6 @@ package top.osjf.generated.mybatisplus;
 import top.osjf.assembly.util.annotation.NotNull;
 import top.osjf.generated.ClassKind;
 import top.osjf.generated.GeneratedCodeAppenderBuilder;
-import top.osjf.generated.impl.annotation.AbstractCodeGenerateInvocation;
 
 /**
  * Generate the necessary mapper interface for accessing database tables
@@ -11,12 +10,14 @@ import top.osjf.generated.impl.annotation.AbstractCodeGenerateInvocation;
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.1.0
  */
-public class MapperCodeGenerateInvocationImpl extends AbstractCodeGenerateInvocation {
+public class MapperCodeGenerateInvocationImpl extends AbstractMybatisPlusCodeGenerateInvocation {
 
     public static final String MYBATIS_PLUS_BASE_MAPPER = "com.baomidou.mybatisplus.core.mapper.BaseMapper";
+    public static final String MYBATIS_PLUS_JOIN_BASE_MAPPER = "com.github.yulichang.base.MPJBaseMapper";
 
-    public MapperCodeGenerateInvocationImpl(String simpleName, String packageName, String targetName) {
-        super(simpleName, packageName, targetName);
+    public MapperCodeGenerateInvocationImpl(String simpleName, String packageName, String targetName,
+                                            boolean join, String tableChineseName) {
+        super(simpleName, packageName, targetName, join, tableChineseName);
     }
 
     @NotNull
@@ -27,7 +28,7 @@ public class MapperCodeGenerateInvocationImpl extends AbstractCodeGenerateInvoca
 
         return builder
                 .classKind(ClassKind.INTERFACE)
-                .extend(MYBATIS_PLUS_BASE_MAPPER)
+                .extend(getJoin() ? MYBATIS_PLUS_JOIN_BASE_MAPPER : MYBATIS_PLUS_BASE_MAPPER)
                 .extendGenerics(new String[]{getTargetName()});
     }
 }
