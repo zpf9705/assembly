2728492678be6aec637da06129cbc99a9657cf80
zhangpengfei <929160069@qq.com>
2024-03-19 09:15:20 +0800

    --- update to many utils

---
+++ assembly-codes/src/main/java/top/osjf/generated/mybatisplus/ServiceImplCodeGenerateInvocationImpl.java
@@ -0,0 +1,67 @@
+package top.osjf.generated.mybatisplus;
+
+import top.osjf.assembly.util.annotation.NotNull;
+import top.osjf.assembly.util.lang.MapUtils;
+import top.osjf.generated.ClassKind;
+import top.osjf.generated.CodeGenerateInvocation;
+import top.osjf.generated.GeneratedCodeAppenderBuilder;
+import top.osjf.generated.impl.annotation.AbstractCodeGenerateInvocation;
+
+/**
+ * Generate the necessary service implementation class for accessing
+ * database tables using the mybatis plus framework.
+ * To modify the class, specify the corresponding mapper interface and
+ * service interface, integrate Spring, and add Spring's service annotations.
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.1.0
+ */
+public class ServiceImplCodeGenerateInvocationImpl extends AbstractCodeGenerateInvocation implements
+        ServiceImplCodeGenerateInvocation {
+
+    public static final String MYBATIS_PLUS_BASE_SERVICE_IMPL =
+            "com.baomidou.mybatisplus.extension.service.impl.ServiceImpl";
+
+    public static final String SPRING_SERVICE_ANNOTATION = "org.springframework.stereotype.Service";
+
+    private CodeGenerateInvocation mapper;
+
+    private CodeGenerateInvocation service;
+
+
+    public ServiceImplCodeGenerateInvocationImpl(String simpleName, String packageName, String targetName) {
+        super(simpleName, packageName, targetName);
+    }
+
+    @Override
+    public ServiceImplCodeGenerateInvocationImpl mapper(CodeGenerateInvocation codeGenerateInvocation) {
+        this.mapper = codeGenerateInvocation;
+        return this;
+    }
+
+    @Override
+    public ServiceImplCodeGenerateInvocationImpl service(CodeGenerateInvocation codeGenerateInvocation) {
+        this.service = codeGenerateInvocation;
+        return this;
+    }
+
+    @NotNull
+    @Override
+    protected GeneratedCodeAppenderBuilder getGeneratedCodeAppenderBuilder() {
+
+        GeneratedCodeAppenderBuilder builder = super.getGeneratedCodeAppenderBuilder();
+
+        builder.classKind(ClassKind.CLASS)
+                .extend(MYBATIS_PLUS_BASE_SERVICE_IMPL)
+                .annotations(MapUtils.of(SPRING_SERVICE_ANNOTATION, ""));
+
+        if (mapper != null) {
+            builder.extendGenerics(new String[]{mapper.getName(), getTargetName()});
+        }
+
+        if (service != null) {
+            builder.interfaces(MapUtils.of(service.getName(), new String[]{}));
+        }
+
+        return builder;
+    }
+}
