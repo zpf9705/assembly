efc0edadb207a26a9767957a7155150326c7c9b8
zhangpengfei <929160069@qq.com>
2024-03-20 10:27:17 +0800

    --- update to many utils

--- assembly-codes/src/main/java/top/osjf/generated/mybatisplus/MybatisPlusCodeGenerateProcessor.java
+++ assembly-codes/src/main/java/top/osjf/generated/mybatisplus/MybatisPlusCodeGenerateProcessor.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import top.osjf.assembly.util.lang.ReflectUtils;
 import top.osjf.assembly.util.lang.StringUtils;
 import top.osjf.generated.AbstractSmartProcessor;
-import top.osjf.generated.CodeGenerateInvocation;
 import top.osjf.generated.GeneratedUtils;
 
 import javax.annotation.processing.Filer;
@@ -38,15 +37,18 @@ public class MybatisPlusCodeGenerateProcessor extends AbstractSmartProcessor {
 
         TypeElement typeElement = (TypeElement) element;
 
-        CodeGenerateInvocation mapperInvocation = getMapperNameMetadata(typeElement, annotation, commonPackage);
+        MybatisPlusCodeGenerateInvocation mapperInvocation = getMapperNameMetadata(typeElement, annotation,
+                commonPackage);
 
         if (mapperInvocation != null) mapperInvocation.write(filer, this);
 
-        CodeGenerateInvocation serviceInvocation = getServiceNameMetadata(typeElement, annotation, commonPackage);
+        MybatisPlusCodeGenerateInvocation serviceInvocation = getServiceNameMetadata(typeElement, annotation,
+                commonPackage);
 
         if (serviceInvocation != null) serviceInvocation.write(filer, this);
 
-        CodeGenerateInvocation serviceImplInvocation = getServiceImplNameMetadata(typeElement, annotation, commonPackage);
+        MybatisPlusCodeGenerateInvocation serviceImplInvocation = getServiceImplNameMetadata(typeElement, annotation,
+                commonPackage);
 
         if (serviceImplInvocation != null) ((ServiceImplCodeGenerateInvocation) serviceImplInvocation)
                 .mapper(mapperInvocation).service(serviceInvocation).write(filer, this);
@@ -57,8 +59,9 @@ public class MybatisPlusCodeGenerateProcessor extends AbstractSmartProcessor {
         return element.getAnnotation(TableName.class) != null && element instanceof TypeElement;
     }
 
-    private CodeGenerateInvocation getMapperNameMetadata(TypeElement element, MybatisPlusCodeGenerate codeGenerate,
-                                                         String commonPackage) {
+    private MybatisPlusCodeGenerateInvocation getMapperNameMetadata(TypeElement element,
+                                                                    MybatisPlusCodeGenerate codeGenerate,
+                                                                    String commonPackage) {
         return getCodeGenerateInvocation(element, codeGenerate.mapperGeneratePackage(),
                 MybatisPlusCodeGenerate.DEFAULT_MAPPER_PACKAGE_SUFFIX_NAME,
                 codeGenerate.mapperSuffixName(),
@@ -69,8 +72,9 @@ public class MybatisPlusCodeGenerateProcessor extends AbstractSmartProcessor {
                 codeGenerate.tableChineseName());
     }
 
-    private CodeGenerateInvocation getServiceNameMetadata(TypeElement element, MybatisPlusCodeGenerate codeGenerate,
-                                                          String commonPackage) {
+    private MybatisPlusCodeGenerateInvocation getServiceNameMetadata(TypeElement element,
+                                                                     MybatisPlusCodeGenerate codeGenerate,
+                                                                     String commonPackage) {
         return getCodeGenerateInvocation(element, codeGenerate.serviceGeneratePackage(),
                 MybatisPlusCodeGenerate.DEFAULT_SERVICE_PACKAGE_SUFFIX_NAME,
                 codeGenerate.serviceSuffixName(),
@@ -81,8 +85,9 @@ public class MybatisPlusCodeGenerateProcessor extends AbstractSmartProcessor {
                 codeGenerate.tableChineseName());
     }
 
-    private CodeGenerateInvocation getServiceImplNameMetadata(TypeElement element, MybatisPlusCodeGenerate codeGenerate,
-                                                              String commonPackage) {
+    private MybatisPlusCodeGenerateInvocation getServiceImplNameMetadata(TypeElement element,
+                                                                         MybatisPlusCodeGenerate codeGenerate,
+                                                                         String commonPackage) {
         return getCodeGenerateInvocation(element, codeGenerate.serviceImplGeneratePackage(),
                 MybatisPlusCodeGenerate.DEFAULT_SERVICE_IMPL_PACKAGE_SUFFIX_NAME,
                 codeGenerate.serviceImplSuffixName(),
@@ -93,13 +98,13 @@ public class MybatisPlusCodeGenerateProcessor extends AbstractSmartProcessor {
                 codeGenerate.tableChineseName());
     }
 
-    private CodeGenerateInvocation getCodeGenerateInvocation(TypeElement element, String appointPackage,
-                                                             String defaultSuffixPackageName, String suffixName,
-                                                             String commonPackage,
-                                                             boolean noProviderPackageUseDefault,
-                                                             Class<? extends CodeGenerateInvocation> clazz,
-                                                             boolean join,
-                                                             String tableChineseName) {
+    private MybatisPlusCodeGenerateInvocation getCodeGenerateInvocation(TypeElement element, String appointPackage,
+                                                                        String defaultSuffixPackageName, String suffixName,
+                                                                        String commonPackage,
+                                                                        boolean noProviderPackageUseDefault,
+                                                                        Class<? extends MybatisPlusCodeGenerateInvocation> clazz,
+                                                                        boolean join,
+                                                                        String tableChineseName) {
         if (StringUtils.isBlank(appointPackage)) {
             appointPackage = commonPackage;
             if (StringUtils.isBlank(appointPackage)) {
