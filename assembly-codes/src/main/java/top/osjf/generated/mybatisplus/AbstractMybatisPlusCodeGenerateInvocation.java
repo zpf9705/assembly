6b91a8abc3a59e38a61ced28824f469785fac083
zhangpengfei <929160069@qq.com>
2024-03-21 11:23:26 +0800

    --- update to many utils

--- assembly-codes/src/main/java/top/osjf/generated/mybatisplus/AbstractMybatisPlusCodeGenerateInvocation.java
+++ assembly-codes/src/main/java/top/osjf/generated/mybatisplus/AbstractMybatisPlusCodeGenerateInvocation.java
@@ -2,13 +2,21 @@ package top.osjf.generated.mybatisplus;
 
 import top.osjf.assembly.util.annotation.NotNull;
 import top.osjf.assembly.util.lang.StringUtils;
-import top.osjf.generated.GeneratedCodeAppenderBuilder;
 import top.osjf.generated.AbstractCodeGenerateInvocation;
+import top.osjf.generated.GeneratedCodeAppenderBuilder;
+import top.osjf.generated.Logger;
+import top.osjf.generated.SystemPrintKind;
+
+import javax.annotation.processing.Filer;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
+import java.io.PrintWriter;
 
 /**
  * Abstract implementation for special attributes of the mybatis plus framework.
- * @see MybatisPlusCodeGenerate
+ *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @see MybatisPlusCodeGenerate
  * @since 1.1.0
  */
 public abstract class AbstractMybatisPlusCodeGenerateInvocation extends AbstractCodeGenerateInvocation
@@ -18,11 +26,26 @@ public abstract class AbstractMybatisPlusCodeGenerateInvocation extends Abstract
 
     private final String tableChineseName;
 
+    private final String suffixName;
+
+    //since 1.1.1
+    private static boolean create;
+
+    //since 1.1.1
+    private static PrintWriter printWriter;
+
+    //since 1.1.1
+    private static int implNum = 3;
+
+    //since 1.1.1
+    public static final String afterSourceFileName = "mybatisPlusCodeGenerate.properties";
+
     public AbstractMybatisPlusCodeGenerateInvocation(String simpleName, String packageName, String targetName,
-                                                     boolean join, String tableChineseName) {
+                                                     boolean join, String tableChineseName, String suffixName) {
         super(simpleName, packageName, targetName);
         this.join = join;
         this.tableChineseName = tableChineseName;
+        this.suffixName = suffixName;
     }
 
     @Override
@@ -35,18 +58,66 @@ public abstract class AbstractMybatisPlusCodeGenerateInvocation extends Abstract
         return tableChineseName;
     }
 
+    @Override
+    public String getSuffixName() {
+        return suffixName;
+    }
+
     @NotNull
     @Override
     protected GeneratedCodeAppenderBuilder getGeneratedCodeAppenderBuilder() {
-
         GeneratedCodeAppenderBuilder builder = super.getGeneratedCodeAppenderBuilder();
-
+        //When the provided table Chinese name is not empty, concatenate and annotate the
+        // description based on the table name with special expressions.
         if (StringUtils.isNotBlank(getTableChineseName())) {
             builder.description(getTableChineseName() + getCharacterization());
         }
-
         return builder;
     }
 
+    //since 1.1.1
+    @Override
+    public void write(Filer filer, Logger logger) {
+        //Write the necessary Java files first.
+        super.write(filer, logger);
+        //Create a file for the mybatis plus configuration project.
+        if (!create) {
+            try {
+                FileObject afterSourceFile = filer.createResource(StandardLocation.CLASS_OUTPUT, "", afterSourceFileName);
+                printWriter = new PrintWriter(afterSourceFile.openWriter());
+                create = true;
+            } catch (Exception e) {
+                logger.log(SystemPrintKind.OUT, "Error creating post file for mybatis plus:  {} {}",
+                        e.getClass().getName(), e.getMessage());
+            }
+        }
+        //Global write of files.
+        if (printWriter != null) {
+            try {
+                printWriter.write(getWritePrefix() + "=" + getSuffixName() + "\n");
+                implNum--;
+            } catch (Exception e) {
+                logger.log(SystemPrintKind.OUT, "Error writing content to afterSource File file::  {} {}",
+                        e.getClass().getName(), e.getMessage());
+            }
+            if (implNum == 0) {
+                printWriter.close();
+                printWriter = null;
+                logger.log(SystemPrintKind.OUT, "The file {} write stream has been closed.", afterSourceFileName);
+            }
+        }
+    }
+
+    /**
+     * Write the configuration item prefix to the temporary configuration file.
+     * @since  1.1.1
+     * @return the configuration item prefix to the temporary configuration file
+     */
+    protected abstract String getWritePrefix();
+
+    /**
+     * Returns a special description of the generated class.
+     * @return a special description of the generated class.
+     */
     protected abstract String getCharacterization();
 }
