0a049529074009a71dfb6a42489f07ed2651d2fa
zhangpengfei <929160069@qq.com>
2024-03-22 11:28:09 +0800

    --- update to many utils

--- assembly-codes/src/main/java/top/osjf/generated/mybatisplus/AbstractMybatisPlusCodeGenerateInvocation.java
+++ assembly-codes/src/main/java/top/osjf/generated/mybatisplus/AbstractMybatisPlusCodeGenerateInvocation.java
@@ -4,13 +4,9 @@ import top.osjf.assembly.util.annotation.NotNull;
 import top.osjf.assembly.util.lang.StringUtils;
 import top.osjf.generated.AbstractCodeGenerateInvocation;
 import top.osjf.generated.GeneratedCodeAppenderBuilder;
-import top.osjf.generated.Logger;
-import top.osjf.generated.SystemPrintKind;
 
-import javax.annotation.processing.Filer;
-import javax.tools.FileObject;
-import javax.tools.StandardLocation;
-import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Abstract implementation for special attributes of the mybatis plus framework.
@@ -27,18 +23,6 @@ public abstract class AbstractMybatisPlusCodeGenerateInvocation extends Abstract
     private final String tableChineseName;
 
     private final String suffixName;
-
-    //since 1.1.1
-    private static boolean create;
-
-    //since 1.1.1
-    private static PrintWriter printWriter;
-
-    //since 1.1.1
-    private static int implNum = 3;
-
-    //since 1.1.1
-    public static final String afterSourceFileName = "mybatisPlusCodeGenerate.properties";
 
     public AbstractMybatisPlusCodeGenerateInvocation(String simpleName, String packageName, String targetName,
                                                      boolean join, String tableChineseName, String suffixName) {
@@ -75,60 +59,30 @@ public abstract class AbstractMybatisPlusCodeGenerateInvocation extends Abstract
         return builder;
     }
 
-    //since 1.1.1
     @Override
-    public void write(Filer filer, Logger logger) {
-        //Write the necessary Java files first.
-        super.write(filer, logger);
-        //Create a file for the mybatis plus configuration project.
-        FileObject fileObject =
-                MybatisPlusCodeGeneratedUtils.getFileObject(filer, logger, StandardLocation.CLASS_OUTPUT, afterSourceFileName);
-        if (fileObject != null) {
-            try (PrintWriter writer = new PrintWriter(fileObject.openWriter())) {
-                writer.write(getTargetName() + "." + getWritePrefix() + "=" + getSuffixName() + "\n");
-            } catch (Exception e) {
-                logger.log(SystemPrintKind.OUT, "Error writing content to afterSource File file::  {} {}",
-                        e.getClass().getName(), e.getMessage());
+    public List<Configuration> getWriteConfiguration() {
+        return Collections.singletonList(new Configuration() {
+            @Override
+            public String getPrefix() {
+                return getSuffixNameConfigurationPrefix();
             }
-        }
-//        if (!create) {
-//            try {
-//                FileObject afterSourceFile = filer.createResource(StandardLocation.CLASS_OUTPUT, "", afterSourceFileName);
-//                printWriter = new PrintWriter(afterSourceFile.openWriter());
-//                create = true;
-//            } catch (Exception e) {
-//                logger.log(SystemPrintKind.OUT, "Error creating post file for mybatis plus:  {} {}",
-//                        e.getClass().getName(), e.getMessage());
-//            }
-//        }
-//        //Global write of files.
-//        if (printWriter != null) {
-//            try {
-//                printWriter.write(getWritePrefix() + "=" + getSuffixName() + "\n");
-//                implNum--;
-//            } catch (Exception e) {
-//                logger.log(SystemPrintKind.OUT, "Error writing content to afterSource File file::  {} {}",
-//                        e.getClass().getName(), e.getMessage());
-//            }
-//            if (implNum == 0) {
-//                printWriter.close();
-//                printWriter = null;
-//                logger.log(SystemPrintKind.OUT, "The file {} write stream has been closed.", afterSourceFileName);
-//            }
-//        }
+
+            @Override
+            public String getValue() {
+                return getSuffixName();
+            }
+        });
     }
 
     /**
-     * Write the configuration item prefix to the temporary configuration file.
-     *
-     * @return the configuration item prefix to the temporary configuration file
-     * @since 1.1.1
+     * Returns the write configuration prefix name for the generated class abbreviation suffix.
+     * @since 1.1.2
+     * @return the write configuration prefix name for the generated class abbreviation suffix.
      */
-    protected abstract String getWritePrefix();
+    protected abstract String getSuffixNameConfigurationPrefix();
 
     /**
      * Returns a special description of the generated class.
-     *
      * @return a special description of the generated class.
      */
     protected abstract String getCharacterization();
