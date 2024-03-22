4d93b68652fc71b94704c6723cdfca235d7944bf
zhangpengfei <929160069@qq.com>
2024-03-22 11:27:38 +0800

    --- update to many utils

--- assembly-codes/src/main/java/top/osjf/generated/mybatisplus/MybatisPlusCodeGenerateInvocation.java
+++ assembly-codes/src/main/java/top/osjf/generated/mybatisplus/MybatisPlusCodeGenerateInvocation.java
@@ -2,6 +2,8 @@ package top.osjf.generated.mybatisplus;
 
 import top.osjf.generated.CodeGenerateInvocation;
 
+import java.util.List;
+
 /**
  * Method extension for mybatis plus framework's special support.
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
@@ -10,23 +12,57 @@ import top.osjf.generated.CodeGenerateInvocation;
 public interface MybatisPlusCodeGenerateInvocation extends CodeGenerateInvocation {
 
     /**
-     * @see MybatisPlusCodeGenerate#join()
      * @return MybatisPlusCodeGenerate#join()
+     * @see MybatisPlusCodeGenerate#join()
      */
     boolean getJoin();
 
     /**
-     * @see MybatisPlusCodeGenerate#tableChineseName()
      * @return MybatisPlusCodeGenerate#tableChineseName()
+     * @see MybatisPlusCodeGenerate#tableChineseName()
      */
     String getTableChineseName();
 
     /**
+     * @return Type of suffixName.
      * @see MybatisPlusCodeGenerate#mapperSuffixName()
      * @see MybatisPlusCodeGenerate#serviceSuffixName()
      * @see MybatisPlusCodeGenerate#serviceImplSuffixName()
      * @since 1.1.1
-     * @return Type of suffixName.
      */
     String getSuffixName();
+
+    /**
+     * Return the set of configuration items that need to be written
+     * to the configuration file after generating the class.
+     *
+     * @return the configuration items after generating the class.
+     */
+    List<Configuration> getWriteConfiguration();
+
+    /**
+     * Write the encapsulation interface for the subsequent configuration.
+     */
+    interface Configuration {
+
+        /**
+         * The write prefix for the properties file.
+         * @return write prefix for the properties file.
+         */
+        String getPrefix();
+
+        /**
+         * The write value for the properties file.
+         * @return write value for the properties file.
+         */
+        String getValue();
+
+        /**
+         * The write rule “prefix=value” for the properties file.
+         * @return write rule “prefix=value” for the properties file.
+         */
+        default String getWriteLine() {
+            return getPrefix() + "=" + getValue() + "\n";
+        }
+    }
 }
