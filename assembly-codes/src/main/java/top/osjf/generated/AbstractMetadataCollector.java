4df4f44bd42168f00b5afd7ee5cfdd79831db6a3
zhangpengfei <929160069@qq.com>
2024-03-28 10:15:31 +0800

    --- update to many utils

--- assembly-codes/src/main/java/top/osjf/generated/AbstractMetadataCollector.java
+++ assembly-codes/src/main/java/top/osjf/generated/AbstractMetadataCollector.java
@@ -1,32 +1,37 @@
 package top.osjf.generated;
 
 import javax.annotation.processing.Filer;
-import javax.lang.model.element.TypeElement;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.Element;
 import java.lang.annotation.Annotation;
 
 /**
  * The abstract {@link MetadataCollector} provides a default constructor
  * and a common property field in which the final state method is called.
- * @see ProcessAble#run(ProcessAble, Logger)
+ *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @see ProcessAble#run(ProcessAble, Logger)
  * @since 1.1.3
  */
 public abstract class AbstractMetadataCollector<T extends Annotation> implements MetadataCollector<T> {
 
     private final T annotation;
 
-    private final TypeElement element;
+    private final RoundEnvironment roundEnvironment;
+
+    private final Element element;
 
     private final Filer filer;
 
     private final Logger logger;
 
-    public AbstractMetadataCollector(T annotation, TypeElement element, Filer filer, Logger logger) {
+    public AbstractMetadataCollector(T annotation, RoundEnvironment roundEnvironment,
+                                     Element element, Filer filer, Logger logger) {
         this.annotation = annotation;
+        this.roundEnvironment = roundEnvironment;
         this.element = element;
         this.filer = filer;
         this.logger = logger;
-        ProcessAble.run(this, logger);
     }
 
     @Override
@@ -35,8 +40,14 @@ public abstract class AbstractMetadataCollector<T extends Annotation> implements
     }
 
     @Override
-    public TypeElement getTypeElement() {
-        return element;
+    public RoundEnvironment getRoundEnvironment() {
+        return roundEnvironment;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <E extends Element> E getElement() {
+        return (E) element;
     }
 
     @Override
