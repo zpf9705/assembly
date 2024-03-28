09515b37f5780af132117b9187f36f6e13e9c81c
zhangpengfei <929160069@qq.com>
2024-03-28 09:40:05 +0800

    --- update to many utils

---
+++ assembly-codes/src/main/java/top/osjf/generated/MetadataCollector.java
@@ -0,0 +1,60 @@
+package top.osjf.generated;
+
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.Element;
+import java.lang.annotation.Annotation;
+
+/**
+ * The build class executes metadata records and executes, including
+ * executor-specific annotations to generate processor initialization
+ * properties related to annotations.
+ * @see ProcessAble
+ * @see AbstractMetadataCollector
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.1.3
+ */
+public interface MetadataCollector<T extends Annotation> extends ProcessAble {
+
+    /**
+     * Return the annotation content that triggers the actuator.
+     * @return the annotation content that triggers the actuator.
+     */
+    T getAnnotation();
+
+    /**
+     * Returns the process environment when the annotation processor
+     * triggers execution.
+     * @return the process environment when the annotation processor
+     *       triggers execution.
+     */
+    RoundEnvironment getRoundEnvironment();
+
+    /**
+     * Returns the {@link Element} object that identifies the target class
+     * of the annotation.
+     * @see javax.lang.model.element.TypeElement
+     * @see javax.lang.model.element.PackageElement
+     * @see javax.lang.model.element.TypeParameterElement
+     * @see javax.lang.model.element.ExecutableElement
+     * @see javax.lang.model.element.VariableElement
+     * @return the {@link Element} object that identifies the target class
+     *      of the annotation.
+     */
+    Element getElement();
+
+    /**
+     * Returns the file manager during compilation.
+     * @return the file manager during compilation.
+     */
+    Filer getFiler();
+
+    /**
+     * Returns a custom log output type.
+     * @return a custom log output type.
+     */
+    Logger getLogger();
+
+    @Override
+    void process();
+}
