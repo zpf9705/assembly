d037a789cc7a054a110a044e225162fa6d373c2a
zhangpengfei <929160069@qq.com>
2024-03-29 13:39:16 +0800

    --- update to many utils

--- assembly-codes/src/main/java/top/osjf/generated/impl/ImplSourceCodeGenerateInvocation.java
+++ assembly-codes/src/main/java/top/osjf/generated/impl/ImplSourceCodeGenerateInvocation.java
@@ -6,12 +6,18 @@ import top.osjf.generated.*;
 import java.util.Map;
 
 /**
- * Create source code files for {@link GeneratedSourceGroup} annotations and
+ * Before 1.1.2 to create source code files for {@link GeneratedSourceGroup} annotations and
  * write them to the executor.
+ *
+ * <p> After version 1.1.3, it was renamed {@code ImplSourceCodeGenerateInvocation},
+ * focusing on matters related to the generation of service implementation classes.
+ *
+ * @see GeneratedSourceGroup
+ * @see SpringServiceSource
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.1.0
  */
-public class SourceCodeGenerateInvocation extends AbstractCodeGenerateInvocation {
+public class ImplSourceCodeGenerateInvocation extends AbstractCodeGenerateInvocation {
 
     private final ClassKind classKind;
 
@@ -23,7 +29,7 @@ public class SourceCodeGenerateInvocation extends AbstractCodeGenerateInvocation
 
     private final Map<String, String> annotationSources;
 
-    public SourceCodeGenerateInvocation(String simpleName, String packageName,
+    public ImplSourceCodeGenerateInvocation(String simpleName, String packageName,
                                         String target, ClassKind classKind, String extendClassName,
                                         String[] extendGenericsClassNames,
                                         Map<String, String[]> interfaceClassSources,
