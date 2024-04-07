a3a3cb5280a4e05a965f24d0716ad6be10049cf8
zhangpengfei <929160069@qq.com>
2024-04-07 11:19:31 +0800

    --- update to many utils

---
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/scope/ApplicationScopeLoader.java
@@ -0,0 +1,16 @@
+package top.osjf.assembly.simplified.scope;
+
+/**
+ * Replace the interface function of Spring container bean scope annotation
+ * {@link org.springframework.web.context.annotation.ApplicationScope} with
+ * additional loading function.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 2.1.6
+ */
+public interface ApplicationScopeLoader extends ScopeLoader {
+    @Override
+    default SupportScopeType type(){
+        return SupportScopeType.APPLICATION;
+    }
+}
