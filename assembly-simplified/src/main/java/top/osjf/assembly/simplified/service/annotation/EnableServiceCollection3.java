583a1f5059b2193500fab06f3a70b20de1634cea
zhangpengfei <929160069@qq.com>
2024-04-24 09:26:00 +0800

    --- Develop a local file upload tool.

---
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/service/annotation/EnableServiceCollection3.java
@@ -0,0 +1,23 @@
+package top.osjf.assembly.simplified.service.annotation;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+/**
+ * The third version of service collection is more accurate
+ * than the previous two versions, which collects rule bean
+ * names during bean renaming.
+ *
+ * @see EnableServiceCollection
+ * @see EnableServiceCollection2
+ * @see ServiceContextRecordConfiguration
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 2.2.0
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Import({ServiceContextRecordConfiguration.class})
+public @interface EnableServiceCollection3 {
+}
