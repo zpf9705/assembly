937b0bcfbdfe2f9cc8413d80a16b1460c12760e0
zhangpengfei <929160069@qq.com>
2024-04-28 17:51:02 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

---
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/dcache/DCacheException.java
@@ -0,0 +1,14 @@
+package top.osjf.assembly.simplified.dcache;
+
+/**
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 2.2.4
+ */
+public class DCacheException extends RuntimeException {
+
+    private static final long serialVersionUID = 8565206356868950701L;
+
+    public DCacheException(Throwable cause) {
+        super(cause);
+    }
+}
