ec7dd2e51143370c3fedac62c7c4cf9e180a9104
zhangpengfei <929160069@qq.com>
2024-04-29 16:36:51 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/SimplifiedAutoConfiguration.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/SimplifiedAutoConfiguration.java
@@ -2,14 +2,8 @@ package top.osjf.assembly.simplified;
 
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Role;
-import top.osjf.assembly.simplified.service.ServiceContextUtils;
-import top.osjf.assembly.simplified.service.context.ServiceContext;
-import top.osjf.assembly.simplified.service.context.ServiceContextAwareBeanPostProcessor;
 
 /**
  * {@link EnableAutoConfiguration Auto-configuration} for assembly-simplified.
@@ -21,15 +15,5 @@ import top.osjf.assembly.simplified.service.context.ServiceContextAwareBeanPostP
 @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 public class SimplifiedAutoConfiguration {
 
-    //———————————————————————————————————————— auto config for serviceContext setting
-
-    @Bean(ServiceContextUtils.SC_AWARE_BPP_NANE)
-    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
-    @ConditionalOnMissingBean(ServiceContextAwareBeanPostProcessor.class)
-    public ServiceContextAwareBeanPostProcessor serviceContextAwareBeanPostProcessor(
-            //Since 2.2.3
-            @Lazy //Here, lazy loading is used to prevent dependent beans from losing the function of AOP weaving.
-            ServiceContext serviceContext) {
-        return new ServiceContextAwareBeanPostProcessor(serviceContext);
-    }
+    //———————————————————————————————————————— auto config
 }
