db6a7014eda070ed1ffec5437018e8810979d2d3
zhangpengfei <929160069@qq.com>
2024-04-29 13:28:31 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

---
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/dcache/CacheUpdate.java
@@ -0,0 +1,27 @@
+package top.osjf.assembly.simplified.dcache;
+
+import top.osjf.assembly.simplified.dcache.sql.CacheDruidFilterEvent;
+
+import java.lang.annotation.*;
+
+/**
+ * The flag annotation for cache updates identifies the method used
+ * to capture cache changes of specific values, depending on how you
+ * develop a cache change strategy.
+ *
+ * <p>For example, this article provides a notification class
+ * {@link CacheDruidFilterEvent} for cache changes caused by SQL updates,
+ * deletions, and additions. For this annotation, it is a record of changes
+ * to indicate timely cache changes.
+ *
+ * <p>You need to mark it on the method generated relative to the cache.
+ *
+ * @see Cacheable
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 2.2.4
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface CacheUpdate {
+}
