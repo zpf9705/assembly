3165d8da1ae26dcf5c8cbaa23c422383b5e554fe
zhangpengfei <929160069@qq.com>
2024-04-30 14:11:22 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/cache/CacheContextSupport.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/cache/CacheContextSupport.java
@@ -5,6 +5,7 @@ import top.osjf.assembly.util.lang.CollectionUtils;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 /**
  * Provide cache related support classes.
@@ -22,15 +23,23 @@ public final class CacheContextSupport {
 
     /*** A calculator for changing the execution order.The initialization value is a {@link AtomicInteger}.*/
     private static final ThreadLocal<AtomicInteger> counter = ThreadLocal.withInitial(() ->
-            new AtomicInteger(-1));
+            new AtomicInteger(0));
 
     /**
-     * Add the current {@link Exchange} in an orderly and non repetitive manner.
+     * Add the current {@link Exchange} in an orderly.
+     *
+     * <p>When adding a {@link Exchange}, its repeatability
+     * is not taken into account. Instead, the additions are
+     * arranged in sequence and repeated calculations are
+     * performed during subsequent induction.
+     *
      * @param exchange the current change.
      */
-    @SuppressWarnings("rawtypes")
     public static void addCurrentExchange(Exchange exchange) {
-        if (((CopyOnWriteArrayList) exchanges.get()).addIfAbsent(exchange)) {
+        //Empty value items will not be recorded here temporarily.
+        if (exchange != null) {
+            exchanges.get().add(exchange);
+            //Add the current index counter.
             counter.get().incrementAndGet();
         }
     }
@@ -47,6 +56,36 @@ public final class CacheContextSupport {
     }
 
     /**
+     * Returns all change information for the current thread,
+     * and convert according to the provided template.
+     *
+     * @param <T> The type that needs to be converted.
+     * @return all change information for the current thread.
+     */
+    public static <T extends Exchange> List<T> currentPurgeExchanges() {
+        List<Exchange> purgeChanges = new CopyOnWriteArrayList<>();
+        currentExchanges().stream().collect(Collectors.groupingBy(Exchange::getValue)).forEach(
+                (value, exchanges) -> {
+                    if (exchanges.stream().anyMatch(Exchange::result)){
+                        Exchange exchange = exchanges.get(0);
+                        exchange.ifSetResult();
+                        purgeChanges.add(exchange);
+                    }
+                }
+        );
+        return (List<T>) purgeChanges;
+    }
+
+    /**
+     * Returns the total count of current changes {@link Exchange}.
+     *
+     * @return the total count of current changes {@link Exchange}.
+     */
+    public static int getCurrentExchangeNumber() {
+        return counter.get().get();
+    }
+
+    /**
      * Returns appoint index change information for the current thread.
      *
      * @param <T> The type that needs to be converted.
@@ -55,7 +94,8 @@ public final class CacheContextSupport {
     public static <T extends Exchange> T currentOrderExchange() {
         List<Exchange> exchange0 = currentExchanges();
         if (CollectionUtils.isEmpty(exchange0)) return null;
-        return (T) exchange0.get(counter.get().get());
+        return (T) exchange0.get(/*Because the count starts from 0, the index sorting order starts with -1*/
+                getCurrentExchangeNumber() - 1);
     }
 
     /**
