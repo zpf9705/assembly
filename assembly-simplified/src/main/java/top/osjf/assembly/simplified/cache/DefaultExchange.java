0a23a620381457a2fc3c7704492a53963a20694c
zhangpengfei <929160069@qq.com>
2024-04-30 11:45:23 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/cache/DefaultExchange.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/cache/DefaultExchange.java
@@ -21,6 +21,11 @@ public class DefaultExchange implements Exchange {
     }
 
     @Override
+    public boolean result() {
+        return true;
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
