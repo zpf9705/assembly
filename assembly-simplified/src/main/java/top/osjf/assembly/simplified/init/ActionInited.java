0d364719d8b16ec321c65f3e5c2bec727e18ceb9
zhangpengfei <929160069@qq.com>
2024-05-08 14:04:18 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

---
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/init/ActionInited.java
@@ -0,0 +1,16 @@
+package top.osjf.assembly.simplified.init;
+
+/**
+ * Whether to initialize the marking interface, specifically
+ * how to mark the current initialization progress, and
+ * define it in the implementation class.
+ *
+ * @see AbstractInit
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 2.2.4
+ */
+public interface ActionInited {
+
+    /*** Rewrite the marking logic after initialization.*/
+    void actionInited();
+}
