ba5c14a900aad575241e48b96ad0ec1e0584dc95
zhangpengfei <929160069@qq.com>
2024-05-08 13:57:58 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

---
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/init/annotation/InitConfiguration.java
@@ -0,0 +1,25 @@
+package top.osjf.assembly.simplified.init.annotation;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
+import top.osjf.assembly.simplified.init.AspectJInitSupport;
+
+/**
+ * The relevant configuration of the initialization mechanism
+ * relies on the annotation {@link EnableInit} to enable loading.
+ *
+ * @see EnableInit
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 2.2.4
+ */
+@Configuration(proxyBeanMethods = false)
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+public class InitConfiguration {
+
+    @Bean
+    public AspectJInitSupport aspectJInitSupport(){
+        return new AspectJInitSupport();
+    }
+}
