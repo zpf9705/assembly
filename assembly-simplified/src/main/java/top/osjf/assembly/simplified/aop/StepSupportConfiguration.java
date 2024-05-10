f3cd75ab1c219108276d580847fe00a07e03714b
zhangpengfei <929160069@qq.com>
2024-05-10 14:01:00 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

---
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/aop/StepSupportConfiguration.java
@@ -0,0 +1,22 @@
+package top.osjf.assembly.simplified.aop;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
+
+/**
+ * Enable loading configuration for {@link EnableStepSupport} on
+ * annotation classes.
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 2.2.5
+ */
+@Configuration(proxyBeanMethods = false)
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+public class StepSupportConfiguration {
+
+    @Bean
+    public AspectJStepSupport aspectJStepSupport(){
+        return new AspectJStepSupport();
+    }
+}
