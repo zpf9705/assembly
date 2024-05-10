f98c58b07a93aa33ba87645583aa6537657b4f36
zhangpengfei <929160069@qq.com>
2024-05-10 14:01:07 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/aop/EnableStepSupport.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/aop/EnableStepSupport.java
@@ -6,6 +6,11 @@ import java.lang.annotation.*;
 
 /**
  * Comment on the switch for the callback in the expense step stage.
+ *
+ * <p>Using this annotation will be supported by an aspectj proxy,
+ * responsible for callback methods at various stages of {@link Step}.
+ *
+ * @see StepSupportConfiguration
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 2.2.5
  */
