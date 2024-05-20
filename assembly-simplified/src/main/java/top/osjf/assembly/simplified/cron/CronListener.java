7e9b35b87fd4c1e1660fbad3976ce67cd48fc2b2
zhangpengfei <929160069@qq.com>
2024-05-20 10:55:30 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/cron/CronListener.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/cron/CronListener.java
@@ -1,14 +1,39 @@
 package top.osjf.assembly.simplified.cron;
 
+import cn.hutool.cron.TaskExecutor;
 import cn.hutool.cron.listener.TaskListener;
 
 /**
- * The monitoring interface for timed task start, end, and exception mainly relies on {@link TaskListener},
- * which can be registered through {@link CronRegister#addListener(CronListener...)} and used for task
+ * The monitoring interface for timed task start, end, and exception
+ * mainly relies on {@link TaskListener},which can be registered through
+ * {@link CronRegister#addListener(CronListener...)} and used for task
  * monitoring during startup.
  *
  * @author zpf
  * @since 1.1.0
  */
 public interface CronListener extends TaskListener {
+
+    /**
+     * Triggered when a scheduled task starts.
+     *
+     * @param executor Job executor.
+     */
+    void onStart(TaskExecutor executor);
+
+    /**
+     * Triggered when the task successfully ends.
+     *
+     * @param executor Job executor.
+     */
+    void onSucceeded(TaskExecutor executor);
+
+    /**
+     * Triggered when the task fails to start.
+     *
+     * @param executor Job executor.
+     * @param exception The specific exception captured by the
+     *                  listener when a scheduled task is abnormal.
+     */
+    void onFailed(TaskExecutor executor, Throwable exception);
 }
