9c7719503bba0c630b7720b51d1089542be27066
zhangpengfei <929160069@qq.com>
2024-05-22 16:58:02 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/annotation/Sdk.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/annotation/Sdk.java
@@ -33,7 +33,7 @@ import java.lang.annotation.*;
  * version {@code 2.0.9}, and the remaining attributes that were not added
  * will not be added, making them useless for this component.
  *
- * <p>If you want to provide visible proxy classes{@link #beanDefinitionClass()},
+ * <p>If you want to provide visible proxy classes{@link #beanDefinitionType()},
  * then you need to understand {@link ImportBeanDefinitionRegistrar} registration
  * {@link BeanDefinition} and the usage of injection models.
  * <p>Please refer to the introduction of {@link #autowireMode()}.
@@ -126,7 +126,7 @@ public @interface Sdk {
      * Spring has not yet started creating beans in this registration stage, and since
      * beans have not been created, it will not parse the {@link Autowired}  or
      * {@link javax.annotation.Resource} annotation.
-     * <p><strong>Therefore, if you provide a {@link #beanDefinitionClass()} class,
+     * <p><strong>Therefore, if you provide a {@link #beanDefinitionType()} class,
      * then in this class do not use {@link Autowired}  or
      * {@link javax.annotation.Resource} annotation for bean injection.</strong>
      * <p>Of course, if you use {@link GenericBeanDefinition#AUTOWIRE_CONSTRUCTOR},
