6c764ef2111fc56ff84d2f6097750adbbd40cac4
zhangpengfei <929160069@qq.com>
2024-05-22 11:15:11 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/process/DefaultErrorResponse.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/process/DefaultErrorResponse.java
@@ -8,14 +8,16 @@ import top.osjf.assembly.util.json.FastJsonUtils;
 import java.util.Objects;
 
 /**
- * Default response impl for {@link Response}.
+ * Default response impl for {@link ErrorResponse}.
+ * <p>Not directly used for response purposes, mainly used
+ * for handling exception conversions in error situations.
  *
- * <p>Used to handle exception requests.</p>
- *
+ * @see #parseErrorResponse(String, ErrorType, Request)
+ * @see #parseErrorResponse(Throwable, ErrorType, Request)
  * @author zpf
  * @since 1.1.0
  */
-public class DefaultErrorResponse extends AbstractResponse {
+public final class DefaultErrorResponse extends AbstractResponse {
 
     private static final long serialVersionUID = -6303939513087992265L;
 
@@ -56,61 +58,96 @@ public class DefaultErrorResponse extends AbstractResponse {
         this.message = message;
     }
 
+    /**
+     * Build a default error response {@code DefaultErrorResponse} using
+     * {@link #SDK_ERROR_CODE} based on the provided information {@code message}.
+     *
+     * @param message Provide information accordingly.
+     * @return a default error response {@code DefaultErrorResponse}.
+     */
     public static DefaultErrorResponse buildSdkExceptionResponse(String message) {
         return new DefaultErrorResponse(SDK_ERROR_CODE, message);
     }
 
+    /**
+     * Build a default error response {@code DefaultErrorResponse} using
+     * {@link #UNKNOWN_ERROR_CODE} based on the provided information {@code message}.
+     *
+     * @param message Provide information accordingly.
+     * @return a default error response {@code DefaultErrorResponse}.
+     */
     public static DefaultErrorResponse buildUnknownResponse(String message) {
         return new DefaultErrorResponse(UNKNOWN_ERROR_CODE, String
                 .format("Happen unknown exception,message=[%s]", message));
     }
 
+    /**
+     * Build a default error response {@code DefaultErrorResponse} using
+     * {@link #DATA_ERROR_CODE} based on the provided information {@code message}.
+     *
+     * @param message Provide information accordingly.
+     * @return a default error response {@code DefaultErrorResponse}.
+     */
     public static DefaultErrorResponse buildDataErrorResponse(String message) {
         return new DefaultErrorResponse(DATA_ERROR_CODE, String
                 .format("Happen data_error exception,message=[%s]", message));
     }
 
+    /**
+     * Convert the specified response {@link Response} to the specified error
+     * type based on {@code error} and {@code type} and {@code request}.
+     *
+     * @param error   Specific error information.
+     * @param type    Wrong enumeration type.
+     * @param request Request parameters.
+     * @param <R>     The type of response class.
+     * @return Response error parameters.
+     */
     public static <R extends Response> R parseErrorResponse(String error, ErrorType type, Request<R> request) {
         return parseErrorResponse(new DataConvertException(error), type, request);
     }
 
+    /**
+     * Convert the specified response {@link Response} to the specified error
+     * type based on {@link Throwable} and {@link ErrorType} and {@link Request}.
+     *
+     * @param error   Specific error information.
+     * @param type    Wrong enumeration type.
+     * @param request Request parameters.
+     * @param <R>     The type of response class.
+     * @return Response error parameters.
+     */
     public static <R extends Response> R parseErrorResponse(Throwable error, ErrorType type, Request<R> request) {
         DefaultErrorResponse response = type.getMessageWithType(error);
-        R r = FastJsonUtils.parseObject(FastJsonUtils.toJSONString(response),
-                request.getResponseRequiredType());
+        R r = FastJsonUtils.parseObject(FastJsonUtils.toJSONString(response), request.getResponseRequiredType());
         r.setErrorCode(response.getCode());
         r.setErrorMessage(response.getMessage());
         return r;
     }
 
-
+    /*** Build a universal interface for {@link DefaultErrorResponse} based on exceptions.*/
     @FunctionalInterface
     interface MessageGetter {
-
         DefaultErrorResponse getMessageWithType(Throwable error);
     }
 
+    /*** Error allocating different {@link DefaultErrorResponse} creation method enumeration
+     * classes for different types.*/
     public enum ErrorType implements MessageGetter {
         SDK {
             @Override
             public DefaultErrorResponse getMessageWithType(Throwable error) {
-                return buildSdkExceptionResponse(
-                        ExceptionUtils.stacktraceToOneLineString(error, 1500)
-                );
+                return buildSdkExceptionResponse(ExceptionUtils.stacktraceToOneLineString(error, 1500));
             }
         }, UN_KNOWN {
             @Override
             public DefaultErrorResponse getMessageWithType(Throwable error) {
-                return buildUnknownResponse(
-                        ExceptionUtils.stacktraceToOneLineString(error, 4500)
-                );
+                return buildUnknownResponse(ExceptionUtils.stacktraceToOneLineString(error, 4500));
             }
         }, DATA {
             @Override
             public DefaultErrorResponse getMessageWithType(Throwable error) {
-                return buildDataErrorResponse(
-                        ExceptionUtils.stacktraceToOneLineString(error, 3000)
-                );
+                return buildDataErrorResponse(ExceptionUtils.stacktraceToOneLineString(error, 3000));
             }
         }
     }
