426ccc5eab999eea36ec294dc56f9838bf8191c7
zhangpengfei <929160069@qq.com>
2024-05-23 14:47:21 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/support/AbstractCglibProxySupport.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/support/AbstractCglibProxySupport.java
@@ -1,22 +1,46 @@
 package top.osjf.assembly.simplified.support;
 
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.cglib.proxy.Callback;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
 import org.springframework.lang.Nullable;
-import top.osjf.assembly.util.lang.ReflectUtils;
 
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 /**
+ * This abstract class is used to create proxy objects using Spring's cglib
+ * pattern. In addition to interfaces, it can also create objects for regular
+ * classes, which compensates for this disadvantage compared to {@link AbstractJdkProxySupport}.
+ *
+ * <p>The process is to first rely on the dynamic proxy {@link MethodInterceptor} of
+ * spring cglib to create a proxy object,hand it over to Spring, and when Spring calls
+ * the proxy object, execute the {@link #intercept(Object, Method, Object[], MethodProxy)}
+ * method,which performs unified logical processing and distribution within this method,
+ *
+ * <p>The method entrusted to Spring to create bean objects is to implement the
+ * {@link FactoryBean} interface and dynamically register beans through
+ * {@link org.springframework.beans.factory.support.BeanDefinitionRegistry}
+ *
  * <p>If you want to standardize processing, you can access {@link AbstractMultipleProxySupport}.
  *
  * @param <T> The data type of the proxy class.
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @see Enhancer
  * @since 2.2.5
  */
-public abstract class AbstractCglibProxySupport<T> implements FactoryBean<T>, InvocationHandler {
+public abstract class AbstractCglibProxySupport<T> implements FactoryBean<T>, MethodInterceptor {
 
+    /**
+     * The target type of dynamic proxy.
+     */
     private Class<T> type;
+
+    /**
+     * Is the object managed by this factory a singleton.
+     */
+    public boolean isSingleton = true;
 
     public void setType(Class<T> type) {
         this.type = type;
@@ -26,6 +50,10 @@ public abstract class AbstractCglibProxySupport<T> implements FactoryBean<T>, In
         return type;
     }
 
+    public void setSingleton(boolean singleton) {
+        isSingleton = singleton;
+    }
+
     @Nullable
     @Override
     public Class<?> getObjectType() {
@@ -33,10 +61,23 @@ public abstract class AbstractCglibProxySupport<T> implements FactoryBean<T>, In
     }
 
     @Override
-    public T getObject() {
-        return ReflectUtils.newProxyInstance(this, type);
+    public boolean isSingleton() {
+        return isSingleton;
     }
 
     @Override
-    public abstract Object invoke(Object proxy, Method method, Object[] args);
+    public T getObject() {
+        return createProxy(type, this);
+    }
+
+    @SuppressWarnings("unchecked")
+    static <T> T createProxy(Class<T> type, Callback callback) {
+        final Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(type);
+        enhancer.setCallback(callback);
+        return (T) enhancer.create();
+    }
+
+    @Override
+    public abstract Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy);
 }
