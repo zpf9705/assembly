a738407861208ea3faea76242ef00cac8d9b877c
zhangpengfei <929160069@qq.com>
2024-05-28 14:30:45 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/cron/annotation/Cron.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/cron/annotation/Cron.java
@@ -1,6 +1,6 @@
 package top.osjf.assembly.simplified.cron.annotation;
 
-import top.osjf.assembly.simplified.cron.CronTaskRegister;
+import org.springframework.core.annotation.AliasFor;
 
 import java.lang.annotation.*;
 
@@ -10,10 +10,16 @@ import java.lang.annotation.*;
  * {@link org.springframework.core.env.Environment} registration,
  * and provides fixed initialization parameters.
  *
- * <p>Relying on {@link CronTaskRegister} or {@link CronTaskRegisterPostProcessor}
- * to scan, obtain, and register corresponding annotation methods.
+ * <p>Relying on {@link CronTaskRegisterPostProcessor} to scan, obtain,
+ * and register corresponding annotation methods，filter the non-static,
+ * publicly county-wide, and annotated methods defined in the current
+ * class as a timed runtime.
  *
  * @author zpf
+ * @see EnableCronTaskRegister2
+ * @see CronTaskRegisterPostProcessor
+ * @see top.osjf.assembly.simplified.cron.CronRegister#register(Object, String[])
+ * @see CronAnnotationAttributes
  * @since 1.1.0
  */
 @Target({ElementType.METHOD})
@@ -22,15 +28,66 @@ import java.lang.annotation.*;
 public @interface Cron {
 
     /**
+     * Provide a default cron expression. When the value of {@link #value()}
+     * or {@link #expression()} is not filled in, the default execution cycle
+     * for the current task is once every 1 second.
+     * @since 2.2.5
+     */
+    String DEFAULT_CRON_EXPRESSION = "0/1 * * * * ?";
+
+    /**
+     * After annotation mapping the map structure, map the key value of
+     * the annotation attribute {@link #expression()}.
+     * @since 2.2.5
+     */
+    String SELECT_OF_EXPRESSION_NAME = "expression";
+
+
+    /**
+     * After annotation mapping the map structure, map the key value of
+     * the annotation attribute {@link #profiles()}.
+     * @since 2.2.5
+     */
+    String SELECT_OF_PROFILES_NAME = "profiles";
+
+    /**
+     * Alias for {@link #expression}.
+     * <p>Intended to be used when no other attributes are needed, for example:
+     * {@code @Cron("0/1 * * * * ?")}.
+     * @return an expression that can be parsed to a cron schedule.
+     * @see #expression
+     * @since 2.2.5
+     */
+    @AliasFor("expression")
+    String value() default "";
+
+    /**
      * Provide a cron expression that conforms to the rules.
      * <p>This parameter will determine the execution interval of the
      * registered scheduled task.
      * <p>Please understand the writing conventions of cron expressions
      * before use to avoid unnecessary errors.
      *
-     * @return Must be provided and cannot be {@literal null}.
+     * <p> A cron-like expression, extending the usual UN*X definition to include triggers
+     * on the second, minute, hour, day of month, month, and day of week.
+     * <p>For example, {@code "0 * * * * MON-FRI"} means once per minute on weekdays
+     * (at the top of the minute - the 0th second).
+     * <p>The fields read from left to right are interpreted as follows.
+     * <ul>
+     * <li>second</li>
+     * <li>minute</li>
+     * <li>hour</li>
+     * <li>day of month</li>
+     * <li>month</li>
+     * <li>day of week</li>
+     * </ul>
+     *
+     * <p>For clearer meaning, it was renamed 'expression' since 2.2.5.
+     *
+     * @return an expression that can be parsed to a cron schedule.
      */
-    String express();
+    @AliasFor("value")
+    String expression() default "";
 
     /**
      * Since the implementation of this annotation needs to be in the
