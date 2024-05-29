a9ce247334ed3997e34f3e4df6fbf2b1cc528133
zhangpengfei <929160069@qq.com>
2024-05-29 09:27:04 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/cron/CronListener.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/cron/CronListener.java
@@ -6,7 +6,7 @@ import cn.hutool.cron.listener.TaskListener;
 /**
  * The monitoring interface for timed task start, end, and exception
  * mainly relies on {@link TaskListener},which can be registered through
- * {@link CronRegister#addListener(CronListener...)} and used for task
+ * {@link CronTaskManager#addCronListeners(CronListener...)} and used for task
  * monitoring during startup.
  *
  * @author zpf
