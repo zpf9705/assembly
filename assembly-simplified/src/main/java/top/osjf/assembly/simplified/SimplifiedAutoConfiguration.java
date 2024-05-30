731f908f83325b52dfeafc407b6361fbbb468861
zhangpengfei <929160069@qq.com>
2024-05-30 17:52:04 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/SimplifiedAutoConfiguration.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/SimplifiedAutoConfiguration.java
@@ -1,8 +1,6 @@
 package top.osjf.assembly.simplified;
 
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -11,9 +9,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Role;
+import org.springframework.context.support.SimpleThreadScope;
 import top.osjf.assembly.simplified.scope.ThreadScoped;
 import top.osjf.assembly.simplified.scope.ThreadScopedExecutor;
-import top.osjf.assembly.util.annotation.NotNull;
 
 import java.util.concurrent.TimeUnit;
 
@@ -26,28 +24,36 @@ import java.util.concurrent.TimeUnit;
 @Configuration(proxyBeanMethods = false)
 @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 @EnableConfigurationProperties(TaskExecutionProperties.class)
-public class SimplifiedAutoConfiguration implements BeanFactoryPostProcessor {
+public class SimplifiedAutoConfiguration {
 
     //———————————————————————————————————————— auto config
 
-    private ConfigurableListableBeanFactory beanFactory;
+    private final ConfigurableListableBeanFactory beanFactory;
 
     private final TaskExecutionProperties properties;
 
-    public SimplifiedAutoConfiguration(TaskExecutionProperties properties) {
+    public SimplifiedAutoConfiguration(ConfigurableListableBeanFactory beanFactory,
+                                       TaskExecutionProperties properties) {
+        this.beanFactory = beanFactory;
         this.properties = properties;
     }
 
-    @Override
-    public void postProcessBeanFactory(@NotNull ConfigurableListableBeanFactory beanFactory)
-            throws BeansException {
-        this.beanFactory = beanFactory;
-    }
-
+    /**
+     * To support annotation {@link top.osjf.assembly.simplified.scope.ThreadScope},
+     * register scope {@link SimpleThreadScope} as support for {@code thread}.
+     *
+     * @return ThreadScopedExecutor
+     */
     @Bean
     @ConditionalOnMissingBean
     public ThreadScopedExecutor threadScopedExecutor() {
         TaskExecutionProperties.Pool pool = properties.getPool();
+        if (pool.getMaxSize() == Integer.MAX_VALUE) {
+            pool.setMaxSize(16);
+        }
+        if (pool.getQueueCapacity() == Integer.MAX_VALUE) {
+            pool.setQueueCapacity(1000);
+        }
         ThreadScopedExecutor executor = new ThreadScopedExecutor(
                 pool.getCoreSize(),
                 pool.getMaxSize(),
