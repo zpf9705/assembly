f7a739f00573c5a84c9a176b4befb271d3eec862
zhangpengfei <929160069@qq.com>
2024-05-31 10:34:57 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/SimplifiedAutoConfiguration.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/SimplifiedAutoConfiguration.java
@@ -1,22 +1,11 @@
 package top.osjf.assembly.simplified;
 
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Role;
-import org.springframework.context.support.SimpleThreadScope;
-import top.osjf.assembly.simplified.scope.ThreadScoped;
-import top.osjf.assembly.simplified.scope.ThreadScopedExecutor;
-import top.osjf.assembly.simplified.scope.ThreadScopedExecutorAgent;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 /**
  * {@link EnableAutoConfiguration Auto-configuration} for assembly-simplified.
@@ -30,40 +19,4 @@ import java.util.concurrent.TimeUnit;
 public class SimplifiedAutoConfiguration {
 
     //———————————————————————————————————————— auto config
-
-    private final ConfigurableListableBeanFactory beanFactory;
-
-    private final TaskExecutionProperties properties;
-
-    public SimplifiedAutoConfiguration(ConfigurableListableBeanFactory beanFactory,
-                                       TaskExecutionProperties properties) {
-        this.beanFactory = beanFactory;
-        this.properties = properties;
-    }
-
-    @Bean
-    public ThreadScopedExecutor threadScopedExecutor() {
-        TaskExecutionProperties.Pool pool = properties.getPool();
-        return new ThreadScopedExecutorAgent(
-                pool.getCoreSize(),
-                pool.getCoreSize() * 2,
-                pool.getKeepAlive().getSeconds(),
-                TimeUnit.SECONDS,
-                pool.getCoreSize() * 100);
-    }
-
-    /**
-     * To support annotation {@link top.osjf.assembly.simplified.scope.ThreadScope},
-     * register scope {@link SimpleThreadScope} as support for {@code thread}.
-     *
-     * @return ThreadScopedExecutor
-     */
-    @Bean
-    @ConditionalOnMissingBean
-    public ThreadScoped threadScopedExecutor(ObjectProvider<List<ThreadScopedExecutor>> executor) {
-        List<ThreadScopedExecutor> executors = executor.getIfAvailable();
-        ThreadScoped threadScoped = new ThreadScoped(executors);
-        beanFactory.registerScope("thread", threadScoped);
-        return threadScoped;
-    }
 }
