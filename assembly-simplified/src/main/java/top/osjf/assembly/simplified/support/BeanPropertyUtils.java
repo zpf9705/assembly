4a893acc86013a07f0532b5f919cfa3fc4a6deb0
zhangpengfei <929160069@qq.com>
2024-06-03 13:25:45 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/support/BeanPropertyUtils.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/support/BeanPropertyUtils.java
@@ -1,12 +1,21 @@
 package top.osjf.assembly.simplified.support;
 
+import org.springframework.context.annotation.Description;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Role;
+import org.springframework.context.annotation.Scope;
+import org.springframework.core.annotation.MergedAnnotation;
+import org.springframework.core.type.AnnotationMetadata;
 import top.osjf.assembly.util.lang.ArrayUtils;
+import top.osjf.assembly.util.lang.CollectionUtils;
+import top.osjf.assembly.util.lang.ConvertUtils;
 
-import java.util.Arrays;
-import java.util.LinkedHashSet;
+import java.lang.annotation.Annotation;
+import java.util.*;
 
 /**
  * The relevant general method tool class for {@link BeanProperty}.
+ *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 2.2.5
  */
@@ -43,5 +52,107 @@ public abstract class BeanPropertyUtils {
                     ArrayUtils.remove(names, 0))).toArray(new String[]{});
         }
         return null;
+    }
+
+    /**
+     * The scope name setting of the bean supports annotations
+     * {@link Scope} corresponding to attributes other than
+     * {@link BeanProperty#scope()}}, and annotations are
+     * used first.
+     * <p>Of course, when there is no direct existence of {@link Scope},
+     * search for the derived annotations of {@link Scope}.
+     * If multiple derived annotations appear, the topmost one
+     * will be selected as the scope definition option for this time.
+     *
+     * @param annotationMetadata Annotate metadata information.
+     * @param defaultValue       The default value when no annotations are provided.
+     * @return Final logical value.
+     */
+    public static String getMaybeAnnotationScope(AnnotationMetadata annotationMetadata, String defaultValue) {
+        if (annotationMetadata.hasAnnotation(Scope.class.getCanonicalName())) {
+            return getMaybeAnnotationValue(annotationMetadata, Scope.class, String.class, defaultValue);
+        }
+        MergedAnnotation<Annotation> mergedAnnotation = annotationMetadata.getAnnotations()
+                .stream()
+                .filter(m -> m.getType().getAnnotation(Scope.class) != null)
+                .min((o1, o2) -> {
+                    int o1_index = o1.getAggregateIndex();
+                    int o2_index = o2.getAggregateIndex();
+                    if (o1_index == o2_index) return 0;
+                    return o1_index > o2_index ? 1 : -1;
+                })
+                .orElse(null);
+        return mergedAnnotation != null ? mergedAnnotation.getType().getAnnotation(Scope.class).value() : defaultValue;
+    }
+
+    /**
+     * The role setting of the bean supports annotations
+     * {@link Role} corresponding to attributes other than
+     * {@link BeanProperty#role()}}, and annotations are
+     * used first.
+     *
+     * @param annotationMetadata Annotate metadata information.
+     * @param defaultValue       The default value when no annotations are provided.
+     * @return Final logical value.
+     */
+    public static int getMaybeAnnotationRole(AnnotationMetadata annotationMetadata, int defaultValue) {
+        return getMaybeAnnotationValue(annotationMetadata, Role.class, int.class, defaultValue);
+    }
+
+    /**
+     * The lazy load setting of the bean supports annotations
+     * {@link Lazy} corresponding to attributes other than
+     * {@link BeanProperty#lazyInit()}}, and annotations are
+     * used first.
+     *
+     * @param annotationMetadata Annotate metadata information.
+     * @param defaultValue       The default value when no annotations are provided.
+     * @return Final logical value.
+     */
+    public static boolean getMaybeAnnotationLazy(AnnotationMetadata annotationMetadata, boolean defaultValue) {
+        return getMaybeAnnotationValue(annotationMetadata, Lazy.class, boolean.class, defaultValue);
+    }
+
+    /**
+     * The description information setting of the bean supports annotations
+     * {@link Description} corresponding to attributes other than
+     * {@link BeanProperty#description()}, and annotations are used first.
+     *
+     * @param annotationMetadata Annotate metadata information.
+     * @param defaultValue       The default value when no annotations are provided.
+     * @return Final logical value.
+     */
+    public static String getMaybeAnnotationDescription(AnnotationMetadata annotationMetadata, String defaultValue) {
+        return getMaybeAnnotationValue(annotationMetadata, Description.class, String.class, defaultValue);
+    }
+
+    /**
+     * Add support for special annotations that support certain defined
+     * properties of annotation {@link BeanProperty}, with priority greater
+     * than the value definition in this annotation. If present, return the
+     * defined {@code value}.
+     *
+     * @param annotationMetadata Annotate metadata information.
+     * @param annotationType     Possible priority annotation types.
+     * @param valueType          The type of the final parsed value.
+     * @param defaultValue       The default value when no annotations are provided.
+     * @param <A>                Possible priority annotation types.
+     * @param <T>                The type of the final parsed value.
+     * @return Final logical value.
+     */
+    private static <A extends Annotation, T> T getMaybeAnnotationValue(AnnotationMetadata annotationMetadata,
+                                                                       Class<A> annotationType,
+                                                                       Class<T> valueType,
+                                                                       T defaultValue) {
+        String canonicalName = annotationType.getCanonicalName();
+        if (!annotationMetadata.hasAnnotation(canonicalName)) {
+            return defaultValue;
+        }
+        Map<String, Object> annotationAttributes =
+                annotationMetadata.getAnnotationAttributes(canonicalName);
+        if (CollectionUtils.isEmpty(annotationAttributes)) {
+            return defaultValue;
+        }
+        return ConvertUtils.convert(valueType, annotationAttributes.get("value"));
     }
 }
