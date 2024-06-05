a3f7e2c990320ec7378faf9b395bc3556a51845f
zhangpengfei <929160069@qq.com>
2024-06-05 09:43:37 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/SimplifiedAutoConfiguration.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/SimplifiedAutoConfiguration.java
@@ -2,8 +2,6 @@ package top.osjf.assembly.simplified;
 
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Role;
 
@@ -15,7 +13,6 @@ import org.springframework.context.annotation.Role;
  */
 @Configuration(proxyBeanMethods = false)
 @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
-@EnableConfigurationProperties(TaskExecutionProperties.class)
 public class SimplifiedAutoConfiguration {
 
     //———————————————————————————————————————— auto config
