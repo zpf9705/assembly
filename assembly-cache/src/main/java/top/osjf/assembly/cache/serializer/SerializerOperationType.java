d0f881deb989849b05148b8bf8a6c9957d5fd1dd
zhangpengfei <929160069@qq.com>
2024-06-12 16:48:12 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-cache/src/main/java/top/osjf/assembly/cache/serializer/SerializerOperationType.java
+++ assembly-cache/src/main/java/top/osjf/assembly/cache/serializer/SerializerOperationType.java
@@ -2,9 +2,10 @@ package top.osjf.assembly.cache.serializer;
 
 /**
  * The type of serialization operation.
+ *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.1.4
  */
-public enum SerializerType {
-    KEY,VALUE
+public enum SerializerOperationType {
+    KEY, VALUE
 }
