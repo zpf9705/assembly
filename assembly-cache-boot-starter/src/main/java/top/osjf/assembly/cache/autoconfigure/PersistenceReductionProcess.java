971f98020d4c7613d241f5c8e708c2a7c85931ff
zhangpengfei <929160069@qq.com>
2024-06-12 10:55:23 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-cache-boot-starter/src/main/java/top/osjf/assembly/cache/autoconfigure/PersistenceReductionProcess.java
+++ assembly-cache-boot-starter/src/main/java/top/osjf/assembly/cache/autoconfigure/PersistenceReductionProcess.java
@@ -1,56 +1,24 @@
 package top.osjf.assembly.cache.autoconfigure;
 
-import top.osjf.assembly.cache.persistence.CachePersistenceReduction;
-import top.osjf.assembly.util.lang.ReflectUtils;
-import top.osjf.assembly.util.lang.StringUtils;
-
-import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.InitializingBean;
+import top.osjf.assembly.cache.persistence.ByteCachePersistence;
 
 /**
  * Cache the process information bean of the persistent file recovery class.
+ *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.0.8
  */
-public class PersistenceReductionProcess {
+public class PersistenceReductionProcess implements InitializingBean {
 
     private final String path;
 
-    private final Class<? extends CachePersistenceReduction> persistenceReductionClass;
-
-    private String resultMessage;
-
-    public PersistenceReductionProcess(String path,
-                                       Class<? extends CachePersistenceReduction> persistenceReductionClass) {
+    public PersistenceReductionProcess(String path) {
         this.path = path;
-        this.persistenceReductionClass = persistenceReductionClass;
     }
 
-    public String getPath() {
-        return path;
-    }
-
-    public Class<? extends CachePersistenceReduction> getPersistenceReductionClass() {
-        return persistenceReductionClass;
-    }
-
-    public String getResultMessage() {
-        return resultMessage;
-    }
-
-    @PostConstruct
-    public void reductionUsePath() {
-        if (StringUtils.isBlank(path) || persistenceReductionClass == null) {
-            resultMessage = "reductionPath or persistenceReductionClass is null";
-            return;
-        }
-        CachePersistenceReduction reduction;
-        try {
-            reduction = ReflectUtils.newInstance(persistenceReductionClass);
-        } catch (Exception e) {
-            resultMessage = "persistenceReductionClass newInstance  for default Constructor filed";
-            return;
-        }
-        reduction.reductionUsePath(path);
-        resultMessage = "process";
+    @Override
+    public void afterPropertiesSet() {
+        new ByteCachePersistence().reductionUsePath(path);
     }
 }
