7c40b0cac262cb2bf9d226ec8e0c9aeae5a5e44d
zhangpengfei <929160069@qq.com>
2024-06-12 16:57:40 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

---
+++ assembly-cache/src/main/java/top/osjf/assembly/cache/serializer/CacheByteIdentify.java
@@ -0,0 +1,26 @@
+package top.osjf.assembly.cache.serializer;
+
+import top.osjf.assembly.cache.persistence.AbstractCachePersistence;
+import top.osjf.assembly.util.data.ByteIdentify;
+
+/**
+ * Carry {@link PairSerializer} to deserialize the value when the cache expires.
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.1.4
+ */
+@SuppressWarnings("rawtypes")
+public class CacheByteIdentify extends ByteIdentify {
+
+    private static final long serialVersionUID = 1227919046430145466L;
+
+    private final String pairSerializerName;
+
+    public CacheByteIdentify(byte[] var, String pairSerializerName) {
+        super(var);
+        this.pairSerializerName = pairSerializerName;
+    }
+
+    public PairSerializer getPairSerializer() {
+        return AbstractCachePersistence.getPairSerializerByName(pairSerializerName);
+    }
+}
