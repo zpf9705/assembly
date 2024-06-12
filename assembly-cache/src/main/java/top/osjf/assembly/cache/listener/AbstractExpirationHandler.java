5b5e8e4f42b45535317b063738dea122933f302d
zhangpengfei <929160069@qq.com>
2024-06-12 11:40:01 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-cache/src/main/java/top/osjf/assembly/cache/listener/AbstractExpirationHandler.java
+++ assembly-cache/src/main/java/top/osjf/assembly/cache/listener/AbstractExpirationHandler.java
@@ -9,22 +9,20 @@ import java.util.List;
 /**
  * The processing plan after expiration, including the notification
  * of custom expiration listeners and the processing of cached files.
+ *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.0.8
  */
 public abstract class AbstractExpirationHandler {
 
-    static List<ExpirationMessageListener> expirationMessageListeners = Configuration.getExpirationMessageListeners();
-
-    public void expiredMessageCapable(DefaultMessage message) {
-        if (message == null){
+    protected void handlerExpirationMessage(DefaultMessage message) {
+        if (message == null) {
             return;
         }
-        //notify
-        if (CollectionUtils.isNotEmpty(expirationMessageListeners)) {
-            expirationMessageListeners.forEach((listener) -> listener.onMessage(message));
+        List<ExpirationMessageListener> listeners = Configuration.getConfiguration().getExpirationMessageListeners();
+        if (CollectionUtils.isNotEmpty(listeners)) {
+            listeners.forEach((listener) -> listener.onMessage(message));
         }
-        //clean persistence
         AbstractRecordActivationCenter.getSingletonCenter().cleanSupportingElements(message);
     }
 }
