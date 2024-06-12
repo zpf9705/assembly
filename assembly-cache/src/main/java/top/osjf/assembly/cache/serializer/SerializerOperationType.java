41a8feabe0e910086b793ab7ad1cf5de93d9620c
zhangpengfei <929160069@qq.com>
2024-06-12 17:01:36 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-cache/src/main/java/top/osjf/assembly/cache/serializer/SerializerOperationType.java
+++ assembly-cache/src/main/java/top/osjf/assembly/cache/serializer/SerializerOperationType.java
@@ -1,11 +1,28 @@
 package top.osjf.assembly.cache.serializer;
 
+import top.osjf.assembly.cache.persistence.CachePersistenceThreadLocal;
+
+import java.util.function.Supplier;
+
 /**
  * The type of serialization operation.
+ *
+ * <p>Retrieve cache persistent variables related to
+ * key and value from the current thread based on their type.
  *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.1.4
  */
-public enum SerializerOperationType {
-    KEY, VALUE
+public enum SerializerOperationType implements Supplier<String> {
+    KEY {
+        @Override
+        public String get() {
+            return CachePersistenceThreadLocal.getKeyPairSerializerName();
+        }
+    }, VALUE {
+        @Override
+        public String get() {
+            return CachePersistenceThreadLocal.getValuePairSerializerName();
+        }
+    };
 }
