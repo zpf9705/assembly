4b54c4e75deda7f3d22889cb058f61488a3fb33a
zhangpengfei <929160069@qq.com>
2024-06-13 12:42:47 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/SdkUtils.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/SdkUtils.java
@@ -2,17 +2,18 @@ package top.osjf.assembly.simplified.sdk;
 
 import org.apache.http.entity.ContentType;
 import top.osjf.assembly.simplified.sdk.http.HttpResultResponse;
-import top.osjf.assembly.simplified.sdk.process.Request;
-import top.osjf.assembly.simplified.sdk.process.Response;
+import top.osjf.assembly.simplified.sdk.process.*;
+import top.osjf.assembly.simplified.sdk.proxy.RequestCreateException;
+import top.osjf.assembly.simplified.sdk.proxy.UnknownRequestParameterException;
+import top.osjf.assembly.simplified.sdk.proxy.UnknownResponseParameterException;
 import top.osjf.assembly.util.lang.ArrayUtils;
 import top.osjf.assembly.util.lang.MapUtils;
+import top.osjf.assembly.util.lang.ReflectUtils;
 
+import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -21,6 +22,7 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author zpf
  * @since 1.1.0
  */
+@SuppressWarnings("rawtypes")
 public abstract class SdkUtils {
 
     public static final String named = "Content-Type";
@@ -42,10 +44,106 @@ public abstract class SdkUtils {
     /***The right angle bracket included in the generic.*/
     static final String RIGHT_ANGLE_BRACKET = ">";
 
+    /***The prefix name of the set method.*/
+    static final String SET_METHOD_PREFIX = "set";
+
     static final Map<Class<?>, Object> rps_classes = new ConcurrentHashMap<>(16);
 
     /**
-     * Obtain the corresponding class generic carried by the request class.
+     * Create corresponding request parameters based on extension
+     * type annotations and interfaces.
+     *
+     * @param method Proxy target method.
+     * @param args   Request parameters.
+     * @return The request class parameters created.
+     * @see ResponseData
+     * @see RequestParam
+     */
+    public static Request<?> invokeCreateRequest(Method method, Object[] args) {
+        Objects.requireNonNull(args);
+        Request<?> request;
+        if (args.length == 1) {
+            Object arg = args[0];
+            if (arg instanceof Request) {
+                //Consider first whether it is the actual request parameter.
+                request = (Request<?>) arg;
+            } else {
+                //When a single parameter is not a request class, consider implementing
+                // the parameter interface. If the parameter interface is not implemented,
+                // an unknown exception will be thrown.
+                if (arg instanceof RequestParameter) {
+                    Class<? extends Request> requestType = ((RequestParameter) arg).getRequestType();
+                    if (requestType == null) throw new UnknownRequestParameterException();
+                    return invokeCreateRequestConstructorWhenFailedUseSet(requestType, arg);
+                }
+                throw new UnknownRequestParameterException();
+            }
+        } else {
+            //Consider annotations for multiple parameters, and throw an
+            // exception for unknown parameter types when there are no annotations present.
+            RequestParam requestParam = method.getAnnotation(RequestParam.class);
+            if (requestParam != null) {
+                return invokeCreateRequestConstructorWhenFailedUseSet(requestParam.value(), args);
+            }
+            throw new UnknownRequestParameterException();
+        }
+        return request;
+    }
+
+    /**
+     * When obtaining a response, convert to the desired type, which can be
+     * specified in {@link ResponseData}.
+     *
+     * @param method   Proxy target method.
+     * @param response The response type obtained.
+     * @return The required return object.
+     */
+    public static Object getResponse(Method method, Response response) {
+        Class<?> returnType = method.getReturnType();
+        if (Response.class.isAssignableFrom(returnType)) {
+            return response;
+        }
+        boolean success = response.isSuccess();
+        if (response instanceof ResponseData) {
+            if (success) return ((ResponseData) response).getData();
+            return null;
+        }
+        throw new UnknownResponseParameterException();
+    }
+
+    static Request<?> invokeCreateRequestConstructorWhenFailedUseSet(Class<? extends Request> requestType,
+                                                                     Object... args) {
+        Request<?> request;
+        try {
+            //First, directly instantiate the request class using the
+            // construction method based on the parameters.
+            request = ReflectUtils.newInstance(requestType, args);
+        } catch (Throwable e) {
+            request = invokeCreateRequestUseSet(requestType, args);
+        }
+        return request;
+    }
+
+    static Request<?> invokeCreateRequestUseSet(Class<? extends Request> requestType,
+                                                Object... args) {
+        Request<?> request;
+        try {
+            //When parameter construction fails, first use an empty
+            // construction to instantiate, and then find the set method.
+            request = ReflectUtils.newInstance(requestType);
+            for (Object arg : args) {
+                ReflectUtils.invoke(request,
+                        SET_METHOD_PREFIX + arg.getClass().getSimpleName(), arg);
+            }
+        } catch (Throwable e) {
+            //There is no remedy at this step, simply throw an exception.
+            throw new RequestCreateException(e);
+        }
+        return request;
+    }
+
+    /**
+     * Return the corresponding class generic carried by the request class.
      *
      * @param request Current request.
      * @return Object types,defaults to {@link HttpResultResponse}.
