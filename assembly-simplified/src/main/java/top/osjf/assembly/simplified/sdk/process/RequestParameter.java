bceb1e141951cb503d348a3c05731bdc1ad1639f
zhangpengfei <929160069@qq.com>
2024-06-13 10:33:44 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

---
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/process/RequestParameter.java
@@ -0,0 +1,33 @@
+package top.osjf.assembly.simplified.sdk.process;
+
+/**
+ * Only a single input parameter is supported to implement this
+ * interface to inform the proxy class of the type of request parameter.
+ *
+ * <p><h3>Examples of usage methods are as follows</h3></p>
+ * <pre>
+ *  public class QueryDto implements RequestParameter {
+ *
+ *      private String token;
+ *
+ *     &#064;Override
+ *     public Object getRequestType(){
+ *         return RequestImpl.class;
+ *     }
+ *  }
+ * </pre>
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @see RequestParam
+ * @since 2.2.6
+ */
+public interface RequestParameter {
+
+    /**
+     * Return the type of the request parameter.
+     *
+     * @return type of the request parameter.
+     */
+    @SuppressWarnings("rawtypes")
+    Class<? extends Request> getRequestType();
+}
