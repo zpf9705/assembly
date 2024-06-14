a870c5846812400580c671be8ea2a1f129f6d77b
zhangpengfei <929160069@qq.com>
2024-06-14 14:39:42 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

---
+++ assembly-util/src/main/java/top/osjf/assembly/util/lang/SimilarAble.java
@@ -0,0 +1,34 @@
+package top.osjf.assembly.util.lang;
+
+/**
+ * It is difficult not to associate with the canonical {@link String} type
+ * and related methods {@link String#equals(Object)} and {@link String#startsWith(String)}
+ * and {@link String#endsWith(String)} and {@link String#contains(CharSequence)}
+ * when comparing with the specified object being passed in.
+ *
+ * <p>Of course, it is only a canonical data type, and the definition of this
+ * interface can also be used to specify its own similarity conditions.
+ * This interface is not within the scope of JSR standards and is only
+ * used for scenario needs.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @see Similarator
+ * @since 1.1.4
+ */
+public interface SimilarAble<T> {
+
+    /**
+     * Compare the similarity between this object and the specified object.
+     * When the first parameter is similar to the second parameter, return
+     * {@code true}, otherwise return {@code false}.<p>
+     *
+     * @param o the object to be similared.
+     * @return a boolean type where true represents
+     * similarity and vice versa represents no similarity.
+     * @throws NullPointerException if an argument is null and this
+     *                              comparator does not permit null arguments
+     * @throws ClassCastException   if the arguments' types prevent them from
+     *                              being compared by this comparator.
+     */
+    boolean similarTo(T o);
+}
