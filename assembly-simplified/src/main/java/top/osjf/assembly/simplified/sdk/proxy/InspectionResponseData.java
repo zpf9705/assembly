db96e6aafd033894a2036fa64700e2bfaee059f3
zhangpengfei <929160069@qq.com>
2024-06-17 15:51:04 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

---
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/proxy/InspectionResponseData.java
@@ -0,0 +1,18 @@
+package top.osjf.assembly.simplified.sdk.proxy;
+
+/**
+ * The default extension {@link ResponseData} is to check
+ * whether the response is successful when the conditions
+ * are met.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 2.2.6
+ */
+public interface InspectionResponseData extends ResponseData {
+
+    /*** {@inheritDoc}*/
+    @Override
+    default boolean inspectionResponseResult() {
+        return true;
+    }
+}
