6ed4dac3e05f4613a2a18b5abfe3a5b98d707227
zhangpengfei <929160069@qq.com>
2024-06-17 14:11:36 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-cache/src/main/java/top/osjf/assembly/cache/factory/Center.java
+++ assembly-cache/src/main/java/top/osjf/assembly/cache/factory/Center.java
@@ -3,10 +3,11 @@ package top.osjf.assembly.cache.factory;
 /**
  * This interface is the core interface for implementing caching methods
  * and subsequent cache recovery and deletion of persistent files.
- * <p>
- * Inherited the help center (i.e. cache operation entity) {@link HelpCenter}
+ *
+ * <p>Inherited the help center (i.e. cache operation entity) {@link HelpCenter}
  * and recovery center (i.e. implementation of subsequent cache recovery
  * methods) {@link ReloadCenter}.
+ *
  * @param <C> The type of help center.
  * @param <K> The type of key.
  * @param <V> The type of value.
