e87bb34d84bf09de5386914b3f933a9f04e4ded0
zhangpengfei <929160069@qq.com>
2024-06-17 16:28:10 +0800

    --- Resolve the issue of AOP function loss caused by ServiceContextAwareBeanPostProcessor.

--- assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/process/InspectionResponseData.java
+++ assembly-simplified/src/main/java/top/osjf/assembly/simplified/sdk/process/InspectionResponseData.java
@@ -1,5 +1,10 @@
 package top.osjf.assembly.simplified.sdk.process;
 
+import top.osjf.assembly.simplified.sdk.SdkUtils;
+import top.osjf.assembly.util.annotation.CanNull;
+
+import java.lang.reflect.Method;
+
 /**
  * The default extension {@link ResponseData} is to check
  * whether the response is successful when the conditions
@@ -10,9 +15,24 @@ package top.osjf.assembly.simplified.sdk.process;
  */
 public interface InspectionResponseData extends ResponseData {
 
-    /** {@inheritDoc}*/
+    /**
+     * {@inheritDoc}
+     */
     @Override
     default boolean inspectionResponseResult() {
         return true;
     }
+
+    /**
+     * Check the placeholder data returned when the request fails,
+     * instead of {@literal null}, to enhance the applicability
+     * of SDK calls.
+     *
+     * <p>Analyze an example to see {@link SdkUtils#getResponse(Method, Response)}.
+     *
+     * @return Placeholder returns data, which may be a global default
+     * value,can be {@literal null}, adapted according to oneself.
+     */
+    @CanNull
+    Object failedSeatData();
 }
