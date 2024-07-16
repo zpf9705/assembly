6f8d77fd2bbaea6af6a0c69270400dcd6ded9989
zhangpengfei <929160069@qq.com>
2024-07-16 10:29:31 +0800

    Initialize the running information storage group of the SDK core project.

---
+++ assembly-simplified-sdk/sdk-core/src/main/java/top/osjf/sdk/core/exception/RequestCreateException.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.osjf.sdk.core.exception;
+
+import top.osjf.sdk.core.process.RequestParam;
+import top.osjf.sdk.core.process.RequestParameter;
+
+/**
+ * Create throw types for request class parameter exceptions based on
+ * {@link RequestParam} and {@link RequestParameter}.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.0
+ */
+public class RequestCreateException extends SdkException {
+
+    private static final long serialVersionUID = -7492000953956318499L;
+
+    public RequestCreateException(Throwable cause) {
+        super("Request class parameter creation failed.", cause);
+    }
+}
