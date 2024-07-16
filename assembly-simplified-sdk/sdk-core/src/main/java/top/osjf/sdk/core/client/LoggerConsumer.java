4aad3c5ea70821b398ce4cdc1f02bae95f0d34b1
zhangpengfei <929160069@qq.com>
2024-07-16 10:23:51 +0800

    Initialize the running information storage group of the SDK core project.

--- assembly-simplified-sdk/sdk-core/src/main/java/top/osjf/sdk/core/client/LoggerConsumer.java
+++ assembly-simplified-sdk/sdk-core/src/main/java/top/osjf/sdk/core/client/LoggerConsumer.java
@@ -1,5 +1,20 @@
-package top.osjf.sdk.core.client;
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package top.osjf.sdk.core.client;
 
 import top.osjf.sdk.core.exception.SdkException;
 
@@ -10,14 +25,14 @@ import java.util.function.BiConsumer;
  * using the function {@link BiConsumer}.
  *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
- * @since 1.1.0
+ * @since 1.0.0
  */
 public interface LoggerConsumer {
 
     /**
      * Regular log entries, user normal log output.
      *
-     * @return For example: {@code  org.slf4j.Logger#info(String, Object...)}
+     * @return For example: {@code org.slf4j.Logger#info(String, Object...)}
      */
     BiConsumer<String, Object[]> normal();
 
@@ -29,7 +44,7 @@ public interface LoggerConsumer {
     BiConsumer<String, Object[]> sdkError();
 
     /**
-     * Abnormal log output except for SDK.
+     * Unknown exception log output except for SDK.
      *
      * @return For example: {@code org.slf4j.Logger#error(String, Object...)}
      */
