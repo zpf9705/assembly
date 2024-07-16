52cce44f4d618f2ae3e63cec0966e221637af175
zhangpengfei <929160069@qq.com>
2024-07-16 10:24:34 +0800

    Initialize the running information storage group of the SDK core project.

--- assembly-simplified-sdk/sdk-core/src/main/java/top/osjf/sdk/core/enums/SdkEnum.java
+++ assembly-simplified-sdk/sdk-core/src/main/java/top/osjf/sdk/core/enums/SdkEnum.java
@@ -10,9 +10,10 @@ package top.osjf.sdk.core.enums;
 public interface SdkEnum {
 
     /**
-     * Returns the true request address of the SDK, using HTTP as an example, which is
-     * an HTTP protocol address that can be curled.
-     * <p>For RPC, it is estimated to only be the server address and port number.</p>
+     * Returns the true request address of the SDK, using HTTP as
+     * an example, which is an HTTP protocol address that can be curled.
+     * <p>For RPC, it is estimated to only be the server address
+     * and port number.</p>
      *
      * @param host The host name of the SDK.
      * @return The request address for the SDK.
@@ -20,10 +21,12 @@ public interface SdkEnum {
     String getUlr(String host);
 
     /**
-     * The name of the SDK request, which is a unique identifier name to distinguish
-     * between successful analysis or failure in the future, is not recommended to be {@literal null}.
+     * The name of the SDK request, which is a unique identifier name
+     * to distinguish between successful analysis or failure in the
+     * future, is not recommended to be {@literal null}.
      *
-     * @return If it is an enumeration, simply rewrite {@link Enum#name()}, and the rest can be customized.
+     * @return If it is an enumeration, simply rewrite {@link Enum#name()},
+     * and the rest can be customized.
      */
     String name();
 }
