643c389ffe6d280ca4ea1c4f8e9d19207f466c7e
zhangpengfei <929160069@qq.com>
2024-07-16 11:43:46 +0800

    Initialize SDK HTTP related components.

---
+++ assembly-simplified-sdk/sdk-http/src/main/java/top/osjf/sdk/http/HttpRequestMethod.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.osjf.sdk.http;
+
+import top.osjf.sdk.commons.annotation.NotNull;
+
+import java.util.Map;
+
+/**
+ * Enumeration of HTTP request types.
+ * <p>Implement the {@link HttpSdkEnum.Action} interface to define the
+ * action methods for each request method.</p>
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.0
+ */
+public enum HttpRequestMethod implements HttpSdkEnum.Action {
+
+    GET {
+        @Override
+        public String doRequest(@NotNull Type type, @NotNull String url, Map<String, String> headers,
+                                Object requestParam, Boolean montage) throws Exception {
+            return type.getInstance().get(url, headers, requestParam, montage);
+        }
+    }, POST {
+        @Override
+        public String doRequest(@NotNull Type type, @NotNull String url, Map<String, String> headers,
+                                Object requestParam, Boolean montage) throws Exception {
+            return type.getInstance().post(url, headers, requestParam, montage);
+        }
+    }, PUT {
+        @Override
+        public String doRequest(@NotNull Type type, @NotNull String url, Map<String, String> headers,
+                                Object requestParam, Boolean montage) throws Exception {
+            return type.getInstance().put(url, headers, requestParam, montage);
+        }
+    }, DELETE {
+        @Override
+        public String doRequest(@NotNull Type type, @NotNull String url, Map<String, String> headers,
+                                Object requestParam, Boolean montage) throws Exception {
+            return type.getInstance().delete(url, headers, requestParam, montage);
+        }
+    };
+
+    @Override
+    public String doRequest(String url, Map<String, String> headers, Object requestParam, Boolean montage) {
+        throw new UnsupportedOperationException();
+    }
+}
