ae4afb689c98cc6571d8e6913e800ffe0ca24013
zhangpengfei <929160069@qq.com>
2024-07-17 11:14:02 +0800

    Initialize CRON spring related components.

--- assembly-simplified-cron/cron-core/src/main/java/top/osjf/cron/core/exception/CronLifeStyleException.java
+++ assembly-simplified-cron/cron-core/src/main/java/top/osjf/cron/core/exception/CronLifeStyleException.java
@@ -17,16 +17,16 @@
 package top.osjf.cron.core.exception;
 
 /**
- * Throw an exception if the scheduled task has already been initiated.
+ * Scheduled task lifecycle exception throw class.
  *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.0.0
  */
-public class CronIsStartedException extends CronException {
+public class CronLifeStyleException extends CronException {
 
     private static final long serialVersionUID = 7007036399239003429L;
 
-    public CronIsStartedException() {
-        super("Scheduler has been started, please stop it first!");
+    public CronLifeStyleException(String message) {
+        super(message);
     }
 }
