88b8aea796aa3a55d53089df86147e2bb5819967
zhangpengfei <929160069@qq.com>
2024-07-17 10:51:53 +0800

    Initialize CRON spring related components.

---
+++ assembly-simplified-cron/cron-core/src/main/java/top/osjf/cron/core/annotation/NotNull.java
@@ -0,0 +1,26 @@
+package top.osjf.cron.core.annotation;
+
+import javax.annotation.Nonnull;
+import javax.annotation.meta.TypeQualifierNickname;
+import java.lang.annotation.*;
+
+/**
+ * Copy form spring framework.
+ * A common annotation to declare that annotated elements cannot be {@code null}.
+ *
+ * <p>Leverages JSR-305 meta-annotations to indicate nullability in Java to common
+ * tools with JSR-305 support.
+ *
+ * <p>Should be used at parameter, return value, and field level. Method overrides should
+ * repeat parent {@code @NotNull} annotations unless they behave differently.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.0
+ */
+@Target({ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Nonnull
+@TypeQualifierNickname
+public @interface NotNull {
+}
