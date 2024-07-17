5ebdc4b7ce1f52f1696228a07b0c1317d6318b32
zhangpengfei <929160069@qq.com>
2024-07-17 13:11:50 +0800

    Initialize CRON spring related components.

---
+++ assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/configuration/HutoolCronTaskConfiguration.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.osjf.cron.spring.configuration;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
+import top.osjf.cron.core.lifestyle.LifeStyle;
+import top.osjf.cron.core.repository.CronTaskRepository;
+import top.osjf.cron.hutool.lifestyle.HutoolCronLifeStyle;
+import top.osjf.cron.hutool.repository.HutoolCronTaskRepository;
+
+/**
+ * Regarding the configuration classes related to scheduled task
+ * registration for hutool.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.0
+ */
+@Configuration(proxyBeanMethods = false)
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+public class HutoolCronTaskConfiguration {
+
+    @Bean
+    public LifeStyle lifeStyle() {
+        return new HutoolCronLifeStyle();
+    }
+
+    @Bean
+    public CronTaskRepository cronTaskRepository() {
+        return new HutoolCronTaskRepository();
+    }
+}
