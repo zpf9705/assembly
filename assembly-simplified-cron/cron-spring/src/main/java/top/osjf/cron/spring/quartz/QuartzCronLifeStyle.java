724cf63931ecafe05c3e37f60c7d84a8fb07a4d1
zhangpengfei <929160069@qq.com>
2024-07-18 16:52:33 +0800

    Initialize CRON spring related components.

---
+++ assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/quartz/QuartzCronLifeStyle.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.osjf.cron.spring.quartz;
+
+import org.quartz.Scheduler;
+import org.springframework.beans.factory.DisposableBean;
+
+/**
+ * Integrate Spring's {@link DisposableBean} to trigger timed task shutdown.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.0
+ */
+public class QuartzCronLifeStyle extends top.osjf.cron.quartz.lifestyle.QuartzCronLifeStyle
+        implements DisposableBean {
+
+    /**
+     * Construct for create {@link QuartzCronLifeStyle} using a {@link Scheduler}.
+     *
+     * @param scheduler {@link Scheduler}.
+     */
+    public QuartzCronLifeStyle(Scheduler scheduler) {
+        super(scheduler);
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        stop();
+    }
+}
