cfbc88b1477e4a7cca1970ae0afa234b95ce7140
zhangpengfei <929160069@qq.com>
2024-07-22 12:57:31 +0800

    Initialize CRON spring related components.

---
+++ assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/AbstractCronTaskRealRegistrant.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.osjf.cron.spring;
+
+import top.osjf.cron.core.repository.CronTaskRepository;
+
+/**
+ * Abstract {@link CronTaskRealRegistrant} provides some common constructs
+ * and methods for subclasses.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.0
+ */
+@SuppressWarnings("rawtypes")
+public abstract class AbstractCronTaskRealRegistrant implements CronTaskRealRegistrant {
+
+    private final CronTaskRepository cronTaskRepository;
+
+    /**
+     * Construct within {@link CronTaskRepository} to be a {@link CronTaskRealRegistrant}.
+     *
+     * @param cronTaskRepository Scheduled task resource operation class.
+     */
+    public AbstractCronTaskRealRegistrant(CronTaskRepository cronTaskRepository) {
+        this.cronTaskRepository = cronTaskRepository;
+    }
+
+    /**
+     * Return the timed task resource operation interface.
+     *
+     * @param <T> Specific types of resources.
+     * @return timed task resource operation interface.
+     */
+    @SuppressWarnings("unchecked")
+    public <T extends CronTaskRepository> T getCronTaskRepository() {
+        return (T) cronTaskRepository;
+    }
+
+    /**
+     * The generics applicable to {@link CronTaskRepository} are the
+     * registration processing of {@link RunnableRegistrant}.
+     *
+     * @throws Exception {@inheritDoc}
+     */
+    @Override
+    public void register(Registrant registrant) throws Exception {
+        if (registrant instanceof RunnableRegistrant) {
+            CronTaskRepository<String, Runnable> sRCronTaskRepository = getCronTaskRepository();
+            RunnableRegistrant runnableRegistrant = (RunnableRegistrant) registrant;
+            sRCronTaskRepository.register(runnableRegistrant.getCronExpression(), runnableRegistrant.getRunnable());
+        }
+    }
+}
