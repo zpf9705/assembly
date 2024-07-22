5a5f00cd47c44935780dd52e011541289d1d921b
zhangpengfei <929160069@qq.com>
2024-07-22 12:39:54 +0800

    Initialize CRON spring related components.

---
+++ assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/RegistrantCollector.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.osjf.cron.spring;
+
+import org.springframework.core.env.Environment;
+
+import java.util.Iterator;
+
+/**
+ * Add function to the condition of registering body {@link Registrant}.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.0
+ */
+public interface RegistrantCollector extends Iterator<Registrant> {
+
+    /**
+     * Add a scheduled task based on the bean's conditions.
+     *
+     * @param realBeanType The true type of bean.
+     * @param bean         Beans awaiting registration and inspection.
+     * @param environment  Spring's environment variables.
+     * @throws Exception Possible abnormal issues that may arise from
+     *                   registration behavior.
+     */
+    void add(Class<?> realBeanType, Object bean, Environment environment) throws Exception;
+}
