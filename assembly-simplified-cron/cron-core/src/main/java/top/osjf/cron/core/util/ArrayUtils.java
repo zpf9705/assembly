631a95218ad1ce52945324ed3e6e58587f95774c
zhangpengfei <929160069@qq.com>
2024-07-23 15:19:13 +0800

    Initialize CRON spring related components.

---
+++ assembly-simplified-cron/cron-core/src/main/java/top/osjf/cron/core/util/ArrayUtils.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright org.apache.commons.lang3.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.osjf.cron.core.util;
+
+import java.lang.reflect.Array;
+
+/**
+ * Copy from {@code org.apache.commons.lang3.ArrayUtils}
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.0
+ */
+public class ArrayUtils {
+
+    /**
+     * <p>Checks if an array of Objects is empty or {@code null}.
+     *
+     * @param array the array to test
+     * @return {@code true} if the array is empty or {@code null}
+     * @since {@code org.apache.commons.lang3.ArrayUtils} 2.1
+     */
+    public static boolean isEmpty(final Object[] array) {
+        return getLength(array) == 0;
+    }
+
+    /**
+     * <p>Returns the length of the specified array.
+     * This method can deal with {@code Object} arrays and with primitive arrays.
+     *
+     * <p>If the input array is {@code null}, {@code 0} is returned.
+     *
+     * <pre>
+     * ArrayUtils.getLength(null)            = 0
+     * ArrayUtils.getLength([])              = 0
+     * ArrayUtils.getLength([null])          = 1
+     * ArrayUtils.getLength([true, false])   = 2
+     * ArrayUtils.getLength([1, 2, 3])       = 3
+     * ArrayUtils.getLength(["a", "b", "c"]) = 3
+     * </pre>
+     *
+     * @param array the array to retrieve the length from, may be null
+     * @return The length of the array, or {@code 0} if the array is {@code null}
+     * @throws IllegalArgumentException if the object argument is not an array.
+     * @since {@code org.apache.commons.lang3.ArrayUtils} 2.1
+     */
+    public static int getLength(final Object array) {
+        if (array == null) {
+            return 0;
+        }
+        return Array.getLength(array);
+    }
+}
