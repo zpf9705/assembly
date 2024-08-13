fea103d623dad1bccc3b90b0f2a096f05bd6b14a
zhangpengfei <929160069@qq.com>
2024-08-13 11:33:32 +0800

    Initialize SDK spring related components.

--- assembly-simplified-sdk/sdk-spring/src/main/java/top/osjf/sdk/spring/proxy/ProxyUtils.java
+++ assembly-simplified-sdk/sdk-spring/src/main/java/top/osjf/sdk/spring/proxy/ProxyUtils.java
@@ -18,6 +18,7 @@ package top.osjf.sdk.spring.proxy;
 
 import org.springframework.cglib.proxy.Callback;
 import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.lang.NonNull;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
@@ -44,6 +45,31 @@ public abstract class ProxyUtils {
     }
 
     /**
+     * Using a {@code class} and {@code InvocationHandler} build a JDK dynamic proxy object.
+     *
+     * @param args The conditions for creating a proxy object.
+     * @param <T>  type.
+     * @return JDK dynamic proxy object
+     */
+    public static <T> T createJdkProxy(@NonNull Object... args) {
+        Class<T> type = null;
+        InvocationHandler invocationHandler = null;
+        for (Object arg : args) {
+            if (type != null && invocationHandler != null) {
+                break;
+            }
+            if (arg instanceof Class) {
+                type = (Class<T>) arg;
+            }
+            if (arg instanceof InvocationHandler) {
+                invocationHandler = (InvocationHandler) arg;
+            }
+        }
+        if (type == null || invocationHandler == null) throw new IllegalArgumentException();
+        return createJdkProxy(type, invocationHandler);
+    }
+
+    /**
      * Using a {@code class} and {@code Callback} build a Cglib dynamic proxy object.
      *
      * @param type     Proxy type.
@@ -56,5 +82,30 @@ public abstract class ProxyUtils {
         enhancer.setSuperclass(type);
         enhancer.setCallback(callback);
         return (T) enhancer.create();
+    }
+
+    /**
+     * Using a {@code class} and {@code Callback} build a Cglib dynamic proxy object.
+     *
+     * @param args The conditions for creating a proxy object.
+     * @param <T>  type.
+     * @return Cglib dynamic proxy object.
+     */
+    public static <T> T createCglibProxy(@NonNull Object... args) {
+        Class<T> type = null;
+        Callback callback = null;
+        for (Object arg : args) {
+            if (type != null && callback != null) {
+                break;
+            }
+            if (arg instanceof Class) {
+                type = (Class<T>) arg;
+            }
+            if (arg instanceof Callback) {
+                callback = (Callback) arg;
+            }
+        }
+        if (type == null || callback == null) throw new IllegalArgumentException();
+        return createCglibProxy(type, callback);
     }
 }
