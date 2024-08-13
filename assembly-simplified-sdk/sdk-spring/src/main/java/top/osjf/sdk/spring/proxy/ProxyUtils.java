b41995cf26bff44fc135e70563c8ac681f307c54
zhangpengfei <929160069@qq.com>
2024-08-13 11:19:46 +0800

    Initialize SDK spring related components.

---
+++ assembly-simplified-sdk/sdk-spring/src/main/java/top/osjf/sdk/spring/proxy/ProxyUtils.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.osjf.sdk.spring.proxy;
+
+import org.springframework.cglib.proxy.Callback;
+import org.springframework.cglib.proxy.Enhancer;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+/**
+ * Proxy related tools.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.0
+ */
+@SuppressWarnings("unchecked")
+public abstract class ProxyUtils {
+
+    /**
+     * Using a {@code class} and {@code InvocationHandler} build a JDK dynamic proxy object.
+     *
+     * @param clazz             Proxy type.
+     * @param invocationHandler Callback interface.
+     * @param <T>               type.
+     * @return JDK dynamic proxy object
+     */
+    public static <T> T createJdkProxy(Class<T> clazz, InvocationHandler invocationHandler) {
+        return (T) Proxy.newProxyInstance(clazz.getClassLoader(), new Class[]{clazz}, invocationHandler);
+    }
+
+    /**
+     * Using a {@code class} and {@code Callback} build a Cglib dynamic proxy object.
+     *
+     * @param type     Proxy type.
+     * @param callback Callback interface.
+     * @param <T>      type.
+     * @return Cglib dynamic proxy object.
+     */
+    public static <T> T createCglibProxy(Class<T> type, Callback callback) {
+        final Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(type);
+        enhancer.setCallback(callback);
+        return (T) enhancer.create();
+    }
+}
