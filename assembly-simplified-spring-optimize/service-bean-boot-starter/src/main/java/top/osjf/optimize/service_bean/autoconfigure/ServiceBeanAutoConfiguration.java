fd73b0e6a1ac1afa52069ae665b2b58dc5e5c288
zhangpengfei <929160069@qq.com>
2024-08-19 16:33:52 +0800

    Initialize SDK spring related components.

---
+++ assembly-simplified-spring-optimize/service-bean-boot-starter/src/main/java/top/osjf/optimize/service_bean/autoconfigure/ServiceBeanAutoConfiguration.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.osjf.optimize.service_bean.autoconfigure;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
+import top.osjf.optimize.service_bean.annotation.EnableServiceCollection;
+import top.osjf.optimize.service_bean.context.ServiceContext;
+
+/**
+ * {@link org.springframework.boot.autoconfigure.EnableAutoConfiguration} for Service bean.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.0
+ */
+@Configuration(proxyBeanMethods = false)
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+@EnableServiceCollection
+@ConditionalOnMissingBean(ServiceContext.class)
+public class ServiceBeanAutoConfiguration {
+}
