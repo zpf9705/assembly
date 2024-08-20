b34bb5b61ac1df503e8afafbc7d9f763dd95eede
zhangpengfei <929160069@qq.com>
2024-08-20 16:24:25 +0800

    Initialize SDK spring related components.

--- assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/quartz/QuartzPropertiesGainer.java
+++ assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/quartz/QuartzPropertiesGainer.java
@@ -20,19 +20,45 @@ package top.osjf.cron.spring.quartz;
 import java.util.Properties;
 
 /**
- * The interface for gain quartz setting properties.
+ * Quartz property retriever interface.
+ * <p>Used to obtain the configuration properties required for Quartz job scheduler.
  *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.0.1
  */
 public interface QuartzPropertiesGainer {
 
+    /**
+     * Get the configuration properties of Quartz job scheduler.
+     *
+     * <p>The implementation of this method should return a 'Properties' object
+     * containing the required configuration properties for Quartz.
+     *
+     * <p>These properties will be used to configure the behavior of the Quartz job scheduler.
+     *
+     * @return The configuration properties of the  Quartz job scheduler.
+     */
     Properties getQuartzProperties();
 
+    /**
+     * The factory method is used to create a QuartzProperties Gainer instance using a given Properties object.
+     *
+     * <p>This method provides a convenient way to quickly instantiated a QuartzProperties Gainer,
+     * The 'Properties' object directly serves as the configuration source for Quartz.
+     *
+     * @param properties The configuration properties of Quartz.
+     * @return Use the QuartzProperties Gainer instance implemented with the given Properties.
+     */
     static QuartzPropertiesGainer of(Properties properties) {
         return new DefaultQuartzPropertiesGainer(properties);
     }
 
+    /**
+     * The default implementation of Quartz property retriever.
+     * <p>
+     * This class implements the QuartzProperties Gainer interface and encapsulates a Properties object,
+     * Used to store and provide configuration properties for Quartz job scheduler.
+     */
     class DefaultQuartzPropertiesGainer implements QuartzPropertiesGainer {
 
         private final Properties properties;
