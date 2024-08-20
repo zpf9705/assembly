6b8f4ae1432f69e7b8ef8af0919a1a615df666ed
zhangpengfei <929160069@qq.com>
2024-08-20 14:21:05 +0800

    Initialize SDK spring related components.

---
+++ assembly-simplified-cron/cron-spring-boot-autoconfigure/src/main/java/top/osjf/cron/autoconfigure/QuartzPropertiesCustomizer.java
@@ -0,0 +1,19 @@
+package top.osjf.cron.autoconfigure;
+
+import java.util.Properties;
+
+/**
+ * Callback interface that can be implemented by beans wishing to customize Quartz's
+ * {@link java.util.Properties Quartz Properties} before it is used.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.0
+ */
+public interface QuartzPropertiesCustomizer {
+
+    /**
+     * Customize the Quartz Properties.
+     * @param properties the {@link Properties} to customize.
+     */
+    void customize(Properties properties);
+}
