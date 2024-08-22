d43d908afac87512cf5eef83d5f14c0e86881bab
zhangpengfei <929160069@qq.com>
2024-08-22 10:57:59 +0800

    Automatic assembly of cron tasks based on springboot.

---
+++ assembly-spring-boot-autoconfigure/src/main/java/top/osjf/spring/autoconfigure/cron/ConfigurableCronTaskRegisterPostProcessor.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.osjf.spring.autoconfigure.cron;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.ReflectionUtils;
+import top.osjf.cron.core.util.ArrayUtils;
+import top.osjf.cron.spring.CronTaskRegisterPostProcessor;
+import top.osjf.cron.spring.RegistrantCollector;
+
+import java.lang.reflect.Method;
+
+/**
+ * Configurable {@link CronTaskRegisterPostProcessor} for more flexible assembly expansion.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.1
+ */
+public class ConfigurableCronTaskRegisterPostProcessor extends CronTaskRegisterPostProcessor {
+
+    @Override
+    protected boolean advanceApprovalOfCondition(Class<?> realBeanType) {
+        return super.advanceApprovalOfCondition(realBeanType)
+                || advanceApprovalOfCondition0(realBeanType);
+    }
+
+    /**
+     * By determining that its target method is to create a bean and return a {@link RegistrantCollector} type.
+     *
+     * @param realBeanType bean real type.
+     * @return If {@code true} skips registration and returns registration detection.
+     */
+    private boolean advanceApprovalOfCondition0(Class<?> realBeanType) {
+        Method[] declaredMethods = ReflectionUtils.getDeclaredMethods(realBeanType);
+        if (!ArrayUtils.isEmpty(declaredMethods)) {
+            for (Method declaredMethod : declaredMethods) {
+                if (declaredMethod.isAnnotationPresent(Bean.class) &&
+                        super.advanceApprovalOfCondition(declaredMethod.getReturnType())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ assembly-spring-boot-autoconfigure/src/main/java/top/osjf/spring/autoconfigure/cron/HutoolCronTaskImportConfiguration.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.osjf.spring.autoconfigure.cron;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import top.osjf.cron.core.lifestyle.StartupProperties;
+import top.osjf.cron.hutool.lifestyle.HutoolCronLifeStyle;
+import top.osjf.cron.hutool.repository.HutoolCronTaskRepository;
+import top.osjf.cron.spring.hutool.HutoolCronTaskConfiguration;
+
+/**
+ * {@link Import Import Configuration} for Hutool Cron Task.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.1
+ */
+@Configuration(proxyBeanMethods = false)
+@Import(HutoolCronTaskConfiguration.class)
+@ConditionalOnClass({HutoolCronLifeStyle.class, HutoolCronTaskRepository.class})
+@ConditionalOnProperty(name = "spring.schedule.cron.client-type", havingValue = "hutool", matchIfMissing = true)
+public class HutoolCronTaskImportConfiguration {
+
+    @Bean
+    public StartupProperties hutoolStartupProperties(CronProperties cronProperties) {
+        return StartupProperties.of(cronProperties.getHutool().toMetadata());
+    }
+}
