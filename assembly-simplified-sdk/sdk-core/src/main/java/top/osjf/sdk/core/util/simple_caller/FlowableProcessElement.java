0ae6e29f653bad831ca26287ccd1f1422d0b2ffc
zhangpengfei <929160069@qq.com>
2024-10-08 16:03:09 +0800

    Add an overload to the core package of SDK for calls to rxjava3

---
+++ assembly-simplified-sdk/sdk-core/src/main/java/top/osjf/sdk/core/util/simple_caller/FlowableProcessElement.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.osjf.sdk.core.util.simple_caller;
+
+import top.osjf.sdk.core.process.Response;
+
+import java.util.function.Consumer;
+
+/**
+ * The {@code FlowableProcessElement} interface defines custom subscription behavior related to
+ * flow processing elements.
+ *
+ * <p>It provides custom consumers for normal response results and exceptions that occur during the
+ * subscription process.
+ * By implementing this interface, specific behaviors can be configured for stream processing
+ * elements to execute corresponding logic when receiving normal responses or encountering exceptions.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.2
+ */
+public interface FlowableProcessElement<R extends Response> {
+
+    /**
+     * Customized subscription for regular consumers, used to handle normal response results.
+     *
+     * @return Customized subscription for regular consumers.
+     */
+    Consumer<R> getCustomSubscriptionRegularConsumer();
+
+    /*** Custom subscription exception consumers, used to handle exceptions that occur during the
+     * subscription process.
+     * @return Custom subscription exception consumers.
+     * */
+    Consumer<Throwable> getCustomSubscriptionExceptionConsumer();
+}
