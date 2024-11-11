e58a131f4513af8cd77954cb285b10192cc7ba1c
zhangpengfei <929160069@qq.com>
2024-11-11 13:54:37 +0800

    Add an overload to the core package of SDK for calls to rxjava3

---
+++ assembly-simplified-sdk/sdk-core/src/main/java/top/osjf/sdk/core/util/SynchronizedWeakHashMap.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.osjf.sdk.core.util;
+
+import com.google.common.collect.ForwardingMap;
+
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+/**
+ * A thread-safe implementation of WeakHashMap. It wraps a WeakHashMap and uses the
+ * {@code Collections.synchronizedMap()} method to ensure that all accesses are synchronized,
+ * thereby achieving thread safety. This class retains all the characteristics of WeakHashMap,
+ * including the use of weak references to store keys, which allows the garbage collector to
+ * reclaim them when they are no longer referenced by other strong references.
+ * <p>
+ * Note: While this method provides thread-safe access, if multiple threads modify this map
+ * concurrently, additional synchronization measures may be required to ensure thread safety.
+ * For example, if the map is modified during iteration (except through the iterators own remove
+ * method), external synchronization must be performed.
+ *
+ * @see WeakHashMap
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.2
+ */
+public class SynchronizedWeakHashMap<K, V> extends ForwardingMap<K, V> implements Map<K, V> {
+
+    private final Map<K, V> delegate;
+
+    /**
+     * Constructs a new, empty <tt>SynchronizedWeakHashMap</tt> with the default initial
+     * capacity (16) and load factor (0.75).
+     */
+    public SynchronizedWeakHashMap() {
+        delegate = Collections.synchronizedMap(new WeakHashMap<>());
+    }
+
+    /**
+     * Constructs a new, empty <tt>SynchronizedWeakHashMap</tt> with the given initial
+     * capacity and the default load factor (0.75).
+     *
+     * @param initialCapacity The initial capacity of the <tt>WeakHashMap</tt>
+     * @throws IllegalArgumentException if the initial capacity is negative
+     */
+    public SynchronizedWeakHashMap(int initialCapacity) {
+        delegate = Collections.synchronizedMap(new WeakHashMap<>(initialCapacity));
+    }
+
+    /**
+     * Constructs a new <tt>SynchronizedWeakHashMap</tt> with the same mappings as the
+     * specified map.  The <tt>WeakHashMap</tt> is created with the default
+     * load factor (0.75) and an initial capacity sufficient to hold the
+     * mappings in the specified map.
+     *
+     * @param m the map whose mappings are to be placed in this map
+     * @throws NullPointerException if the specified map is null
+     */
+    public SynchronizedWeakHashMap(Map<? extends K, ? extends V> m) {
+        delegate = Collections.synchronizedMap(new WeakHashMap<>(m));
+    }
+
+    /**
+     * Constructs a new, empty <tt>SynchronizedWeakHashMap</tt> with the given initial
+     * capacity and the given load factor.
+     *
+     * @param initialCapacity The initial capacity of the <tt>WeakHashMap</tt>
+     * @param loadFactor      The load factor of the <tt>WeakHashMap</tt>
+     * @throws IllegalArgumentException if the initial capacity is negative,
+     *                                  or if the load factor is nonpositive.
+     */
+    public SynchronizedWeakHashMap(int initialCapacity, float loadFactor) {
+        delegate = Collections.synchronizedMap(new WeakHashMap<>(initialCapacity, loadFactor));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @Nonnull
+    protected Map<K, V> delegate() {
+        return delegate;
+    }
+}
