3254970e6a04e89e3bfb61edf396bcb2fe910171
zhangpengfei <929160069@qq.com>
2024-11-20 17:34:57 +0800

    Add an overload to the core package of SDK for calls to rxjava3

--- assembly-simplified-sdk/sdk-http/src/main/java/top/osjf/sdk/http/HttpRequestExecutor.java
+++ assembly-simplified-sdk/sdk-http/src/main/java/top/osjf/sdk/http/HttpRequestExecutor.java
@@ -21,26 +21,8 @@ import feign.Request;
 import feign.Response;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Map;
 
 /**
- * The HTTP request executor interface defines various HTTP request methods and provides a
- * unified request execution method.
- * <p>
- * This interface allows mapping lowercase HTTP method names to specific methods of the
- * current class through a unified {@link #unifiedDoRequest} method,And execute the corresponding
- * HTTP request. In addition, GET, POST, PUT, DELETE, TRACE,OPTIONS, HEAD, PATCH, etc. are
- * directly defined Specific HTTP request methods.
- * <p>
- * In each request method, it is allowed to specify the request address (URL), request headers,
- * request parameters (params), and Whether to concatenate the parameters in Map or Json format
- * to the URL and use them as a key value pair (month).
- * <p>
- * All request methods return a return value of type String and may throw exceptions.
- *
- *
- * <p>
  * The HttpRequestExecutor interface inherits from the feature client interface since version 1.0.2,
  * Adopting open label technology support for executing HTTP requests.
  * <p>
@@ -51,7 +33,7 @@ import java.util.Map;
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.0.0
  */
-public interface HttpRequestExecutor extends Client {
+public interface HttpRequestExecutor extends Client, CustomizeHttpRequestExecutor {
     /**
      * {@inheritDoc}
      *
@@ -65,146 +47,16 @@ public interface HttpRequestExecutor extends Client {
     Response execute(Request request, Request.Options options) throws IOException;
 
     /**
-     * Use lowercase HTTP method names to map the request method names of
-     * the current class and make unified method calls.
+     * Return whether to use the custom {@link CustomizeHttpRequestExecutor}
+     * request method.
+     * <p>By default, the open flag client parameter leader {@link #execute}
+     * scheme is used，that is to say, the return value is {@code false}.
+     * <p>Set the return value to {@code true} to execute custom logic.
      *
-     * @param methodName Http method name map to this class method name.
-     * @param url        The actual request address,must not be {@literal null}.
-     * @param headers    Header information map,can be {@literal null}.
-     * @param param      Request parameters with type {@link Object},can be {@literal null}.
-     * @param montage    Whether to concatenate parameters in the form of {@link Map} or {@code Json}
-     *                   as key/value after the URL.
-     * @return The {@code String} type of the return value
-     * @throws Exception unknown exception.
+     * @return whether to use the custom {@link CustomizeHttpRequestExecutor}
+     * request method.
      */
-    @Deprecated
-    default String unifiedDoRequest(String methodName,
-                                    String url,
-                                    Map<String, String> headers, Object param, boolean montage)
-            throws Exception {
-        try {
-            return getClass().getMethod(methodName,
-                    String.class,
-                    Map.class,
-                    Object.class,
-                    boolean.class).invoke(this, url, headers, param, montage).toString();
-        } catch (InvocationTargetException e) {
-            //If it is an execution exception of InvocationTargetException,
-            // convert it to a reflection exception of the parent class and
-            // put the target exception in the cause.
-            throw new ReflectiveOperationException(e.getTargetException());
-        }
+    default boolean useCustomize() {
+        return false;
     }
-
-    /**
-     * HTTP request method for {@code Get}.
-     *
-     * @param url     The actual request address,must not be {@literal null}.
-     * @param headers Header information map,can be {@literal null}.
-     * @param param   Request parameters with type {@link Object},can be {@literal null}.
-     * @param montage Whether to concatenate parameters in the form of {@link Map} or {@code Json}
-     *                as key/value after the URL.
-     * @return The {@code String} type of the return value
-     * @throws Exception unknown exception.
-     */
-    @Deprecated
-    String get(String url, Map<String, String> headers, Object param, boolean montage) throws Exception;
-
-    /**
-     * HTTP request method for {@code Post}.
-     *
-     * @param url     The actual request address,must not be {@literal null}.
-     * @param headers Header information map,can be {@literal null}.
-     * @param param   Request parameters with type {@link Object},can be {@literal null}.
-     * @param montage Whether to concatenate parameters in the form of {@link Map} or {@code Json}
-     *                as key/value after the URL.
-     * @return The {@code String} type of the return value
-     * @throws Exception unknown exception.
-     */
-    @Deprecated
-    String post(String url, Map<String, String> headers, Object param, boolean montage) throws Exception;
-
-    /**
-     * HTTP request method for {@code Put}.
-     *
-     * @param url     The actual request address,must not be {@literal null}.
-     * @param headers Header information map,can be {@literal null}.
-     * @param param   Request parameters with type {@link Object},can be {@literal null}.
-     * @param montage Whether to concatenate parameters in the form of {@link Map} or {@code Json}
-     *                as key/value after the URL.
-     * @return The {@code String} type of the return value
-     * @throws Exception unknown exception.
-     */
-    @Deprecated
-    String put(String url, Map<String, String> headers, Object param, boolean montage) throws Exception;
-
-    /**
-     * HTTP request method for {@code Delete}.
-     *
-     * @param url     The actual request address,must not be {@literal null}.
-     * @param headers Header information map,can be {@literal null}.
-     * @param param   Request parameters with type {@link Object},can be {@literal null}.
-     * @param montage Whether to concatenate parameters in the form of {@link Map} or {@code Json}
-     *                as key/value after the URL.
-     * @return The {@code String} type of the return value
-     * @throws Exception unknown exception.
-     */
-    @Deprecated
-    String delete(String url, Map<String, String> headers, Object param, boolean montage) throws Exception;
-
-    /**
-     * HTTP request method for {@code trace}.
-     *
-     * @param url     The actual request address,must not be {@literal null}.
-     * @param headers Header information map,can be {@literal null}.
-     * @param param   Request parameters with type {@link Object},can be {@literal null}.
-     * @param montage Whether to concatenate parameters in the form of {@link Map} or {@code Json}
-     *                as key/value after the URL.
-     * @return The {@code String} type of the return value
-     * @throws Exception unknown exception.
-     */
-    @Deprecated
-    String trace(String url, Map<String, String> headers, Object param, boolean montage) throws Exception;
-
-    /**
-     * HTTP request method for {@code options}.
-     *
-     * @param url     The actual request address,must not be {@literal null}.
-     * @param headers Header information map,can be {@literal null}.
-     * @param param   Request parameters with type {@link Object},can be {@literal null}.
-     * @param montage Whether to concatenate parameters in the form of {@link Map} or {@code Json}
-     *                as key/value after the URL.
-     * @return The {@code String} type of the return value
-     * @throws Exception unknown exception.
-     */
-    @Deprecated
-    String options(String url, Map<String, String> headers, Object param, boolean montage) throws Exception;
-
-    /**
-     * HTTP request method for {@code head}.
-     *
-     * @param url     The actual request address,must not be {@literal null}.
-     * @param headers Header information map,can be {@literal null}.
-     * @param param   Request parameters with type {@link Object},can be {@literal null}.
-     * @param montage Whether to concatenate parameters in the form of {@link Map} or {@code Json}
-     *                as key/value after the URL.
-     * @return The {@code String} type of the return value
-     * @throws Exception unknown exception.
-     */
-    @Deprecated
-    String head(String url, Map<String, String> headers, Object param, boolean montage) throws Exception;
-
-    /**
-     * HTTP request method for {@code patch}.
-     *
-     * @param url     The actual request address,must not be {@literal null}.
-     * @param headers Header information map,can be {@literal null}.
-     * @param param   Request parameters with type {@link Object},can be {@literal null}.
-     * @param montage Whether to concatenate parameters in the form of {@link Map} or {@code Json}
-     *                as key/value after the URL.
-     * @return The {@code String} type of the return value
-     * @throws Exception unknown exception.
-     */
-    @Deprecated
-    String patch(String url, Map<String, String> headers, Object param, boolean montage) throws Exception;
 }
