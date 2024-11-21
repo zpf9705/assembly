07f1855a96cf2ccf3d3465be363485590d67870c
zhangpengfei <929160069@qq.com>
2024-11-21 14:40:38 +0800

    Add component support for SDK for HTTP requests.

--- assembly-simplified-sdk/sdk-http-google/src/main/java/top/osjf/sdk/http/google/GoogleHttpRequestExecutor.java
+++ assembly-simplified-sdk/sdk-http-google/src/main/java/top/osjf/sdk/http/google/GoogleHttpRequestExecutor.java
@@ -39,20 +39,77 @@ import java.io.IOException;
  */
 @LoadOrder(Integer.MIN_VALUE + 12)
 public class GoogleHttpRequestExecutor extends UnsupportedCustomizeHttpRequestExecutor implements HttpRequestExecutor {
+    /**
+     * Define a private final variable of type {@code GoogleHttpClient} for executing HTTP requests
+     */
     private final GoogleHttpClient googleHttpClient;
 
+    /**
+     * A no-argument constructor that initializes the object with a default
+     * {@code GoogleHttpClient} instance.
+     * <p>
+     * This is a convenience constructor that internally calls a constructor
+     * with a {@code GoogleHttpClient} parameter,passing a newly created
+     * {@code GoogleHttpClient} instance with default configuration.
+     */
     public GoogleHttpRequestExecutor() {
         this(new GoogleHttpClient());
     }
 
+    /**
+     * A constructor with an {@code HttpTransport} parameter that allows the
+     * user to pass a custom {@code HttpTransport} instance,then uses this
+     * instance to create a {@code GoogleHttpClient}, and initializes the object with it.
+     * <p>
+     * {@code HttpTransport} is an abstraction layer for handling the sending
+     * and receiving of HTTP requests. By passing a custom {@code HttpTransport},
+     * the user can control the details of the underlying communication, such as
+     * using different HTTP client libraries, setting timeouts, etc.
+     * <p>
+     * This constructor provides flexibility, allowing users to configure the
+     * {@code HttpTransport} according to their needs.
+     *
+     * @param transport A user-defined {@code HttpTransport} instance.
+     */
     public GoogleHttpRequestExecutor(HttpTransport transport) {
         this(new GoogleHttpClient(transport));
     }
 
+    /**
+     * A constructor with an {@code HttpRequestFactory} parameter that allows
+     * the user to pass a custom {@code HttpRequestFactory} instance,then uses
+     * this instance to create a {@code GoogleHttpClient}, and initializes the
+     * object with it.
+     * <p>
+     * {@code HttpRequestFactory} is a factory class for creating {@code HttpRequest}
+     * objects. An {@code HttpRequest} object represents an HTTP request,
+     * including the request method (e.g., GET, POST), request headers, request body,
+     * etc. By passing a custom {@code HttpRequestFactory},
+     * the user can control the creation process of {@code HttpRequest}, such as setting
+     * default request headers, modifying the request body, etc.
+     * <p>
+     * This constructor also provides flexibility, allowing users to configure the
+     * {@code HttpRequestFactory} according to their needs.
+     *
+     * @param httpRequestFactory A user-defined {@code HttpRequestFactory} instance.
+     */
     public GoogleHttpRequestExecutor(HttpRequestFactory httpRequestFactory) {
         this(new GoogleHttpClient(httpRequestFactory));
     }
 
+    /**
+     * A constructor with a {@code GoogleHttpClient} parameter that directly uses
+     * this parameter to initialize the object.
+     * <p>
+     * This constructor allows the user to directly pass a already-configured
+     * {@code GoogleHttpClient} instance,which is suitable for scenarios where
+     * finer-grained control over the {@code GoogleHttpClient} configuration is
+     * required,or when the user already has a {@code GoogleHttpClient} instance
+     * and wishes to reuse it.
+     *
+     * @param googleHttpClient A user-defined and already-configured
+     *                         {@code GoogleHttpClient} instance.
+     */
     public GoogleHttpRequestExecutor(GoogleHttpClient googleHttpClient) {
         this.googleHttpClient = googleHttpClient;
     }
