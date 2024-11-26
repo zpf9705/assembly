cd57bbb7359beba0d2191a822aca2ad2e7bc9c3d
zhangpengfei <929160069@qq.com>
2024-11-26 15:24:49 +0800

    Add component support for SDK for HTTP requests.

---
+++ assembly-simplified-sdk/sdk-core/src/main/java/top/osjf/sdk/core/support/Nullable.java
@@ -0,0 +1,27 @@
+package top.osjf.sdk.core.support;
+
+import javax.annotation.Nonnull;
+import javax.annotation.meta.TypeQualifierNickname;
+import javax.annotation.meta.When;
+import java.lang.annotation.*;
+
+/**
+ * A common annotation to declare that annotated elements can be {@code null} under
+ * some circumstance.
+ *
+ * <p>Leverages JSR-305 meta-annotations to indicate nullability in Java to common
+ * tools with JSR-305 support and used by Kotlin to infer nullability of Spring API.
+ *
+ * <p>Should be used at parameter, return value, and field level. Methods override should
+ * repeat parent {@code @CanNull} annotations unless they behave differently.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.2
+ */
+@Target({ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Nonnull(when = When.MAYBE)
+@TypeQualifierNickname
+public @interface Nullable {
+}
