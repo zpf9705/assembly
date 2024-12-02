2ddf8d3f9b7b509c9d1ccb840781dc4f388bc59c
zhangpengfei <929160069@qq.com>
2024-12-02 16:10:04 +0800

    Add component support for SDK for HTTP requests.

---
+++ assembly-simplified-sdk/sdk-http/src/main/java/top/osjf/sdk/http/ServiceLoaderLoggerHttpClient.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2023-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.osjf.sdk.http;
+
+import top.osjf.sdk.core.client.Client;
+import top.osjf.sdk.core.client.LoggerConsumer;
+import top.osjf.sdk.core.support.NotNull;
+import top.osjf.sdk.core.support.ServiceLoadManager;
+
+import java.util.function.BiConsumer;
+
+/**
+ * A {@code Client} implementation class that can utilize Java's
+ * {@link java.util.ServiceLoader} mechanism to obtain the highest
+ * priority {@code LoggerConsumer} for use.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.2
+ */
+public class ServiceLoaderLoggerHttpClient<R extends HttpResponse> extends DefaultHttpClient<R> {
+
+    private static final long serialVersionUID = 4669129398883621311L;
+
+    private LoggerConsumer consumer;
+
+    /*** Constructing for {@link Client} objects using unique identifier.
+     * @param unique The unique identifier string for this client's cache.
+     */
+    public ServiceLoaderLoggerHttpClient(String unique) {
+        super(unique);
+    }
+
+    /**
+     * Return a {@code LoggerConsumer},if null use {@link ServiceLoadManager}
+     * load a high priority {@code LoggerConsumer} to use in this {@code Client}.
+     *
+     * @return a use {@code LoggerConsumer}.
+     */
+    public LoggerConsumer getConsumer() {
+        if (consumer == null) {
+            consumer = ServiceLoadManager.loadHighPriority(LoggerConsumer.class);
+        }
+        return consumer;
+    }
+
+    @Override
+    @NotNull
+    public BiConsumer<String, Object[]> normal() {
+        return getConsumer().normal();
+    }
+
+    @Override
+    @NotNull
+    public BiConsumer<String, Object[]> sdkError() {
+        return getConsumer().sdkError();
+    }
+
+    @Override
+    @NotNull
+    public BiConsumer<String, Object[]> unKnowError() {
+        return getConsumer().unKnowError();
+    }
+}
