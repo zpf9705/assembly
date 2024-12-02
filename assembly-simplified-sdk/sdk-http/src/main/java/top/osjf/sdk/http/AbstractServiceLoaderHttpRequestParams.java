22203bad5dd68d8a1a94ea60d3f467186e734ac5
zhangpengfei <929160069@qq.com>
2024-12-02 16:28:29 +0800

    Add component support for SDK for HTTP requests.

---
+++ assembly-simplified-sdk/sdk-http/src/main/java/top/osjf/sdk/http/AbstractServiceLoaderHttpRequestParams.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.osjf.sdk.http;
+
+import top.osjf.sdk.core.client.Client;
+import top.osjf.sdk.core.support.NotNull;
+
+/**
+ * Extends for {@code AbstractHttpRequestParams} to provider a {@code Client}
+ * is {@code ServiceLoaderLoggerHttpClient}.
+ *
+ * @param <R> Implement a unified response class data type.
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.2
+ */
+@SuppressWarnings("rawtypes")
+public abstract class AbstractServiceLoaderHttpRequestParams<R extends AbstractHttpResponse> extends
+        AbstractHttpRequestParams<R> {
+
+    private static final long serialVersionUID = 7487068349280012103L;
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Use {@code ServiceLoaderLoggerHttpClient}.
+     *
+     * @return {@inheritDoc}.
+     */
+    @Override
+    @NotNull
+    public Class<? extends Client> getClientCls() {
+        return ServiceLoaderLoggerHttpClient.class;
+    }
+}
---
+++ assembly-simplified-sdk/sdk-http/src/main/java/top/osjf/sdk/http/JsonSerialServiceLoaderLoggerHttpRequestParams.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2023-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.osjf.sdk.http;
+
+import top.osjf.sdk.core.client.Client;
+import top.osjf.sdk.core.support.NotNull;
+
+/**
+ * Extends for {@code JsonSerialHttpRequestParams} to provider a {@code Client}
+ * is {@code ServiceLoaderLoggerHttpClient}.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.2
+ */
+@SuppressWarnings("rawtypes")
+public abstract class JsonSerialServiceLoaderLoggerHttpRequestParams<R extends AbstractHttpResponse> extends
+        JsonSerialHttpRequestParams<R> {
+
+    private static final long serialVersionUID = 4587196966635484290L;
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Use {@code ServiceLoaderLoggerHttpClient}.
+     *
+     * @return {@inheritDoc}.
+     */
+    @Override
+    @NotNull
+    public Class<? extends Client> getClientCls() {
+        return ServiceLoaderLoggerHttpClient.class;
+    }
+}
