58ccab9eac4a7a41354b0a3742cee322aedef9be
zhangpengfei <929160069@qq.com>
2024-12-03 14:06:25 +0800

    Add component support for SDK for HTTP requests.

---
+++ assembly-simplified-sdk/sdk-http/src/main/java/top/osjf/sdk/http/process/UrlQueryParam.java
@@ -0,0 +1,32 @@
+package top.osjf.sdk.http.process;
+
+import top.osjf.sdk.core.support.Nullable;
+
+/**
+ * URL Query Parameter Interface.
+ * <p>
+ * The interface defines a method for obtaining the value associated
+ * with the query parameter in the URL.
+ * <p>
+ * The object implementing this interface should be able to provide
+ * data corresponding to specific query parameters.
+ *
+ * @see AbstractUrlQueryHttpRequest
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.2
+ */
+public interface UrlQueryParam {
+
+    /**
+     * Gets the value associated with the URL query parameter.
+     * <p>
+     * The value associated with the query parameter specified
+     * in the URL, preferably a map structure, JSON string, or
+     * entity object.
+     *
+     * @return Query parameter objects with desired features.
+     * @see top.osjf.sdk.http.support.HttpSdkSupport#resolveMontageObj(Object)
+     */
+    @Nullable
+    Object getParam();
+}
