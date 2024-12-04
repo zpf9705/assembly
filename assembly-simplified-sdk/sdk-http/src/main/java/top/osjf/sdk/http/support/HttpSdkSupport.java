dbdea717ac00673554cb43d6889e3ba6c69ffaf2
zhangpengfei <929160069@qq.com>
2024-12-04 10:02:41 +0800

    Add component support for SDK for HTTP requests.

--- assembly-simplified-sdk/sdk-http/src/main/java/top/osjf/sdk/http/support/HttpSdkSupport.java
+++ assembly-simplified-sdk/sdk-http/src/main/java/top/osjf/sdk/http/support/HttpSdkSupport.java
@@ -21,7 +21,6 @@ import top.osjf.sdk.core.process.Request;
 import top.osjf.sdk.core.process.Response;
 import top.osjf.sdk.core.support.SdkSupport;
 import top.osjf.sdk.core.util.*;
-import top.osjf.sdk.http.process.HttpRequest;
 import top.osjf.sdk.http.util.UrlUtils;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -30,12 +29,9 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.ByteArrayInputStream;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.net.MalformedURLException;
-import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.BiConsumer;
 import java.util.function.BiPredicate;
 
 /**
@@ -51,7 +47,7 @@ import java.util.function.BiPredicate;
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.0.0
  */
-@SuppressWarnings({"rawtypes", "unchecked"})
+@SuppressWarnings({"unchecked"})
 public abstract class HttpSdkSupport extends SdkSupport {
 
     /***The left angle bracket included in the generic.*/
@@ -143,69 +139,6 @@ public abstract class HttpSdkSupport extends SdkSupport {
             return true;
         }
         return body.matches("^<.*$");
-    }
-
-    /**
-     * Get the unique client URL (excluding query parameters).
-     * <p>
-     * This method takes a URL string as input. If the input URL is blank or does
-     * not contain query parameters (i.e., the "?" character),it directly returns
-     * the original URL. If the URL contains query parameters,it returns the part
-     * of the URL before the "?",excluding the query parameters.
-     *
-     * @param url The input URL string
-     * @return The URL string excluding query parameters
-     */
-    public static String getClientUnique(String url) {
-        return StringUtils.isBlank(url) || !url.contains("?") ? url : url.substring(0, url.indexOf("?"));
-    }
-
-    /**
-     * Return the {@code request body } and {@code URL access} address when parameters
-     * are needed as URL concatenation and processed normally.
-     * <p>
-     * This method does not return a value and consumes the result through a custom
-     * function {@link BiConsumer}.
-     *
-     * @param request        An object encapsulating HTTP request information.
-     * @param url            The URL address of the request.
-     * @param resultConsumer The consumption function for the final body and URL.
-     * @throws Exception Format and encoding set related errors that
-     *                   occur during the URL formatting process.
-     */
-    public static void resolveIfMontageUrlAndBody(HttpRequest request, String url,
-                                                  BiConsumer<Object, String> resultConsumer) throws Exception {
-        Object body;
-        String realUrl;
-        if (request.montage()) {
-            //When URL parameter concatenation is required,
-            // it is handled on a case by case basis here.
-            Object montageObj;
-            //Obtain segmentation parameters for special interfaces.
-            if (request instanceof HttpRequest.MontageParam) {
-                montageObj = ((HttpRequest.MontageParam) request).getParam();
-                if (montageObj == null) {
-                    //The segmentation parameter obtained by the special
-                    // interface is null, and the body parameter is still used.
-                    montageObj = request.getRequestParam();
-                    body = null; //When the request parameter is a concatenation parameter,
-                    // the body parameter is null.
-                    //On the contrary, the body is normally set as a request parameter.
-                } else body = request.getRequestParam();
-            } else {
-                //If no special interface is implemented,
-                // simply set the request parameters as URL segmentation parameters.
-                montageObj = request.getRequestParam();
-                body = null;
-            }
-            //Format the URL for the special needs of splitting parameters.
-            realUrl = HttpSdkSupport.formatMontageTrueUrl(url, montageObj, request.getCharset());
-        } else {
-            //Under normal circumstances, just set the parameters one by one.
-            body = request.getRequestParam();
-            realUrl = url;
-        }
-        resultConsumer.accept(body, realUrl);
     }
 
     /**
@@ -316,95 +249,6 @@ public abstract class HttpSdkSupport extends SdkSupport {
     /**
      * Converts an object to a Map format suitable for URL concatenation.
      *
-     * <p>This method receives a boolean {@code montage} and an object {@code body}
-     * as parameters. When {@code montage == true} and {@code body != null}, this
-     * method converts {@code body} to a {@code Map<String, Object>} object according
-     * to its type for use in URL concatenation.
-     *
-     * <p>The conversion logic is as follows:</p>
-     * <ul>
-     * <li>If {@code body} is of type {@code Map}, it is directly converted to {@code Map<String, Object>},
-     * with all keys being converted to string type.</li>
-     * <li>If {@code body} is of type {@code String}, it attempts to parse it as a JSON
-     * string and extract the internal Map parameters. If parsing fails or it is not a valid
-     * JSON format, {@literal null} is returned.</li>
-     * <li>If {@code body} is of another type, it attempts to convert it to a JSON string
-     * and then parse the JSON string into a Map object. If any exception occurs during this
-     * process (e.g., {@code body} cannot be serialized to a JSON string), an {@code IllegalArgumentException}
-     * is thrown.</li>
-     * </ul>
-     *
-     * <p>This method is primarily used to handle different types of request bodies and is
-     * very useful when the request body needs to be converted to Map format for URL concatenation.
-     *
-     * @param montageObj The object to be converted. It can be of type {@code Map}, {@code String}, or other types.
-     * @return The converted {@code Map<String, Object>} object. If no conversion is needed or the conversion fails,
-     * {@literal null} is returned.
-     * @throws IllegalArgumentException Thrown if the {@code body} type cannot be correctly converted to Map format.
-     */
-    @SuppressWarnings("unchecked")
-    public static Map<String, Object> resolveMontageObj(Object montageObj) {
-        Map<String, Object> montageParams = null;
-        if (montageObj != null) {
-            if (montageObj instanceof Map) {
-                //When obtaining the body type in map format,
-                // it can be directly converted.
-                Map<Object, Object> mapInstanceof = (Map<Object, Object>) montageObj;
-                montageParams = new HashMap<>();
-                for (Map.Entry<Object, Object> entry : mapInstanceof.entrySet()) {
-                    montageParams.put(entry.getKey().toString(), entry.getValue());
-                }
-            } else if (montageObj instanceof String) {
-                //Consider JSON format for string format and obtain the
-                // specified map parameters from JSON conversion.
-                //When it is not in JSON format, return null.
-                montageParams = JSONUtil.getInnerMapByJsonStr((String) montageObj);
-            } else {
-                //Considering in the form of a single object, using JSON conversion
-                // to convert this object to map format may result in JSON conversion
-                // errors if the object is not familiar with defining it.
-                try {
-                    montageParams = JSONUtil.parseObject(JSONUtil.toJSONString(montageObj));
-                } catch (Exception e) {
-                    //Capture possible conversion errors, such as String type.
-                    throw new IllegalArgumentException
-                            ("The splicing URL requirement for the body parameter has been set, " +
-                                    "but the [" + montageObj.getClass().getName() + "] type of the body does not match");
-                }
-            }
-        }
-        return montageParams;
-    }
-
-    /**
-     * Use method {@link #resolveMontageObj} to analyze and obtain relevant additional URL query parameters,
-     * and rely on {@code hutool}'s {@link UrlBuilder} to concatenate the parameters.
-     *
-     * @param montageObj The object to be converted. It can be of type {@code Map}, {@code String}, or other types.
-     * @param charset    Encoding character set format.
-     * @param url        Access addresses that require specific formatting.
-     * @return Specific formatted access address.
-     * @throws CharacterCodingException if decoding percent-encoded bytes fails and charsetDecoder is configured to
-     *                                  report errors.
-     * @throws MalformedURLException    if no protocol is specified, or an
-     *                                  unknown protocol is found, or {@code spec} is {@code null}.
-     */
-    public static String formatMontageTrueUrl(String url, Object montageObj, Charset charset) throws
-            CharacterCodingException, MalformedURLException {
-        Map<String, Object> montageParams = resolveMontageObj(montageObj);
-        if (MapUtils.isNotEmpty(montageParams)) {
-            UrlBuilder urlBuilder = UrlUtils.toPalominolabsBuilder(url, charset);
-            for (Map.Entry<String, Object> entry : montageParams.entrySet()) {
-                urlBuilder.queryParam(entry.getKey(), String.valueOf(entry.getValue()));
-            }
-            url = urlBuilder.toUrlString();
-        }
-        return url;
-    }
-
-    /**
-     * Converts an object to a Map format suitable for URL concatenation.
-     *
      * <p>The conversion logic is as follows:</p>
      * <ul>
      * <li>If {@code body} is of type {@code Map}, it is directly converted to {@code Map<String, Object>},
@@ -427,7 +271,6 @@ public abstract class HttpSdkSupport extends SdkSupport {
      * @throws IllegalArgumentException Thrown if the {@code body} type cannot be correctly converted to Map format.
      * @since 1.0.2
      */
-    @SuppressWarnings("unchecked")
     public static Map<String, Object> convertUrlQueryParamToMap(Object urlQueryParm) {
         Map<String, Object> queryParamMap = null;
         if (urlQueryParm != null) {
@@ -478,7 +321,7 @@ public abstract class HttpSdkSupport extends SdkSupport {
      * @since 1.0.2
      */
     public static String buildAndFormatUrlWithQueryParams(String url, Object queryParam, Charset charset) {
-        Map<String, Object> queryParamMap = resolveMontageObj(queryParam);
+        Map<String, Object> queryParamMap = convertUrlQueryParamToMap(queryParam);
         if (MapUtils.isNotEmpty(queryParamMap)) {
             try {
                 UrlBuilder urlBuilder = UrlUtils.toPalominolabsBuilder(url, charset);
