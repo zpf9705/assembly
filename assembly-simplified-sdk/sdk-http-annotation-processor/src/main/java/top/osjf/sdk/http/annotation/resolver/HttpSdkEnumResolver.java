988c975da515f5919193aee0685efba93dead4bb
zhangpengfei <929166069@qq.com>
2024-12-07 16:35:10 +0800

    utils update

--- assembly-simplified-sdk/sdk-http-annotation-processor/src/main/java/top/osjf/sdk/http/annotation/resolver/HttpSdkEnumResolver.java
+++ assembly-simplified-sdk/sdk-http-annotation-processor/src/main/java/top/osjf/sdk/http/annotation/resolver/HttpSdkEnumResolver.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package top.osjf.sdk.http.annotation.processor;
+package top.osjf.sdk.http.annotation.resolver;
 
 import com.sun.tools.javac.code.Flags;
 import com.sun.tools.javac.tree.JCTree;
@@ -24,6 +24,7 @@ import com.sun.tools.javac.util.Name;
 import com.sun.tools.javac.util.Names;
 import top.osjf.sdk.core.util.StringUtils;
 import top.osjf.sdk.http.annotation.HttpSdkEnumCultivate;
+import top.osjf.sdk.http.annotation.intelligence.DefaultCultivateHttpSdkEnum;
 
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.Element;
@@ -32,10 +33,18 @@ import javax.lang.model.type.TypeMirror;
 import java.util.Set;
 
 /**
+ * The implementation class for processing {@code Resolver} related
+ * services annotated with {@link HttpSdkEnumCultivate}.
+ *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.0.2
  */
 public class HttpSdkEnumResolver implements Resolver {
+
+    static final String METHOD_NAME = "matchSdkEnum";
+    static final String DEFAULT_VAR_NAME = "DEFAULT_HTTP_SDK_ENUM";
+    static final String INTELLIGENCE_SIMPLE_NAME = DefaultCultivateHttpSdkEnum.class.getSimpleName();
+    static final String INTELLIGENCE_PACKAGE_NAME = DefaultCultivateHttpSdkEnum.class.getPackage().getName();
 
     @Override
     public void resolve(ResolverMetadata resolverMetadata) {
@@ -71,10 +80,47 @@ public class HttpSdkEnumResolver implements Resolver {
 
         unitTree.defs = unitTree.defs.prepend(jcImport);
 
-        //Change the variable of method `matchSdkEnum` to DEFAULT_HTTP_SDK_ENUM.
+        updateMethodVar(classDecl, treeMaker, names);
+    }
+
+    private JCTree.JCVariableDecl getStaticVar(TreeMaker maker, Names names, TypeElement element) {
+
+        JCTree.JCModifiers modifiers = maker.Modifiers(Flags.PRIVATE | Flags.STATIC | Flags.FINAL);
+
+        Name fieldName = names.fromString(DEFAULT_VAR_NAME);
+
+        JCTree.JCExpression fieldType = maker.Ident(names.fromString(INTELLIGENCE_SIMPLE_NAME));
+
+        HttpSdkEnumCultivate cultivate = element.getAnnotation(HttpSdkEnumCultivate.class);
+
+        String name = StringUtils.isNotBlank(cultivate.name()) ? cultivate.name() : element.getQualifiedName().toString();
+
+        JCTree.JCNewClass newClass = maker.NewClass(
+                null,
+                List.nil(),
+                maker.Ident(names.fromString(INTELLIGENCE_SIMPLE_NAME)),
+                List.of(maker.Literal(cultivate.url()),
+                        maker.Literal(cultivate.version()),
+                        maker.Literal(cultivate.protocol().name()),
+                        maker.Literal(cultivate.method().name()),
+                        maker.Literal(name)),
+                null
+        );
+
+        return maker.VarDef(modifiers, fieldName, fieldType, newClass);
+
+    }
+
+    private JCTree.JCImport getImport(TreeMaker maker, Names names) {
+        JCTree.JCIdent packageJCIdent = maker.Ident(names.fromString(INTELLIGENCE_PACKAGE_NAME));
+        Name className = names.fromString(INTELLIGENCE_SIMPLE_NAME);
+        return maker.Import(maker.Select(packageJCIdent, className), false);
+    }
+
+    private void updateMethodVar(JCTree.JCClassDecl classDecl, TreeMaker treeMaker, Names names) {
         classDecl.defs.stream()
                 .filter(jct -> jct instanceof JCTree.JCMethodDecl
-                        && "matchSdkEnum".equals(((JCTree.JCMethodDecl) jct).name.toString()))
+                        && METHOD_NAME.equals(((JCTree.JCMethodDecl) jct).name.toString()))
                 .findFirst()
                 .ifPresent(jcTree -> {
                     JCTree.JCBlock body = ((JCTree.JCMethodDecl) jcTree).getBody();
@@ -84,51 +130,9 @@ public class HttpSdkEnumResolver implements Resolver {
                             .findFirst()
                             .ifPresent(jcStatement -> {
                                 JCTree.JCReturn jcReturn = (JCTree.JCReturn) jcStatement;
-                                jcReturn.expr = treeMaker.Ident(names.fromString("DEFAULT_HTTP_SDK_ENUM"));
+                                jcReturn.expr = treeMaker.Ident(names.fromString(DEFAULT_VAR_NAME));
                             });
                 });
-    }
-
-    private JCTree.JCVariableDecl getStaticVar(TreeMaker maker, Names names, TypeElement element) {
-
-        JCTree.JCModifiers modifiers = maker.Modifiers(Flags.PRIVATE | Flags.STATIC | Flags.FINAL);
-
-        Name fieldName = names.fromString("DEFAULT_HTTP_SDK_ENUM");
-
-        JCTree.JCExpression fieldType = maker.Ident(names.fromString("DefaultCultivateHttpSdkEnum"));
-
-        HttpSdkEnumCultivate cultivate = element.getAnnotation(HttpSdkEnumCultivate.class);
-
-        String name = StringUtils.isNotBlank(cultivate.name()) ? cultivate.name() : element.getQualifiedName().toString();
-
-        JCTree.JCNewClass newClass = maker.NewClass(
-                null,
-                List.nil(),
-                maker.Ident(names.fromString("DefaultCultivateHttpSdkEnum")),
-                List.of(maker.Literal(cultivate.url()),
-                        maker.Literal(cultivate.version()),
-                        maker.Literal(cultivate.protocol().name()),
-                        maker.Literal(cultivate.method().name()),
-                        maker.Literal(name)),
-                null
-        );
-
-        return maker.VarDef(
-                modifiers,
-                fieldName,
-                fieldType,
-                newClass
-        );
-
-    }
-
-    private JCTree.JCImport getImport(TreeMaker maker, Names names) {
-
-        JCTree.JCIdent packageJCIdent = maker.Ident(names.fromString("top.osjf.sdk.http.annotation"));
-
-        Name className = names.fromString("DefaultCultivateHttpSdkEnum");
-
-        return maker.Import(maker.Select(packageJCIdent, className), false);
     }
 
     private boolean isRequestAssignableFrom(TypeElement element) {
