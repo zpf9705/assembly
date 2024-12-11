74d26765062c25df6e7c9bfcc7c4b5a837f3fb8d
zhangpengfei <929160069@qq.com>
2024-12-11 14:25:27 +0800

    Add component support for SDK for HTTP requests.

--- assembly-simplified-sdk/sdk-http/src/main/java/top/osjf/sdk/http/HttpSdkEnumManager.java
+++ assembly-simplified-sdk/sdk-http/src/main/java/top/osjf/sdk/http/HttpSdkEnumManager.java
@@ -1,6 +1,23 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package top.osjf.sdk.http;
 
-import top.osjf.sdk.core.process.Request;
+import top.osjf.sdk.http.process.HttpRequest;
+import top.osjf.sdk.http.process.HttpResponse;
 import top.osjf.sdk.http.process.HttpSdkEnum;
 
 import javax.annotation.concurrent.ThreadSafe;
@@ -14,12 +31,12 @@ import javax.annotation.concurrent.ThreadSafe;
  * {@link java.util.ServiceLoader}, with annotations {@link top.osjf.sdk.core.support.LoadOrder},
  * to achieve self defined extensions.
  *
+ * @param <R> Implement a unified response class data type.
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.0.2
  */
 @ThreadSafe
-@SuppressWarnings("rawtypes")
-public interface HttpSdkEnumManager {
+public interface HttpSdkEnumManager<R extends HttpResponse> {
 
     /**
      * Based on a current request object {@code Request}, retrieve and cache a
@@ -33,5 +50,5 @@ public interface HttpSdkEnumManager {
      *                               currently executed {@code HttpSdkEnumCultivate},
      *                               this exception object will be thrown.
      */
-    HttpSdkEnum getAndSetHttpSdkEnum(Request currentRequest) throws IllegalStateException;
+    HttpSdkEnum getAndSetHttpSdkEnum(HttpRequest<R> currentRequest) throws IllegalStateException;
 }
