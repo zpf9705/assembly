a8858f3f94e3f4d7076eadca45ac00d31e416ed7
zhangpengfei <929160069@qq.com>
2024-12-16 16:48:43 +0800

    Add component support for SDK for HTTP requests.

--- assembly-simplified-sdk/sdk-http/src/main/java/top/osjf/sdk/http/support/HttpSdkSupport.java
+++ assembly-simplified-sdk/sdk-http/src/main/java/top/osjf/sdk/http/support/HttpSdkSupport.java
@@ -18,23 +18,21 @@ package top.osjf.sdk.http.support;
 
 import com.google.common.annotations.Beta;
 import com.palominolabs.http.url.UrlBuilder;
-import top.osjf.sdk.core.process.Request;
-import top.osjf.sdk.core.process.Response;
 import top.osjf.sdk.core.support.NotNull;
 import top.osjf.sdk.core.support.Nullable;
 import top.osjf.sdk.core.support.SdkSupport;
-import top.osjf.sdk.core.util.*;
+import top.osjf.sdk.core.util.JSONUtil;
+import top.osjf.sdk.core.util.StringUtils;
 import top.osjf.sdk.http.util.UrlUtils;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.ByteArrayInputStream;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 import java.nio.charset.Charset;
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.function.BiPredicate;
 
 /**
@@ -52,18 +50,6 @@ import java.util.function.BiPredicate;
  */
 @SuppressWarnings({"unchecked"})
 public abstract class HttpSdkSupport extends SdkSupport {
-
-    /***The left angle bracket included in the generic.*/
-    protected static final String LEFT_ANGLE_BRACKET = "<";
-
-    /***The right angle bracket included in the generic.*/
-    protected static final String RIGHT_ANGLE_BRACKET = ">";
-
-    /*** cache for dynamically obtain the type of response class.
-     * Since 1.0.2,cache modification to weak references, freeing up memory in appropriate
-     * places to prevent memory leaks.
-     * */
-    protected static final Map<Class<?>, Object> rps_classes = new SynchronizedWeakHashMap<>();
 
     /*** Cache for context type determination.
      * @since 1.0.2
@@ -173,111 +159,6 @@ public abstract class HttpSdkSupport extends SdkSupport {
     }
 
     /**
-     * Retrieves the required response type for a given request.
-     *
-     * <p>This method determines and returns the required response type based on the
-     * passed request object and its inheritance hierarchy.
-     * It first attempts to retrieve the type from a cache, and if not found, traverses
-     * the class hierarchy (including interfaces and parent classes)
-     * of the request object to find a matching response type. If a matching generic type
-     * is found, it is returned; otherwise,
-     * if no matching type is found, the default type is returned.</p>
-     *
-     * @param request The request object used to determine the required response type.
-     * @param def     The default type to return if no matching response type is found.
-     * @return The required response type for the request, or the default type if none is found.
-     * @see Class#getGenericInterfaces()
-     * @see Class#getGenericSuperclass()
-     */
-    public static Object getResponseRequiredType(Request<?> request, Object def) {
-
-        if (request == null) {
-            return null;
-        }
-        //Loop stop marker.
-        boolean goWhile = true;
-
-        //The final return type.
-        Object type = null;
-
-        //The class object of the current request class.
-        final Class<?> inletClass = request.getClass();
-
-        //Try reading the cache first.
-        Object inlet = rps_classes.get(inletClass);
-        if (inlet != null) {
-            return inlet;
-        }
-
-        //Cache temporary type class objects.
-        Map<Type, Class<?>> type_classes = new ConcurrentHashMap<>(16);
-
-        //There is no cache to execute the fetch logic.
-        Class<?> clazz = inletClass;
-        while (goWhile) {
-            //Collect the type of the class for visual objects.
-            //Including interfaces and classes.
-            ClassLoader classLoader = clazz.getClassLoader();
-            List<Type> types = new ArrayList<>();
-            Type genericSuperclass = clazz.getGenericSuperclass();
-            if (genericSuperclass != null) {
-                types.add(genericSuperclass);
-            }
-            Type[] genericInterfaces = clazz.getGenericInterfaces();
-            if (ArrayUtils.isNotEmpty(genericInterfaces)) {
-                types.addAll(Arrays.asList(genericInterfaces));
-            }
-
-            //Filter the main class of the request main class, taking the first one.
-            Type typeFilter = types.stream().filter(linkType -> {
-                Class<?> typeClass = getTypeClass(linkType, classLoader);
-                if (typeClass == null) {
-                    return false;
-                }
-                //Cache the class objects of the current classes for future use
-                type_classes.putIfAbsent(linkType, typeClass);
-                return request.isAssignableRequest(typeClass);
-            }).findFirst().orElse(null);
-
-            //If there is nothing available, simply exit the loop.
-            if (typeFilter == null) {
-                goWhile = false;
-                continue;
-            }
-
-            //If it is a type that carries a generic, then obtain whether
-            // it contains a generic that responds to the main class and obtain it.
-            if (typeFilter instanceof ParameterizedType) {
-                Object arguments = getActualResponseTypeArguments(typeFilter, clazz.getClassLoader());
-                if (arguments != null) {
-                    type = arguments;
-                    //After obtaining it, bind the response generic of the
-                    // main class and use it for subsequent caching.
-                    rps_classes.putIfAbsent(inletClass, type);
-                    goWhile = false;
-                } else {
-
-                    //The paradigm does not carry response generics, and the class
-                    // object logic for filtering types is executed.
-                    // According to the specification of the idea, it will not go this far.
-                    clazz = type_classes.get(typeFilter);
-                }
-            } else {
-
-                //Non generic classes, directly use class objects of filter types for subsequent logic.
-                clazz = type_classes.get(typeFilter);
-            }
-
-        }
-
-        //If the type is empty, cache a default type.
-        if (type == null) {
-            rps_classes.put(inletClass, def);
-        }
-        return rps_classes.get(inletClass);
-    }
-
-    /**
      * Converts an object to a Map format suitable for URL concatenation.
      *
      * <p>The conversion logic is as follows:</p>
@@ -363,37 +244,5 @@ public abstract class HttpSdkSupport extends SdkSupport {
             throw new IllegalArgumentException(" Format URL error ", e);
         }
         return url;
-    }
-
-    private static Object getActualResponseTypeArguments(Type type, ClassLoader classLoader) {
-        Type[] actualTypeArguments = ((ParameterizedType) type).getActualTypeArguments();
-        // as Response and retrieve the first one.
-        return Arrays.stream(actualTypeArguments).filter(argType -> {
-                    Class<?> typeClass = getTypeClass(argType, classLoader);
-                    return typeClass != null && Response.class.isAssignableFrom(typeClass);
-                })
-                .findFirst()
-                .orElse(null);
-    }
-
-    private static Class<?> getTypeClass(Type type, ClassLoader classLoader) {
-        String typeName = type.getTypeName();
-        String className = typeName;
-        //A generic type name that carries<...>, After removing such content,
-        // the original name of the class is obtained.
-        if (typeName.contains(LEFT_ANGLE_BRACKET) && typeName.contains(RIGHT_ANGLE_BRACKET)) {
-            className = typeName.split(LEFT_ANGLE_BRACKET)[0];
-        }
-        Class<?> classObj;
-        try {
-            classObj = Class.forName(className, false, classLoader);
-        } catch (ClassNotFoundException e) {
-            classObj = null;
-        }
-        return classObj;
-    }
-
-    public static Object[] toLoggerArray(Object... args) {
-        return args;
     }
 }
