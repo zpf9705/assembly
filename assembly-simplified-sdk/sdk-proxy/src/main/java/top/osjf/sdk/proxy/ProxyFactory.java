934b5a5cfa1596475f709121cecfea5f9b9f23ae
zhangpengfei <929160069@qq.com>
2024-12-19 14:54:33 +0800

    Add component support for SDK for HTTP requests.

--- assembly-simplified-sdk/sdk-proxy/src/main/java/top/osjf/sdk/proxy/ProxyFactory.java
+++ assembly-simplified-sdk/sdk-proxy/src/main/java/top/osjf/sdk/proxy/ProxyFactory.java
@@ -16,24 +16,30 @@
 
 package top.osjf.sdk.proxy;
 
-import top.osjf.sdk.core.support.NotNull;
-
 /**
  * Factory interface for proxy object creation.
  *
- * <p>This interface defines a generic proxy factory method for creating
- * proxy objects of any type and the method accepts a variable number of
- * arguments,which can be used to provide necessary configurations or
- * initialization data when creating the proxy object.
+ * <p>This interface is a factory interface for creating proxy objects,
+ * which allows you to create an object that can perform operations on
+ * behalf of another object (the original object) and the proxy object
+ * can add additional behavior before or after executing the methods of
+ * the original object, or completely replace the method implementation
+ * of the original object.
  *
- * <p>With the generic method {@link #newProxy}, it allows flexible creation
- * of proxy instances of any type. This is very useful when dynamic proxy objects
- * need to be generated, such as in AOP (Aspect-Oriented Programming), or when
- * proxy logic (e.g., caching, transaction management, permission checking)
- * needs to be provided for certain objects.
+ * <p>Proxy objects are typically dynamically created through reflection
+ * mechanisms and implement the same interface as the original object
+ * (such as the dynamic proxy {@link java.lang.reflect.Proxy} provided
+ * by JDK). Proxy objects can intercept method calls and forward them
+ * to the original object, or perform other logic such as logging,
+ * transaction management, security checks, etc.
  *
- * <p>Classes implementing this interface need to define how to create and
- * return proxy objects of a specific type based on the passed arguments.
+ * <p>This interface will summarize most popular proxy object construction
+ * frameworks. {@code DelegationCallback} is the general proxy interface
+ * for proxy callbacks, and its implementation {@link top.osjf.sdk.core.Wrapper}
+ * can be freely switched within subclasses.
+ *
+ * <p>Please note that methods in this interface may throw exceptions,
+ * and the caller needs to handle these exceptions appropriately.
  *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.0.2
@@ -43,28 +49,30 @@ public interface ProxyFactory {
     /**
      * Creates a new proxy object.
      *
-     * <p>This method accepts a generic type parameter {@code T} to specify
-     * the type of the proxy object. It also accepts a variable number of
-     * {@code Object} type parameters {@code args}, which will be used when
-     * creating the proxy object.
+     * <p>This method accepts a generic type parameter {@code T} to
+     * specify the type of the proxy object based on the specified
+     * type (interface or class) {@code Class} and callback
+     * {@code DelegationCallback}.
      *
-     * <p>Callers can specify the type of proxy object they want to create
-     * and pass necessary configuration information, dependencies, or other
-     * initialization data through the {@code args} parameters. The proxy
-     * factory will dynamically generate a proxy object based on this
-     * information.
+     * <p>Callers can specify the type of proxy object they want
+     * to create and pass necessary configuration information,
+     * dependencies, or other initialization data through method
+     * parameters and proxy factory will dynamically generate a
+     * proxy object based on this information.
      *
-     * <p>The generated proxy object will have the same interface or parent
-     * class as the original object (type support depends on the architecture)
-     * , and additional behavior can be added when calling its methods, such
-     * as JDK's {@link java.lang.reflect.InvocationHandler}.
+     * <p>The generated proxy object will have the same interface
+     * or parent class as the original object (depending on the
+     * underlying framework), but additional behavior (such as
+     * pre-processing and post-processing) can be added when calling
+     * its methods.
      *
-     * @param <T>  the type of the proxy object.
-     * @param args a variable number of arguments used to provide necessary
-     *             configurations or initialization data when creating the
-     *             proxy object.
+     * @param <T>      the type of the proxy object.
+     * @param type     the type of proxy object to be created (the
+     *                 specific type depends on the underlying framework).
+     * @param callback the {@code DelegationCallback} callback executed
+     *                 when the method of the proxy object is called.
      * @return specify {@code T} the type of proxy object.
      * @throws Throwable it can be any error that can be thrown.
      */
-    <T> T newProxy(@NotNull Object... args) throws Throwable;
+    <T> T newProxy(Class<T> type, DelegationCallback callback) throws Throwable;
 }
