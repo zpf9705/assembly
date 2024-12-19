ac21aa162fcf162587ddbdbaf22567df18d233df
zhangpengfei <929160069@qq.com>
2024-12-19 16:10:06 +0800

    Add component support for SDK for HTTP requests.

---
+++ assembly-simplified-sdk/sdk-proxy/src/main/java/top/osjf/sdk/proxy/ProxyModel.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package top.osjf.sdk.proxy;
+
+
+import top.osjf.sdk.proxy.cglib.CglibProxyFactory;
+import top.osjf.sdk.proxy.jdk.JDKProxyFactory;
+import top.osjf.sdk.proxy.springcglib.SpringCglibProxyFactory;
+
+/**
+ * The {@code ProxyModel} enumeration defines different proxy
+ * models, each corresponding to a specific proxy factory
+ * responsible for creating proxy objects of the corresponding type.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.2
+ */
+public enum ProxyModel implements ProxyFactory {
+
+    /**
+     * A proxy model created using JDK dynamic proxy factory.
+     */
+    JDK(new JDKProxyFactory()),
+
+    /**
+     * A proxy model created using CGLIB proxy factory.
+     */
+    CGLIB(new CglibProxyFactory()),
+
+    /**
+     * A proxy model created using the Spring CGLIB proxy factory.
+     */
+    SPRING_CGLIB(new SpringCglibProxyFactory());
+
+    final ProxyFactory proxyFactory;
+
+    ProxyModel(ProxyFactory proxyFactory) {
+        this.proxyFactory = proxyFactory;
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Create a proxy object using the factory method of the current model.
+     */
+    @Override
+    public <T> T newProxy(Class<T> type, DelegationCallback callback) throws Throwable {
+        return proxyFactory.newProxy(type, callback);
+    }
+}
