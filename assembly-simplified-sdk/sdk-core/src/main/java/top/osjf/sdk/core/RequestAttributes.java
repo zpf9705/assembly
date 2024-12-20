c452d5c50930626f2039e46d4a7db15e87f921fd
zhangpengfei <929160069@qq.com>
2024-12-20 15:45:54 +0800

    Add component support for SDK for HTTP requests.

--- assembly-simplified-sdk/sdk-core/src/main/java/top/osjf/sdk/core/RequestAttributes.java
+++ assembly-simplified-sdk/sdk-core/src/main/java/top/osjf/sdk/core/RequestAttributes.java
@@ -16,10 +16,19 @@
 
 package top.osjf.sdk.core;
 
+import top.osjf.sdk.core.caller.RequestCaller;
+import top.osjf.sdk.core.support.Nullable;
+
 /**
- * Regarding the definition property of {@link Request},
- * the aware interface needs to be implemented to set and
- * obtain the corresponding request properties.
+ * The {@code RequestAttributes} interface defines a set of auxiliary
+ * request attributes that are optional for SDK request functionality,
+ * but their presence can greatly enhance the functionality and flexibility
+ * of requests.
+ *
+ * <p>This interface provides a series of methods to set and retrieve
+ * these auxiliary properties, including hostname and request caller.
+ * If a property is not set, the related method will return null and
+ * will not affect the basic request function of the SDK.
  *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.0.0
@@ -27,16 +36,61 @@ package top.osjf.sdk.core;
 public interface RequestAttributes {
 
     /**
-     * Set the host address required for the SDK request
-     * domain, which can be a domain name or an IP address
-     * containing a good port.
+     * Sets the host name.
      *
-     * @param host Given request a host address.
+     * <p>This method is used to set the host name for the
+     * current object. If the passed host parameter is null,
+     * it means no host name is set.
+     *
+     * <p>The recommended setting for this value is to enable
+     * dynamic changes to the server requested by the SDK,
+     * with better scalability. However, this is only a
+     * suggestion and depends on individual needs.
+     *
+     * @param host The host name, which can be null.
      */
-    void setHost(String host);
+    void setHost(@Nullable String host);
+
 
     /**
-     * @return A request builder within a host.
+     * Gets the host name.
+     *
+     * <p>This method is used to retrieve the host name set
+     * for the current object. If no host name has been set
+     * before, it will return null.
+     *
+     * @return The host name, which may be null.
      */
+    @Nullable
     String getHost();
+
+    /**
+     * Sets the request caller {@code RequestCaller}.
+     *
+     * <p>This method is used to set a request caller {@code RequestCaller}
+     * for the current object,if the passed {@code RequestCaller} parameter
+     * is null, it means no request caller is set.
+     *
+     * <p>The recommended setting for this value is to implement special
+     * auxiliary functions such as retry for SDK call failures. Please
+     * refer to class {@code RequestCaller} for details. Of course, this
+     * is just a suggestion and depends on individual needs.
+     *
+     * @param requestCaller The request caller, which can be null.
+     * @since 1.0.2
+     */
+    void setRequestCaller(@Nullable RequestCaller requestCaller);
+
+    /**
+     * Gets the request caller {@code RequestCaller}.
+     *
+     * <p>This method is used to retrieve the request caller
+     * {@code RequestCaller} set for the current object. If no
+     * request caller has been set before, it will return null.
+     *
+     * @return The request caller, which may be null.
+     * @since 1.0.2
+     */
+    @Nullable
+    RequestCaller getRequestCaller();
 }
