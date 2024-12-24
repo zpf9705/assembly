9da1b765da2a7f7351f2a57386b0fc44b5a8be1e
zhangpengfei <929160069@qq.com>
2024-12-24 17:07:53 +0800

    Add component support for SDK for HTTP requests.

---
+++ assembly-simplified-spring-optimize/service-bean/src/main/java/top/osjf/optimize/service_bean/annotation/ServiceScopeBeanPostProcessorRegistrar.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package top.osjf.optimize.service_bean.annotation;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanNameGenerator;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.lang.NonNull;
+import top.osjf.optimize.service_bean.context.ServiceContext;
+import top.osjf.optimize.service_bean.context.ServiceScopeBeanPostProcessor;
+
+/**
+ * The {@code ServiceScopeBeanPostProcessorRegistrar} class is a Spring component
+ * registrar that implements the {@code ImportBeanDefinitionRegistrar} interface.
+ *
+ * <p>In this specific implementation, {@code ServiceScopeBeanPostProcessorRegistrar}
+ * is responsible for registering a Bean of type {@code ServiceScopeBeanPostProcessor},
+ * which is a custom {@code BeanPostProcessor} used in the process of creating and
+ * initializing beans in the Spring container,perform additional processing or optimization
+ * on beans within a specific scope {@link ServiceContext#SUPPORT_SCOPE}.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.3
+ */
+public class ServiceScopeBeanPostProcessorRegistrar implements ImportBeanDefinitionRegistrar {
+
+    @Override
+    public void registerBeanDefinitions(@NonNull AnnotationMetadata importingClassMetadata,
+                                        @NonNull BeanDefinitionRegistry registry,
+                                        @NonNull BeanNameGenerator importBeanNameGenerator) {
+        BeanDefinition beanDefinition =
+                BeanDefinitionBuilder
+                        .genericBeanDefinition(ServiceScopeBeanPostProcessor.class).getBeanDefinition();
+        registry.registerBeanDefinition(importBeanNameGenerator.generateBeanName(beanDefinition, registry),
+                beanDefinition);
+    }
+}
