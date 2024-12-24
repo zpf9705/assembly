a34efef51e834ebb926eb02e4f295568deb724f8
zhangpengfei <929160069@qq.com>
2024-12-24 20:14:21 +0800

    Add component support for SDK for HTTP requests.

--- assembly-simplified-spring-optimize/service-bean/src/main/java/top/osjf/optimize/service_bean/context/ServiceContext.java
+++ assembly-simplified-spring-optimize/service-bean/src/main/java/top/osjf/optimize/service_bean/context/ServiceContext.java
@@ -16,6 +16,7 @@
 
 package top.osjf.optimize.service_bean.context;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
@@ -23,7 +24,24 @@ import org.springframework.stereotype.Component;
 import java.io.Closeable;
 
 /**
- * The central interface for obtaining service context configuration.
+ * The {@code ServiceContext} interface defines the interaction specifications
+ * with service beans in the Spring framework, including the retrieval, addition,
+ * existence checking, and removal of services.
+ *
+ * <p>It provides a mechanism that allows fine-grained management of service beans
+ * outside the Spring container or within a specific scope, thereby enhancing the
+ * flexibility and scalability of the Spring framework and extend Java's {@link Closeable}
+ * interface to elegantly close Spring's {@link ApplicationContext} by calling the
+ * {@link #close()} method of this interface.
+ *
+ * <p>The constant {@link #SUPPORT_SCOPE} in the interface defines a custom scope
+ * called {@code service}, which is used to specify the lifecycle and visibility
+ * of specific beans in Spring configuration. By implementing the custom scope
+ * {@link ServiceScope}, the behavior of this scope is further defined.
+ *
+ * <p>The methods in the interface cover the full lifecycle management of services,
+ * including service acquisition, addition, existence checking, and removal. These
+ * methods are based on generics to support multiple types of services.
  *
  * <p>The underlying implementation utilizes the bean query function method of spring
  * context central interface {@link ApplicationContext}.
@@ -34,18 +52,34 @@ import java.io.Closeable;
 public interface ServiceContext extends Closeable {
 
     /**
+     * Define a constant to represent a custom scope name {@code service} for a
+     * Bean in the Spring framework.
+     *
+     * <p>This constant means that it represents a custom scope named {@code service}
+     * , and in Spring configuration, the behavior of this scope can be defined through
+     * a specific class {@link ServiceScope}.
+     *
+     * @see org.springframework.beans.factory.config.Scope
+     * @see org.springframework.context.annotation.AnnotationScopeMetadataResolver
+     */
+    String SUPPORT_SCOPE = "service";
+
+    /**
      * Returns a service with a specified name, which can be shared or independent.
+     *
      * <p>The operation is equivalent to {@link ApplicationContext#getBean(String, Class)}},
      * and on this basis,Add the class name {@link Class#getName()}} of the routing service
-     * in the name to prevent potential bean duplication issues, in order to better adapt to single
-     * interface multi implementation class routing scenarios in the Spring framework.
+     * in the name to prevent potential bean duplication issues, in order to better adapt to
+     * single interface multi implementation class routing scenarios in the Spring framework.
+     *
      * <p>According to the specifications of Spring's beans, multiple alias queries are supported,
-     * but methods in the context will be called to support {@link ApplicationContext#getBean(String, Class)}.
+     * but methods in the context will be called to support {@link ApplicationContext#getBean(
+     *String, Class)}.
      *
      * @param serviceName  the name of the service to retrieve.
-     * @param requiredType type the bean must match; can be an interface or superclass.
-     * @param <S>          types of required.
-     * @return an instance of the service.
+     * @param requiredType type the bean must match,can be an interface or superclass.
+     * @param <S>          the type of service to retrieve.
+     * @return The service instance obtained by parsing the service name and type.
      * @throws NoSuchServiceException if there is no such service.
      */
     <S> S getService(String serviceName, Class<S> requiredType) throws NoSuchServiceException;
@@ -62,12 +96,14 @@ public interface ServiceContext extends Closeable {
      *
      * <p>Only beans that have not been marked by Spring to be added to the container are supported,
      * which means only dynamic bean registration is supported. Container objects that have been
-     * recognized by Spring will be automatically added to the current context, so this must be noted.
+     * recognized by Spring will be automatically added to the current context, so this must be
+     * noted.
      *
-     * @param serviceName the name of the service to retrieve,can be empty, when empty,
+     * @param serviceName the name of the service to add,can be empty, when empty,
      *                    use a type qualified name instead.
-     * @param serviceType real and instantiated service types.
-     * @param <S>         types of required.
+     * @param serviceType real and instantiated service types,can be an interface or
+     *                    superclass.
+     * @param <S>         the type of service to add.
      * @return If {@code true} is returned, it indicates successful addition; otherwise,
      * it indicates failed addition.
      * @since 1.0.2
@@ -75,52 +111,36 @@ public interface ServiceContext extends Closeable {
     <S> boolean addService(@Nullable String serviceName, Class<S> serviceType);
 
     /**
-     * Returns a Boolean value representing the result of the containing operation, which re
-     * encodes the final service name using the provided service name and type to match the
-     * bean of the specific type provided.This method does not throw undiscovered exceptions
-     * and is represented by a Boolean result.
+     * Returns a {@code Boolean} value representing the result of the containing operation,
+     * which re encodes the final service name using the provided service name and type to
+     * match the bean of the specific type provided.This method does not throw undiscovered
+     * exceptions and is represented by a {@code Boolean} result.
      *
      * <p>Essentially call the factory method of {@link ApplicationContext#containsBean} and
      * make intake adjustments in the name stage.
      *
-     * @param serviceName  the name of the service to retrieve.
+     * @param serviceName  the name of the service to contains.
      * @param requiredType type the bean must match; can be an interface or superclass.
-     * @param <S>          types of required.
+     * @param <S>          the type of service to contains.
      * @return If {@code true} is returned, this service exists, otherwise it does not exist.
      * @since 1.0.2
      */
     <S> boolean containsService(String serviceName, Class<S> requiredType);
 
     /**
-     * Return a boolean type result representing the result of service removal,
+     * Return a {@code Boolean} type result representing the result of service removal,
      * based on the re encoding of the provided name and type.
      *
      * <p>The removed service will be destroyed in the Spring container at the
      * same time as it is removed in the context of this service, with a callback
-     * {@link org.springframework.beans.factory.DisposableBean}.
+     * {@link DisposableBean}.
      *
-     * @param serviceName  the name of the service to retrieve.
+     * @param serviceName  the name of the service to remove.
      * @param requiredType type the bean must match; can be an interface or superclass.
-     * @param <S>          types of required.
-     * @return If {@code true} is returned, the service has been successfully removed, otherwise
-     * it does not exist or the removal has failed.
+     * @param <S>          the type of service to remove.
+     * @return If {@code true} is returned, the service has been successfully removed,
+     * otherwise it does not exist or the removal has failed.
      * @since 1.0.2
      */
     <S> boolean removeService(String serviceName, Class<S> requiredType);
-
-    /**
-     * Returns the context assist class for the spring framework.
-     * <p>When the current service context cannot meet the scenario, spring's context
-     * object can be obtained to operate the corresponding API to meet the current
-     * service acquisition requirements.
-     *
-     * @return Return a helper of Spring context.
-     */
-    ApplicationContext getApplicationContext();
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    void close();
 }
