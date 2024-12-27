f3421e8b321991be712de84fec9f45622b217bf9
zhangpengfei <929160069@qq.com>
2024-12-27 19:58:54 +0800

    Add component support for SDK for HTTP requests.

--- assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/AbstractRegistrantCollector.java
+++ assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/AbstractRegistrantCollector.java
@@ -16,50 +16,74 @@
 
 package top.osjf.cron.spring;
 
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 import org.springframework.lang.NonNull;
+import org.springframework.util.ReflectionUtils;
+import top.osjf.cron.core.DefaultRegistrant;
+import top.osjf.cron.core.Registrant;
 import top.osjf.cron.core.util.ArrayUtils;
+import top.osjf.cron.core.util.CollectionUtils;
 import top.osjf.cron.spring.annotation.Cron;
 import top.osjf.cron.spring.annotation.CronAnnotationAttributes;
 
-import java.lang.reflect.AnnotatedElement;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.*;
 
 /**
- * <p>Regarding the registration criteria for scheduled tasks, including the judgment
- * of environment specified in the {@link Cron} annotation, the default {@link Registrant}
- * mentioned above does not meet the requirements and can be further overridden by subclasses.
- *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.0.0
  */
-public abstract class AbstractRegistrantCollector implements RegistrantCollector {
+public abstract class AbstractRegistrantCollector implements RegistrantCollector, EnvironmentAware {
 
-    /*** The temporary collection set of {@link Registrant}. */
+    private List<String> activeProfiles;
+    private Iterator<Registrant> iterator;
     private final List<Registrant> registrants = new ArrayList<>();
 
-    /***  Iterator for {@link #registrants}. */
-    private Iterator<Registrant> iterator;
-
-    /**
-     * Return the temporary collection of {@link Registrant}.
-     *
-     * @return collection of {@link Registrant}.
-     */
-    public List<Registrant> getRegistrants() {
+    protected List<Registrant> getRegistrants() {
         return registrants;
     }
 
-    Iterator<Registrant> getIterator() {
+    private Iterator<Registrant> getIterator() {
         if (iterator == null) iterator = registrants.iterator();
         return iterator;
     }
 
     @Override
-    public void close() {
-        registrants.clear();
+    public void setEnvironment(@NonNull Environment environment) {
+        activeProfiles = Arrays.asList(environment.getActiveProfiles());
+    }
+
+    @Override
+    public void add(Class<?> targetType, Object target) {
+        for (Method method : findAndFilterAnnotatedMethods(targetType)) {
+            CronAnnotationAttributes cronAttribute = CronAnnotationAttributes.of(method);
+            String expression = getExpression(cronAttribute.getExpression());
+            Runnable runnable = () -> ReflectionUtils.invokeMethod(method, target);
+            if (CollectionUtils.isEmpty(activeProfiles)
+                    || Arrays.stream(cronAttribute.getProfiles()).anyMatch(activeProfiles::contains)) {
+                addRunnableRegistrant(expression, runnable, method);
+            }
+        }
+    }
+
+    protected List<Method> findAndFilterAnnotatedMethods(Class<?> realBeanType) {
+        Method[] uniqueDeclaredMethods = ReflectionUtils.getUniqueDeclaredMethods(realBeanType, method -> {
+            int modifiers = method.getModifiers();
+            return method.isAnnotationPresent(Cron.class)
+                    && !Modifier.isStatic(modifiers) && Modifier.isPublic(modifiers);
+        });
+        return ArrayUtils.isEmpty(uniqueDeclaredMethods) ? Collections.emptyList() :
+                new ArrayList<>(Arrays.asList(uniqueDeclaredMethods));
+    }
+
+    protected String getExpression(String expression) {
+        return expression;
+    }
+
+    protected void addRunnableRegistrant(String expression, Runnable runnable, Method method) {
+        getRegistrants().add(new DefaultRegistrant(expression, runnable));
     }
 
     @Override
@@ -72,45 +96,8 @@ public abstract class AbstractRegistrantCollector implements RegistrantCollector
         return getIterator().next();
     }
 
-    /**
-     * Add a {@link Registrant}.
-     *
-     * @param registrant Pending registration.
-     */
-    public void addRegistrant(@NonNull Registrant registrant) {
-        getRegistrants().add(registrant);
-    }
-
-    /**
-     * Return the content of the {@link AnnotatedElement} annotation
-     * obtained based on the method or type of {@link top.osjf.cron.spring.annotation.Cron}.
-     *
-     * @param element program elements that can carry annotations.
-     * @return {@link top.osjf.cron.spring.annotation.Cron}'s attributes.
-     */
-    protected CronAnnotationAttributes getCronAttribute(AnnotatedElement element) {
-        return CronAnnotationAttributes.of(element);
-    }
-
-    /**
-     * Check if the required operating environment is within the list of activated environments.
-     *
-     * @param providerProfiles The set of execution environment names that the
-     *                         registration task aims to satisfy.
-     * @param activeProfiles   The current set of activated environments.
-     * @return if {@code true} allow registration ,otherwise no allowed.
-     */
-    protected boolean profilesCheck(String[] providerProfiles, String[] activeProfiles) {
-        if (ArrayUtils.isEmpty(activeProfiles)) {
-            //When the environment is not activated, it indicates that
-            // everything is applicable and can be registered directly.
-            return true;
-        }
-        if (ArrayUtils.isEmpty(providerProfiles)) {
-            //When no running environment is provided, register directly.
-            return true;
-        }
-        //Adaptation provides the presence of the required environment in the activated environment.
-        return Arrays.stream(providerProfiles).anyMatch(p -> Arrays.asList(activeProfiles).contains(p));
+    @Override
+    public void close() {
+        registrants.clear();
     }
 }
