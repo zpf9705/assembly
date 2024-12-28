859d35d8b5744c05efcd38f934b9fa5d71be9a10
zhangpengfei <929160069@qq.com>
2024-12-28 15:22:50 +0800

    Add component support for SDK for HTTP requests.

--- assembly-simplified-cron/cron-core/src/main/java/top/osjf/cron/core/CronTask.java
+++ assembly-simplified-cron/cron-core/src/main/java/top/osjf/cron/core/CronTask.java
@@ -17,47 +17,26 @@
 
 package top.osjf.cron.core;
 
-import top.osjf.cron.core.lang.NotNull;
-import top.osjf.cron.core.lang.Nullable;
-
-import java.lang.reflect.Method;
-import java.util.Objects;
-
 /**
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.0.3
  */
 public final class CronTask {
 
-    @NotNull
     private final String expression;
 
-    @NotNull
-    private final Runnable runnable;
+    private final CronMethodRunnable runnable;
 
-    @Nullable
-    private final Method method;
-
-    public CronTask(@NotNull String expression, @NotNull Runnable runnable, @Nullable Method method) {
-        Objects.requireNonNull(expression,"<expression> == <null>");
-        Objects.requireNonNull(expression,"<runnable> == <null>");
+    public CronTask(String expression, CronMethodRunnable runnable) {
         this.expression = expression;
         this.runnable = runnable;
-        this.method = method;
     }
 
-    @NotNull
     public String getExpression() {
         return expression;
     }
 
-    @NotNull
-    public Runnable getRunnable() {
+    public CronMethodRunnable getRunnable() {
         return runnable;
-    }
-
-    @Nullable
-    public Method getMethod() {
-        return method;
     }
 }
