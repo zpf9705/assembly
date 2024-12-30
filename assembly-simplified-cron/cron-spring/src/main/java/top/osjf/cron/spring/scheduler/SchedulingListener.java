b48c24b3bf740d1b4a195bb9adde22b7a94f297c
zhangpengfei <929160069@qq.com>
2024-12-30 12:17:51 +0800

    Add component support for SDK for HTTP requests.

--- assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/scheduler/SchedulingListener.java
+++ assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/scheduler/SchedulingListener.java
@@ -17,12 +17,40 @@
 package top.osjf.cron.spring.scheduler;
 
 import top.osjf.cron.core.listener.CronListener;
+import top.osjf.cron.core.listener.ListenerContext;
 
 /**
- * Task execution cycle monitoring callback interface.
+ * The spring scheduling cron task listener {@link CronListener}.
  *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.0.0
  */
-public interface SchedulingListener extends CronListener<SchedulingInfo> {
+public class SchedulingListener implements CronListener {
+
+    @Override
+    public void start(ListenerContext context) {
+        CronListener.super.start(context);
+    }
+
+    @Override
+    public void success(ListenerContext context) {
+        CronListener.super.success(context);
+    }
+
+    @Override
+    public void failed(ListenerContext context, Throwable exception) {
+        CronListener.super.failed(context, exception);
+    }
+
+    @Override
+    public void startWithId(String id) {
+    }
+
+    @Override
+    public void successWithId(String id) {
+    }
+
+    @Override
+    public void failedWithId(String id, Throwable exception) {
+    }
 }
