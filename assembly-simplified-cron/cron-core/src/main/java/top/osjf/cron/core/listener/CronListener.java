84ff5c4e6abe9d474e8961f86b8822a52c74d337
zhangpengfei <929160069@qq.com>
2024-12-30 10:28:48 +0800

    Add component support for SDK for HTTP requests.

--- assembly-simplified-cron/cron-core/src/main/java/top/osjf/cron/core/listener/CronListener.java
+++ assembly-simplified-cron/cron-core/src/main/java/top/osjf/cron/core/listener/CronListener.java
@@ -17,42 +17,131 @@
 package top.osjf.cron.core.listener;
 
 /**
- * The CronListener interface defines the event listening methods
- * during the execution of scheduled tasks.
+ * The scheduled task callback interface defines callback methods for task start,
+ * successful completion, and failure.
  *
- * <p>Classes implementing this interface can listen to the start, successful
- * completion, and failure events of scheduled tasks,and perform corresponding
- * processing accordingly.
+ * <p>This interface aims to provide a standardized callback mechanism for timed task
+ * frameworks, allowing developers to execute specific logic at different stages of
+ * task execution.
  *
- * @param <T> The generic T represents the data type passed to the listener methods,
- *            allowing different tasks to pass different data.
+ * <p>By implementing this interface, the framework can trigger corresponding callback
+ * methods at the beginning, success, or failure of tasks, allowing developers to implement
+ * flexible and scalable task monitoring and processing mechanisms without affecting task
+ * execution logic.
+ *
+ * <p>The three default methods in the interface are {@link #start}, {@link #success}, and
+ * {@link #failed} provides developers with a convenient callback entry. These methods are
+ * called at different stages of task execution and provide necessary context and error
+ * information to the implementation class through the passed {@link ListenerContext} object
+ * or exception object. Implementation classes can execute specific listening logic based on
+ * this information, such as logging, status updates, notification sending, or error handling.
+ *
+ * <p>In addition, the interface also defines three abstract methods ({@link #startWithId},
+ * {@link #successWithId}, and {@link #failedWithId}), these methods need to be implemented by
+ * the implementation class itself. These methods receive the unique identifier (or ID) of the
+ * task and (in case of failure) the exception object as parameters, allowing the implementation
+ * class to execute more specific listening logic based on the task ID and error information.
+ *
+ * <p>When designing and implementing a scheduled task system, this interface provides key
+ * information transmission and callback execution mechanisms. By implementing this interface,
+ * developers can create custom listener classes and trigger corresponding callback methods at
+ * different stages of task execution, thereby achieving flexible task monitoring and processing
+ * strategies.
+ *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.0.0
  */
-public interface CronListener<T> {
+public interface CronListener {
 
     /**
-     * This method is triggered when a scheduled task starts.
+     * The default callback method executed at the beginning of a
+     * scheduled task.
      *
-     * @param value The parameters passed when starting the scheduled task.
+     * <p>This method triggers callback logic by calling the {@link #startWithId}
+     * abstract method and passing the unique identifier of the context object.
+     *
+     * <p>It provides developers with a convenient way to listen for the
+     * start event of scheduled tasks without directly implementing the method.
+     *
+     * @param context listen to the context {@code ListenerContext} object.
      */
-    void onStart(T value);
+    default void start(ListenerContext context) {
+        startWithId(context.getID());
+    }
 
     /**
-     * This method is triggered when the scheduled task successfully ends.
+     * The default callback method executed when a scheduled task is successfully
+     * completed.
      *
-     * @param value The parameters passed when the scheduled task successfully
-     *              completes.
+     * <p>This method triggers callback logic by calling the {@link #successWithId}
+     * abstract method and passing the unique identifier of the context object.
+     *
+     * <p>It provides developers with a convenient way to listen for the succeeded
+     * event of scheduled tasks without directly implementing the method.
+     *
+     * @param context listen to the context {@code ListenerContext} object.
      */
-    void onSucceeded(T value);
+    default void success(ListenerContext context) {
+        successWithId(context.getID());
+    }
 
     /**
-     * This method is triggered when the scheduled task fails to start.
+     * The default callback method executed when a scheduled task fails.
      *
-     * @param value     The parameters passed when attempting to start the
-     *                  scheduled task.
-     * @param exception The specific exception captured by the listener, indicating
-     *                  the reason for the failure of the scheduled task to start.
+     * <p>This method triggers callback logic by calling the {@link #failedWithId}
+     * abstract method and passing the unique identifier of the context object and
+     * exception object.
+     *
+     * <p>It provides developers with a convenient way to listen for the failed event
+     * of scheduled tasks without directly implementing the method.
+     *
+     * @param context   listen to the context {@code ListenerContext} object.
+     * @param exception the exception object of the failed callback contains specific
+     *                  failure information.
      */
-    void onFailed(T value, Throwable exception);
+    default void failed(ListenerContext context, Throwable exception) {
+        failedWithId(context.getID(), exception);
+    }
+
+    /**
+     * The callback method executed at the beginning of a scheduled task needs to be
+     * defined by the implementation framework's own {@link ListenerContext}.
+     *
+     * <p>This method receives a unique identifier as a parameter to identify the timed
+     * task that triggers the callback. The implementation class should define specific
+     * listening logic in this method, such as logging, updating status, or triggering
+     * other related operations.
+     *
+     * @param id unique identifier for scheduled tasks.
+     */
+    void startWithId(String id);
+
+    /**
+     * The callback method executed upon successful completion of scheduled tasks requires
+     * the implementation framework to define its own {@link ListenerContext}.
+     *
+     * <p>This method receives a unique identifier as a parameter to identify the timed task
+     * that triggers the callback. The implementation class should define specific listening
+     * logic in this method, such as logging, updating status, sending notifications, or
+     * triggering other related operations
+     *
+     * @param id unique identifier for scheduled tasks.
+     */
+    void successWithId(String id);
+
+    /**
+     * The callback method executed when a scheduled task fails successfully requires the
+     * implementation framework to define its own {@link ListenerContext}.
+     *
+     * <p>This method receives a unique identifier and an exception message as parameters
+     * to identify the timed task that triggers the callback and provide detailed error
+     * information. The implementation class should define specific listening logic in
+     * this method, such as recording error logs, sending alerts, updating status, or
+     * performing other error handling operations.
+     *
+     * @param id        unique identifier for scheduled tasks.
+     * @param exception the exception object of the failed callback contains specific
+     *                  failure information.
+     */
+    void failedWithId(String id, Throwable exception);
 }
