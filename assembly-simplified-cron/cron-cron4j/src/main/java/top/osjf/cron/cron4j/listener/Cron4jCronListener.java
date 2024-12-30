d02fe9cf9542a25bee655c846e077259c3bbdb95
zhangpengfei <929160069@qq.com>
2024-12-30 12:12:35 +0800

    Add component support for SDK for HTTP requests.

--- assembly-simplified-cron/cron-cron4j/src/main/java/top/osjf/cron/cron4j/listener/Cron4jCronListener.java
+++ assembly-simplified-cron/cron-cron4j/src/main/java/top/osjf/cron/cron4j/listener/Cron4jCronListener.java
@@ -21,25 +21,41 @@ import it.sauronsoftware.cron4j.TaskExecutor;
 import top.osjf.cron.core.listener.CronListener;
 
 /**
- * The Cron4j cron task {@link CronListener},reply to {@link SchedulerListener}.
+ * The Cron4j cron task listener {@link CronListener}.
  *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
- * @since 1.0.0
+ * @since 1.0.3
  */
-public interface Cron4jCronListener extends SchedulerListener, CronListener<TaskExecutor> {
+public class Cron4jCronListener implements SchedulerListener, CronListener {
 
     @Override
-    default void taskLaunching(TaskExecutor executor) {
-        onStart(executor);
+    public final void taskLaunching(TaskExecutor executor) {
+        start(newCron4jListenerContent(executor));
     }
 
     @Override
-    default void taskSucceeded(TaskExecutor executor) {
-        onSucceeded(executor);
+    public final void taskSucceeded(TaskExecutor executor) {
+        success(newCron4jListenerContent(executor));
     }
 
     @Override
-    default void taskFailed(TaskExecutor executor, Throwable exception) {
-        onFailed(executor, exception);
+    public final void taskFailed(TaskExecutor executor, Throwable exception) {
+        failed(newCron4jListenerContent(executor), exception);
+    }
+
+    final Cron4jListenerContent newCron4jListenerContent(TaskExecutor executor) {
+        return new Cron4jListenerContent(String.valueOf(executor.getTask().getId()), executor);
+    }
+
+    @Override
+    public void startWithId(String id) {
+    }
+
+    @Override
+    public void successWithId(String id) {
+    }
+
+    @Override
+    public void failedWithId(String id, Throwable exception) {
     }
 }
