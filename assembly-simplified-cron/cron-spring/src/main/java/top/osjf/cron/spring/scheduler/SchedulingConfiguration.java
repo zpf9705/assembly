dfa3f08d142f9daf2b6b004a680c7824a7bb4387
zhangpengfei <929160069@qq.com>
2024-12-31 13:13:18 +0800

    Add component support for SDK for HTTP requests.

--- assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/scheduler/SchedulingConfiguration.java
+++ assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/scheduler/SchedulingConfiguration.java
@@ -19,36 +19,33 @@ package top.osjf.cron.spring.scheduler;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Role;
-import org.springframework.scheduling.annotation.ScheduledAnnotationBeanPostProcessor;
-import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.config.TaskManagementConfigUtils;
 
 /**
- * Enhanced version configuration {@link org.springframework.scheduling.annotation.SchedulingConfiguration}.
+ * {@code @Configuration} class that registers a {@link ScheduledAnnotationBeanPostProcessor}
+ * bean capable of processing Spring's @{@link Scheduled} annotation.
+ *
+ * <p>This configuration class is automatically imported when using the
+ * {@link EnableScheduling @EnableScheduling} annotation. See
+ * {@code @EnableScheduling}'s javadoc for complete usage details.
  *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.0.0
+ * @see EnableScheduling
  */
 @Configuration(proxyBeanMethods = false)
-@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 public class SchedulingConfiguration {
-
-    /**
-     * @param schedulingRepository scheduling repository.
-     * @return {@link ScheduledAnnotationBeanPostProcessor}
-     * @see ScheduledAnnotationBeanPostProcessor#ScheduledAnnotationBeanPostProcessor(ScheduledTaskRegistrar)
-     */
-    @Bean(name = TaskManagementConfigUtils.SCHEDULED_ANNOTATION_PROCESSOR_BEAN_NAME)
-    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
-    public ScheduledAnnotationBeanPostProcessor scheduledAnnotationProcessor(@Lazy SchedulingRepository
-                                                                                     schedulingRepository) {
-        return new ScheduledAnnotationBeanPostProcessor(schedulingRepository.getTaskRegistrar());
-    }
 
     @Bean
     public SchedulingRepository schedulingRepository() {
         return new SchedulingRepository();
+    }
+
+    @Bean(name = TaskManagementConfigUtils.SCHEDULED_ANNOTATION_PROCESSOR_BEAN_NAME)
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+    public ScheduledAnnotationBeanPostProcessor scheduledAnnotationProcessor() {
+        return new ScheduledAnnotationBeanPostProcessor();
     }
 }
