0c57a67401cb522a46a5f5ae24d879c4845107f3
zhangpengfei <929160069@qq.com>
2025-01-01 10:47:33 +0800

    Add component support for SDK for HTTP requests.

--- assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/quartz/QuartzJobFactory.java
+++ assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/quartz/QuartzJobFactory.java
@@ -18,10 +18,7 @@ package top.osjf.cron.spring.quartz;
 
 import org.quartz.Job;
 import org.quartz.JobDetail;
-import org.quartz.JobKey;
-import org.quartz.Scheduler;
 import org.quartz.spi.JobFactory;
-import org.quartz.spi.TriggerFiredBundle;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -33,8 +30,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.lang.NonNull;
 import org.springframework.util.ClassUtils;
-import org.springframework.util.ReflectionUtils;
+import top.osjf.cron.core.util.ReflectUtils;
 import top.osjf.cron.quartz.MethodLevelJob;
+import top.osjf.cron.quartz.MethodLevelJobFactory;
 
 import java.lang.reflect.Method;
 
@@ -46,12 +44,12 @@ import java.lang.reflect.Method;
  * <p>Convert Quartz's scheduled tasks for class structure to method level,
  * use the registered {@link JobDetail} to obtain the specific method for
  * executing the bean, and dynamically create a bean to execute the scheduled
- * task. The structure can be viewed from {@link RunnableJob}.
+ * task. The structure can be viewed from {@link MethodLevelJob}.
  *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.0.0
  */
-public class QuartzJobFactory implements JobFactory, ApplicationContextAware, BeanClassLoaderAware {
+public class QuartzJobFactory extends MethodLevelJobFactory implements ApplicationContextAware, BeanClassLoaderAware {
 
     private ApplicationContext applicationContext;
 
@@ -71,59 +69,21 @@ public class QuartzJobFactory implements JobFactory, ApplicationContextAware, Be
     }
 
     @Override
-    public Job newJob(TriggerFiredBundle bundle, Scheduler scheduler) {
-        JobDetail jobDetail = bundle.getJobDetail();
-
-        // MethodJob assignable is method level scheduled task
-        //Dynamically create beans and execute them
-        if (MethodLevelJob.class.isAssignableFrom(jobDetail.getJobClass())) {
-            return getMethodJobBean(jobDetail);
-        }
-
-        //If an operation class is used and a Job task is directly registered,
-        // it can still be obtained directly from the Spring container.
-        return applicationContext.getBean(jobDetail.getJobClass());
-    }
-
-    private RunnableJob getMethodJobBean(JobDetail jobDetail) {
-
-        JobKey key = jobDetail.getKey();
-
-        String beanName = key.toString();
-
+    protected Job getJob(String declaringClassName, String methodName) {
+        final String beanName = methodName + "@" + declaringClassName;
         if (applicationContext.containsBean(beanName)) {
-
-            return applicationContext.getBean(beanName, RunnableJob.class);
+            return applicationContext.getBean(beanName, MethodLevelJob.class);
         }
-
-        return registerAndInitialize(key, beanName);
-    }
-
-    private RunnableJob registerAndInitialize(JobKey key, String beanName) {
-
-        BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(RunnableJob.class)
+        Class<?> beanClassName = ClassUtils.resolveClassName(declaringClassName, classLoader);
+        Object targetBean = applicationContext.getBean(beanClassName);
+        Method method = ReflectUtils.getMethod(beanClassName, methodName);
+        BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(MethodLevelJob.class)
                 .setScope(BeanDefinition.SCOPE_SINGLETON)
-                .addConstructorArgValue(getRunnable(key.getGroup(), key.getName()));
-
-        BeanDefinitionReaderUtils.registerBeanDefinition(new BeanDefinitionHolder(builder.getBeanDefinition(),
-                beanName), beanDefinitionRegistry);
-
-        return applicationContext.getBean(beanName, RunnableJob.class);
-    }
-
-    private Runnable getRunnable(String beanClassName, String methodName) {
-
-        return () -> {
-
-            Class<?> clazz = ClassUtils.resolveClassName(beanClassName, classLoader);
-
-            Method method = ReflectionUtils.findMethod(clazz, methodName);
-
-            if (method == null) {
-                throw new IllegalArgumentException(new NoSuchMethodException(methodName));
-            }
-
-            ReflectionUtils.invokeMethod(method, applicationContext.getBean(clazz));
-        };
+                .addConstructorArgValue(targetBean)
+                .addConstructorArgValue(method);
+        BeanDefinitionReaderUtils
+                .registerBeanDefinition(new BeanDefinitionHolder(builder.getBeanDefinition(), beanName),
+                        beanDefinitionRegistry);
+        return applicationContext.getBean(beanName, MethodLevelJob.class);
     }
 }
