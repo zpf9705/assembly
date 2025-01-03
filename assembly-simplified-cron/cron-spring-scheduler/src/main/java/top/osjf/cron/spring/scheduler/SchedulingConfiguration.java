e46e8750978d69b6f6b1ea39b92e5cc6c822184a
zhangpengfei <929160069@qq.com>
2025-01-03 11:42:17 +0800

    Add component support for SDK for HTTP requests.

--- assembly-simplified-cron/cron-spring-scheduler/src/main/java/top/osjf/cron/spring/scheduler/SchedulingConfiguration.java
+++ assembly-simplified-cron/cron-spring-scheduler/src/main/java/top/osjf/cron/spring/scheduler/SchedulingConfiguration.java
@@ -21,7 +21,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Role;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.scheduling.config.TaskManagementConfigUtils;
 
 /**
@@ -37,17 +36,8 @@ import org.springframework.scheduling.config.TaskManagementConfigUtils;
  * @see EnableScheduling
  */
 @Configuration(proxyBeanMethods = false)
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 public class SchedulingConfiguration {
-
-    @Bean
-    public SchedulingRepository schedulingRepository() {
-        return new SchedulingRepository();
-    }
-
-    @Bean
-    public ScheduledTaskRegistrar scheduledTaskRegistrar(SchedulingRepository schedulingRepository){
-        return schedulingRepository.getScheduledTaskRegistrar();
-    }
 
     @Bean(name = TaskManagementConfigUtils.SCHEDULED_ANNOTATION_PROCESSOR_BEAN_NAME)
     @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
