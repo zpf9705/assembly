4c2b84fc0cbc8c92b31856f8347994c3051b15e4
zhangpengfei <929160069@qq.com>
2025-01-03 10:57:18 +0800

    Add component support for SDK for HTTP requests.

--- assembly-spring-boot-autoconfigure/src/main/java/top/osjf/spring/autoconfigure/cron/QuartzPropertiesCustomizer.java
+++ assembly-spring-boot-autoconfigure/src/main/java/top/osjf/spring/autoconfigure/cron/QuartzPropertiesCustomizer.java
@@ -1,10 +1,10 @@
 package top.osjf.spring.autoconfigure.cron;
 
-import java.util.Properties;
+import top.osjf.cron.core.lifecycle.SuperiorProperties;
 
 /**
  * Callback interface that can be implemented by beans wishing to customize Quartz's
- * {@link Properties Quartz Properties} before it is used.
+ * {@link SuperiorProperties Quartz Properties} before it is used.
  *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.0.1
@@ -13,7 +13,7 @@ public interface QuartzPropertiesCustomizer {
 
     /**
      * Customize the Quartz Properties.
-     * @param properties the {@link Properties} to customize.
+     * @param properties the {@link SuperiorProperties} to customize.
      */
-    void customize(Properties properties);
+    void customize(SuperiorProperties properties);
 }
