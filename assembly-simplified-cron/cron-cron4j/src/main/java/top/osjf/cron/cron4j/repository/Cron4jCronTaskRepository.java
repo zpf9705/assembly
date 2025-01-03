ff3c2ef0ef5ded5b8042fce613c57a48bf8dfe85
zhangpengfei <929160069@qq.com>
2025-01-03 09:18:27 +0800

    Add component support for SDK for HTTP requests.

--- assembly-simplified-cron/cron-cron4j/src/main/java/top/osjf/cron/cron4j/repository/Cron4jCronTaskRepository.java
+++ assembly-simplified-cron/cron-cron4j/src/main/java/top/osjf/cron/cron4j/repository/Cron4jCronTaskRepository.java
@@ -19,9 +19,14 @@ package top.osjf.cron.cron4j.repository;
 import it.sauronsoftware.cron4j.InvalidPatternException;
 import it.sauronsoftware.cron4j.Scheduler;
 import top.osjf.cron.core.lang.NotNull;
+import top.osjf.cron.core.lifecycle.SuperiorProperties;
 import top.osjf.cron.core.listener.CronListener;
 import top.osjf.cron.core.repository.*;
 import top.osjf.cron.cron4j.listener.Cron4jCronListener;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.TimeZone;
 
 /**
  * The Cron4j cron task repository {@link CronTaskRepository}.
@@ -31,23 +36,110 @@ import top.osjf.cron.cron4j.listener.Cron4jCronListener;
  */
 public class Cron4jCronTaskRepository implements CronTaskRepository {
 
-    /*** scheduler management*/
-    private final Scheduler scheduler;
+    /**
+     * The daemon flag. If true the scheduler and its spawned threads acts like
+     * daemons.
+     */
+    private boolean daemon;
 
     /**
-     * Construct for create {@link Scheduler}.
+     * The time zone applied by the scheduler.
+     */
+    private TimeZone timezone = TimeZone.getDefault();
+
+    private Scheduler scheduler;
+
+    private boolean setDaemon;
+    private boolean setTimeZone;
+
+    /**
+     * @since 1.0.3
      */
     public Cron4jCronTaskRepository() {
-        this.scheduler = new Scheduler();
     }
 
     /**
-     * Return the scheduled task management class of Cron4j.
-     *
-     * @return {@link Scheduler}.
+     * Creates a {@code Cron4jCronTaskRepository} by given {@code Scheduler} instance.
+     * @param scheduler the given {@code Scheduler} instance after initialize.
+     * @since 1.0.3
      */
-    public Scheduler getScheduler() {
-        return scheduler;
+    public Cron4jCronTaskRepository(Scheduler scheduler) {
+        this.scheduler = scheduler;
+    }
+
+    /**
+     * Marks this scheduler daemon flag. When a scheduler is marked as a daemon
+     * scheduler it spawns only daemon threads. The Java Virtual Machine exits
+     * when the only threads running are all daemon threads.
+     * <p>
+     * This method must be called before the scheduler is started.
+     *
+     * @param daemon If true, the scheduler will spawn only daemon threads.
+     * @throws IllegalStateException If the scheduler is started.
+     */
+    public void setDaemon(boolean daemon) {
+        this.daemon = daemon;
+        setDaemon = true;
+    }
+
+    /**
+     * <p>
+     * Sets the time zone applied by the scheduler.
+     * </p>
+     * <p>
+     * Current system time is adapted to the supplied time zone before comparing
+     * it with registered scheduling patterns. The result is that any supplied
+     * scheduling pattern is treated according to the specified time zone. In
+     * example, suppose:
+     * </p>
+     * <ul>
+     * <li>System time: 10:00</li>
+     * <li>System time zone: GMT+1</li>
+     * <li>Scheduler time zone: GMT+3</li>
+     * </ul>
+     * <p>
+     * The scheduler, before comparing system time with patterns, translates
+     * 10:00 from GMT+1 to GMT+3. It means that 10:00 becomes 12:00. The
+     * resulted time is then used by the scheduler to activate tasks. So, in the
+     * given configuration at the given moment, any task scheduled as
+     * <em>0 12 * * *</em> will be executed, while any <em>0 10 * * *</em> will
+     * not.
+     * </p>
+     *
+     * @param timezone The time zone applied by the scheduler.
+     */
+    public void setTimezone(TimeZone timezone) {
+        this.timezone = timezone;
+        setTimeZone = true;
+    }
+
+    /**
+     * Set the parameter {@link SuperiorProperties} object for building the cron4j task
+     * scheduler, compatible with the Cron framework startup parameter series.
+     *
+     * <p>The configuration file cannot overwrite the value set by the external active
+     * call to the set method.
+     *
+     * @param superiorProperties {@link SuperiorProperties} object for building the cron4j
+     *                           task scheduler.
+     * @since 1.0.3
+     */
+    public void setProperties(SuperiorProperties superiorProperties) {
+        if (superiorProperties != null) {
+            if (!setDaemon)
+                setDaemon(superiorProperties.getProperty("daemon", false));
+            if (!setTimeZone)
+                setTimezone(superiorProperties.getProperty("timeZone", TimeZone.getDefault()));
+        }
+    }
+
+    @PostConstruct
+    public void initialize() {
+        if (scheduler == null) {
+            scheduler = new Scheduler();
+            scheduler.setDaemon(daemon);
+            scheduler.setTimeZone(timezone);
+        }
     }
 
     /**
@@ -66,7 +158,7 @@ public class Cron4jCronTaskRepository implements CronTaskRepository {
     @NotNull
     public String register(@NotNull String expression, @NotNull TaskBody body) {
         return RepositoryUtils.doRegister(() ->
-                scheduler.schedule(expression, body.unwrap(RunnableTaskBody.class).getRunnable()),
+                        scheduler.schedule(expression, body.unwrap(RunnableTaskBody.class).getRunnable()),
                 InvalidPatternException.class);
     }
 
@@ -107,5 +199,21 @@ public class Cron4jCronTaskRepository implements CronTaskRepository {
     @Override
     public void removeListener(@NotNull CronListener listener) {
         scheduler.removeSchedulerListener(listener.unwrap(Cron4jCronListener.class));
+    }
+
+    @Override
+    public void start() {
+        scheduler.start();
+    }
+
+    @Override
+    @PreDestroy
+    public void stop() {
+        scheduler.stop();
+    }
+
+    @Override
+    public boolean isStarted() {
+        return scheduler.isStarted();
     }
 }
