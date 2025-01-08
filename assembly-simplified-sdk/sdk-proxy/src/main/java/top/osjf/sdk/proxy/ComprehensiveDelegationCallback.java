7ce022567de2afff4721d19b33de089adab84f11
zhangpengfei <929160069@qq.com>
2025-01-08 14:04:59 +0800

    Add component support for SDK for HTTP requests.

--- assembly-simplified-sdk/sdk-proxy/src/main/java/top/osjf/sdk/proxy/ComprehensiveDelegationCallback.java
+++ assembly-simplified-sdk/sdk-proxy/src/main/java/top/osjf/sdk/proxy/ComprehensiveDelegationCallback.java
@@ -174,7 +174,6 @@ public class ComprehensiveDelegationCallback implements RequestAttributes,
         switch (method.getName()){
             case "toString": return toString();
             case "hashCode": return hashCode();
-            case "equals": return equals(args[0]);
         }
         Pair<Request<?>, List<Callback>> pair = SdkSupport.createRequest(method, args);
         Request<?> request = pair.getFirst();
