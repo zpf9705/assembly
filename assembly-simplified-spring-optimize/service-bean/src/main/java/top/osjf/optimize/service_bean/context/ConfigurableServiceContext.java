3e2be26a6ad26d157eaeff1096ffa857219b1aa4
zhangpengfei <929160069@qq.com>
2025-02-13 14:06:17 +0800

    Add component support for SDK for HTTP requests.

--- assembly-simplified-spring-optimize/service-bean/src/main/java/top/osjf/optimize/service_bean/context/ConfigurableServiceContext.java
+++ assembly-simplified-spring-optimize/service-bean/src/main/java/top/osjf/optimize/service_bean/context/ConfigurableServiceContext.java
@@ -17,11 +17,14 @@
 
 package top.osjf.optimize.service_bean.context;
 
+import org.springframework.beans.factory.DisposableBean;
+
 import java.io.Closeable;
+import java.io.IOException;
 
 /**
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
- * @since 2025.02.13
+ * @since 1.0.3
  */
 public interface ConfigurableServiceContext extends ServiceContext, Closeable {
 
@@ -37,4 +40,62 @@ public interface ConfigurableServiceContext extends ServiceContext, Closeable {
      * @see org.springframework.context.annotation.AnnotationScopeMetadataResolver
      */
     String SUPPORT_SCOPE = "service";
+
+    /**
+     * Return a boolean tag representing the result of deleting the corresponding
+     * service instance using the provided name.
+     *
+     * <p>The removed service will be destroyed in the Spring container at the
+     * same time as it is removed in the context of this service, with a callback
+     * {@link DisposableBean}.
+     *
+     * <p>Only applicable for deleting service classes that have specific scope
+     * {@link org.springframework.beans.factory.config.Scope} storage, such as
+     * the currently supported {@link ServiceScope}, developers can push this type.
+     *
+     * <p>
+     * <strong>Note:</strong>
+     * <p>The transmission of this service name requires the use of the original core
+     * processing class {@link ServiceDefinitionUtils} method {@code ServiceCore#enhancement}
+     * and it must be a service name ending with {@code ServiceCore#BEAN_NAME_CLOSE_TAG}
+     * , which has been specially processed.
+     *
+     * @param serviceName the name of the service to remove.
+     * @return If {@code true} is returned, the service has been successfully removed,
+     * otherwise it does not exist or not end with {@code ServiceCore#BEAN_NAME_CLOSE_TAG}.
+     * @throws NullPointerException if input serviceName is {@literal null}.
+     */
+    boolean removeService(String serviceName);
+
+    /**
+     * Return a boolean type result representing the result of service removal,
+     * based on the re encoding of the provided service name and type.
+     *
+     * <p>The removed service will be destroyed in the Spring container at the
+     * same time as it is removed in the context of this service, with a callback
+     * {@link DisposableBean}.
+     *
+     * <p>Only applicable for deleting service classes that have specific scope
+     * {@link org.springframework.beans.factory.config.Scope} storage, such as
+     * the currently supported {@link ServiceScope}, developers can push this type.
+     *
+     * <p>
+     * <strong>Note:</strong>
+     * <p>Due to the limitations of the Spring framework for removing beans from
+     * specific scopes, the {@code requiredType} of this method must be the native
+     * type of the service class used to construct the name of the bean for removing
+     * beans from specific scopes.
+     *
+     * @param serviceName  the name of the service to remove.
+     * @param requiredType type the bean must match; can be an interface or superclass.
+     * @param <S>          the type of service to remove.
+     * @return If {@code true} is returned, the service has been successfully removed,
+     * otherwise it does not exist or not end with {@code ServiceCore#BEAN_NAME_CLOSE_TAG}.
+     * @throws NullPointerException if input serviceName or requiredType is {@literal null}.
+     * @since 1.0.2
+     */
+    <S> boolean removeService(String serviceName, Class<S> requiredType);
+
+    @Override
+    void close() throws IOException;
 }
