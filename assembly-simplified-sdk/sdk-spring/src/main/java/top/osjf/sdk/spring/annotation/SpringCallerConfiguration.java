be0ed6ac12a7dcf0ec5dda6a3edad9b628bfa0f0
zhangpengfei <929160069@qq.com>
2025-02-20 15:27:22 +0800

    ResponseData.java # resolveResponse # Resolve the issue of data values not being set when successful checks are not required.

--- assembly-simplified-sdk/sdk-spring/src/main/java/top/osjf/sdk/spring/annotation/SpringCallerConfiguration.java
+++ assembly-simplified-sdk/sdk-spring/src/main/java/top/osjf/sdk/spring/annotation/SpringCallerConfiguration.java
@@ -20,12 +20,7 @@ package top.osjf.sdk.spring.annotation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import top.osjf.sdk.core.caller.CallOptions;
-import top.osjf.sdk.core.caller.RequestCaller;
-import top.osjf.sdk.core.util.ReflectUtil;
 import top.osjf.sdk.spring.SpringRequestCaller;
-import top.osjf.sdk.spring.proxy.SdkProxyFactoryBean;
-
-import java.lang.reflect.Field;
 
 /**
  * {@code SpringRequestCaller} configuration class, used to define
@@ -38,36 +33,12 @@ import java.lang.reflect.Field;
 public class SpringCallerConfiguration {
 
     /**
-     * The name of the internal bean {@code SpringRequestCaller}.
-     */
-    protected static final String INTERNAL_SPRING_REQUEST_CALLER
-            = "top.osjf.sdk.spring.SpringRequestCaller.internal";
-
-    /**
-     * the attribute name of {@code SpringRequestCaller} from the
-     * {@code AbstractSdkProxyBean} class.
-     */
-    protected static String SPRING_REQUEST_CALLER_FIELD_NAME;
-
-    /*
-     * Find the attribute name of SpringRequestCaller from the AbstractSdkProxyBean class.
-     * */
-    static {
-        for (Field declaredField : ReflectUtil.getAllDeclaredFields(SdkProxyFactoryBean.class)) {
-            if (RequestCaller.class.isAssignableFrom(declaredField.getType())) {
-                SPRING_REQUEST_CALLER_FIELD_NAME = declaredField.getName();
-                break;
-            }
-        }
-    }
-
-    /**
      * Create a {@code SpringRequestCaller} bean for sdk proxy bean
      * to support resolve {@link CallOptions} annotation.
      *
      * @return a singleton for {@code SpringRequestCaller}.
      */
-    @Bean(INTERNAL_SPRING_REQUEST_CALLER)
+    @Bean(SdkManagementConfigUtils.INTERNAL_SPRING_REQUEST_CALLER_BEAN_NAME)
     public SpringRequestCaller requestCaller() {
         return new SpringRequestCaller();
     }
