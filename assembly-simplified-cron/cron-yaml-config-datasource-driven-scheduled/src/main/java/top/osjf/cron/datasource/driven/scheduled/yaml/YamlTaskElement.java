886c2812654c185cde2fac4383f03340f6836285
zhangpengfei <929160069@qq.com>
2025-05-23 10:44:37 +0800

    1.0.4-alpha - for cron

---
+++ assembly-simplified-cron/cron-yaml-config-datasource-driven-scheduled/src/main/java/top/osjf/cron/datasource/driven/scheduled/yaml/YamlTaskElement.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package top.osjf.cron.datasource.driven.scheduled.yaml;
+
+import top.osjf.cron.core.util.StringUtils;
+import top.osjf.cron.datasource.driven.scheduled.TaskElement;
+
+import java.util.Map;
+
+/**
+ * {@code YamlTaskElement} class implements the TaskElement interface, mapping data from
+ * YAML configuration files to task elements.
+ *
+ * <p>The names of YAML configuration properties for tasks written by developers need to
+ * be based on constants written in this category, such as {@code x123355:{id:x123355 ,
+ * taskName: example}}.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.4
+ */
+public class YamlTaskElement implements TaskElement {
+
+    private static final long serialVersionUID = -2374827664799945441L;
+
+    /**
+     * Source yaml config file key name of {@link TaskElement#getId()}.
+     */
+    public static final String ID_KEY_NAME = "id";
+
+    /**
+     * Source yaml config file key name of {@link TaskElement#getTaskId()}.
+     */
+    public static final String TASK_ID_KEY_NAME = "taskId";
+
+    /**
+     * Source yaml config file key name of {@link TaskElement#getTaskName()}.
+     */
+    public static final String TASK_NAME_KEY_NAME = "taskName";
+
+    /**
+     * Source yaml config file key name of {@link TaskElement#getProfiles()}.
+     */
+    public static final String PROFILES_KEY_NAME = "profiles";
+
+    /**
+     * Source yaml config file key name of {@link TaskElement#getTaskDescription()}.
+     */
+    public static final String TASK_DESCRIPTION_KEY_NAME = "taskDescription";
+
+    /**
+     * Source yaml config file key name of {@link TaskElement#getStatus()}.
+     */
+    public static final String STATUS_KEY_NAME = "status";
+
+    /**
+     * Source yaml config file key name of {@link TaskElement#getStatusDescription()}.
+     */
+    public static final String STATUS_DESCRIPTION_KEY_NAME = "statusDescription";
+
+    /**
+     * Source yaml config file key name of {@link TaskElement#getExpression()}.
+     */
+    public static final String EXPRESSION_KEY_NANE = "expression";
+
+    /**
+     * Source yaml config file key name of {@link TaskElement#getUpdateSign()}.
+     */
+    public static final String UPDATE_SIGN_KEY_NAME = "updateSign";
+
+    /**
+     * Source yaml config map corresponds to {@link #getId()}.
+     */
+    public final Map<String, String> sourceYamlConfig;
+
+    /**
+     * Constructs a {@code YamlTaskElement} with this {@link #getId()} corresponds to
+     * source yaml config map.
+     *
+     * @param sourceConfig the Source yaml config map.
+     */
+    public YamlTaskElement(Map<String, String> sourceConfig) {
+        this.sourceYamlConfig = sourceConfig;
+    }
+
+    @Override
+    public String getId() {
+        return sourceYamlConfig.get(ID_KEY_NAME);
+    }
+
+    @Override
+    public String getTaskId() {
+        return sourceYamlConfig.get(TASK_ID_KEY_NAME);
+    }
+
+    @Override
+    public void setTaskId(String taskId) {
+        sourceYamlConfig.put(TASK_ID_KEY_NAME, taskId);
+    }
+
+    @Override
+    public String getTaskName() {
+        return sourceYamlConfig.get(TASK_NAME_KEY_NAME);
+    }
+
+    @Override
+    public String getProfiles() {
+        return sourceYamlConfig.get(PROFILES_KEY_NAME);
+    }
+
+    @Override
+    public String getTaskDescription() {
+        return sourceYamlConfig.get(TASK_DESCRIPTION_KEY_NAME);
+    }
+
+    @Override
+    public String getStatus() {
+        return sourceYamlConfig.get(STATUS_KEY_NAME);
+    }
+
+    @Override
+    public void setStatus(String status) {
+        sourceYamlConfig.put(STATUS_KEY_NAME, status);
+    }
+
+    @Override
+    public String getStatusDescription() {
+        return sourceYamlConfig.get(STATUS_DESCRIPTION_KEY_NAME);
+    }
+
+    @Override
+    public void setStatusDescription(String statusDescription) {
+        sourceYamlConfig.put(STATUS_DESCRIPTION_KEY_NAME, statusDescription);
+    }
+
+    @Override
+    public String getExpression() {
+        return sourceYamlConfig.get(EXPRESSION_KEY_NANE);
+    }
+
+    @Override
+    public Integer getUpdateSign() {
+        return Integer.parseInt(sourceYamlConfig.get(UPDATE_SIGN_KEY_NAME));
+    }
+
+    @Override
+    public void setUpdateSign(Integer updateSign) {
+        sourceYamlConfig.put(UPDATE_SIGN_KEY_NAME, updateSign.toString());
+    }
+
+    /**
+     * @return Configure the relevant information corresponding to the unique ID of the task {@link #getId()}.
+     */
+    public Map<String, String> getSourceYamlConfig() {
+        return sourceYamlConfig;
+    }
+
+    /**
+     * Purge potential task IDs, update tags, and task status descriptions from the data.
+     *
+     * @param drivenTaskYamlConfig Complete reference configuration loaded.
+     * @return if {@code true} has been purge out，{@code false} otherwise.
+     */
+    protected boolean purge(Map<String, Map<String, String>> drivenTaskYamlConfig) {
+        if (!StringUtils.isBlank(getTaskId())) {
+            sourceYamlConfig.put(TASK_ID_KEY_NAME, "");
+            sourceYamlConfig.put(UPDATE_SIGN_KEY_NAME, "0");
+            sourceYamlConfig.put(STATUS_DESCRIPTION_KEY_NAME, "");
+            drivenTaskYamlConfig.put(getId(), sourceYamlConfig);
+            return true;
+        }
+        return false;
+    }
+}
