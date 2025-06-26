cb17317a7faa1c354db4d06d6155602ff4e13ea2
zhangpengfei <929160069@qq.com>
2025-06-26 14:01:17 +0800

    Idempotent verification support framework.

---
+++ assembly-simplified-spring-optimize/idempotent/src/main/java/top/osjf/optimize/idempotent/annotation/Idempotent.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package top.osjf.optimize.idempotent.annotation;
+
+import org.intellij.lang.annotations.Language;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * NOTE: This file has been copied and slightly modified from {com.healthy-chn.cloud}.
+ * <p>
+ * The annotation of information enrichment required for idempotency.
+ *
+ * <p>This annotation is used to indicate the idempotent information of a certain idempotent
+ * method, including the name, idempotent control time, reminder when idempotent validation
+ * fails, and idempotent active deletion settings to ensure idempotent activity.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.4
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Idempotent {
+
+    /**
+     * Idempotent classification markers to prevent repetition of idempotent markers
+     * {@link #unique()} can be obtained using constants or spring's el expressions.
+     *
+     * @return Idempotent classification markers.
+     */
+    @Language("SpEL")
+    String prefix() default "idem";
+
+    /**
+     * The unique identifier of idempotent methods serves as the sole basis for determining
+     * idempotency, which can accept fixed constants and the el expression pattern of Spring.
+     *
+     * @return Unique identifier for idempotent methods.
+     */
+    @Language("SpEL")
+    String unique() default "";
+
+    /**
+     * <p>
+     * The duration of idempotent control must be greater than the processing time of the business.
+     * </p>
+     * The value is the duration of the marking of the idempotent key. If it exceeds the marking time,
+     * the idempotent key can be used again.
+     *
+     * @return tag duration, default is 60 seconds.
+     */
+    long duration() default 60;
+
+    /**
+     * Idempotent control duration unit, default is {@link TimeUnit#SECONDS}.
+     *
+     * @return idempotent control duration unit.
+     */
+    TimeUnit timeUnit() default TimeUnit.SECONDS;
+
+    /**
+     * Indicate the information reminder given when idempotent parity check fails.
+     *
+     * @return Reminder information.
+     */
+    String message() default "Repeated request, please try again later";
+
+    /**
+     * The {@code boolean} flag that should the idempotent key be cleared immediately
+     * after the completion of the business
+     *
+     * @return {@code true}: Clear Now {@code false}: Not handled.
+     */
+    boolean removeKeyWhenFinished() default false;
+
+    /**
+     * The {@code boolean} flag that should the key of the exponent be cleared
+     * immediately when the business execution is abnormal.
+     *
+     * @return {@code true}: Clear Now {@code false}: Not handled.
+     */
+    boolean removeKeyWhenError() default false;
+}
