cfc6c252bc2dd7774c9250c768033c9838817e1f
zhangpengfei <929160069@qq.com>
2025-07-02 09:40:59 +0800

    Idempotent verification support framework.

---
+++ assembly-spring-boot-autoconfigure/src/main/java/top/osjf/spring/autoconfigure/cron/ClientType.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2024-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package top.osjf.spring.autoconfigure.cron;
+
+/**
+ * Supported cron client types (defined in order of precedence).
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 1.0.4
+ */
+public enum ClientType {
+
+    /**
+     * Native support for the Spring framework.
+     */
+    SPRING_SCHEDULER,
+
+    /**
+     * Hutool timed task client.
+     */
+    HUTOOL,
+
+    /**
+     * Quartz timed task client.
+     */
+    QUARTZ,
+
+    /**
+     * Cron4j timed task client.
+     */
+    CRON4J,
+
+    /**
+     * Simple timed task client.
+     */
+    SIMPLE,
+
+    /**
+     * No client.
+     */
+    NONE
+}
