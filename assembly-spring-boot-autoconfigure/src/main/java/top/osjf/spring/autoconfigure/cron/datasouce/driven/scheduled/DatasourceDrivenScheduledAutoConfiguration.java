07acd42becc87d4355457ad3942cb12b1fab1576
zhangpengfei <929160069@qq.com>
2025-08-08 11:52:59 +0800

    3.0.1

--- assembly-spring-boot-autoconfigure/src/main/java/top/osjf/spring/autoconfigure/cron/datasouce/driven/scheduled/DatasourceDrivenScheduledAutoConfiguration.java
+++ assembly-spring-boot-autoconfigure/src/main/java/top/osjf/spring/autoconfigure/cron/datasouce/driven/scheduled/DatasourceDrivenScheduledAutoConfiguration.java
@@ -15,7 +15,7 @@
  */
 
 
-package top.osjf.spring.autoconfigure.cron;
+package top.osjf.spring.autoconfigure.cron.datasouce.driven.scheduled;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -23,11 +23,16 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportSelector;
+import org.springframework.core.type.AnnotationMetadata;
+import top.osjf.cron.core.lang.NotNull;
 import top.osjf.cron.datasource.driven.scheduled.DatasourceTaskElementsOperation;
 import top.osjf.cron.datasource.driven.scheduled.NoOpDatasourceTaskElementsOperation;
 import top.osjf.cron.spring.annotation.DatabaseDrivenScheduledConfiguration;
+import top.osjf.cron.spring.datasource.driven.scheduled.DataSource;
 import top.osjf.cron.spring.datasource.driven.scheduled.SpringDatasourceDrivenScheduled;
 import top.osjf.spring.autoconfigure.ConditionalOnPropertyProfiles;
+import top.osjf.spring.autoconfigure.cron.ClientType;
 
 /**
  * {@link EnableAutoConfiguration Auto-configuration} for {@link SpringDatasourceDrivenScheduled}.
@@ -43,7 +48,7 @@ public class DatasourceDrivenScheduledAutoConfiguration {
             = "spring.schedule.cron.datasource.driven.active-profiles.matched";
 
     @Configuration(proxyBeanMethods = false)
-    @Import({DatabaseDrivenScheduledConfiguration.class, DatasourceTaskElementsOperationAutoConfiguration.class})
+    @Import({DatasourceDrivenConfigurationImportSelector.class, DatabaseDrivenScheduledConfiguration.class})
     @ConditionalOnPropertyProfiles(propertyName = PROPERTY_NAME_OF_MATCHED_PROFILES)
     public static class DatasourceDrivenScheduledSelectiveAutoConfiguration {
     }
@@ -53,4 +58,22 @@ public class DatasourceDrivenScheduledAutoConfiguration {
     public NoOpDatasourceTaskElementsOperation noOpDatasourceTaskElementsOperation() {
         return new NoOpDatasourceTaskElementsOperation();
     }
+
+    /**
+     * {@link ImportSelector} to add {@link ClientType} configuration classes.
+     * @since 1.0.4
+     */
+    public static class DatasourceDrivenConfigurationImportSelector implements ImportSelector {
+        @Override
+        @NotNull
+        public String[] selectImports(@NotNull AnnotationMetadata importingClassMetadata) {
+            DataSource[] types = DataSource.values();
+            String[] imports = new String[types.length];
+            for (int i = 0; i < types.length; i++) {
+                imports[i] = DatasourceDrivenAutoConfigurations.getConfigurationClass(types[i]);
+            }
+            return imports;
+        }
+    }
+
 }
