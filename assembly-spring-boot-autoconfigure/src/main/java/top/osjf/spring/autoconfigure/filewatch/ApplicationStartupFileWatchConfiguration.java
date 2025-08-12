e56fbe026b18be73c9fd286ac2f9eb635b0e4e86
zhangpengfei <929160069@qq.com>
2025-08-12 16:31:38 +0800

    3.0.1

---
+++ assembly-spring-boot-autoconfigure/src/main/java/top/osjf/spring/autoconfigure/filewatch/ApplicationStartupFileWatchConfiguration.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2025-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package top.osjf.spring.autoconfigure.filewatch;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import top.osjf.filewatch.application.startup.ApplicationStartupFileWatchListener;
+import top.osjf.filewatch.application.startup.StartupJarElement;
+
+import java.util.List;
+
+/**
+ * {@link Configuration Configuration} for self start monitoring of application packages.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 2025.08.12
+ */
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnClass({ApplicationStartupFileWatchListener.class, StartupJarElement.class})
+class ApplicationStartupFileWatchConfiguration {
+
+    @Bean
+    public FileWatchServiceCustomizer fileWatchServiceCustomizer(FileWatchProperties fileWatchProperties) {
+        return fileWatchService -> {
+            List<StartupJarElement> elements = fileWatchProperties.getApplicationStartup().getElements();
+            if (!elements.isEmpty()) {
+                fileWatchService.registerListener(new ApplicationStartupFileWatchListener(elements));
+                for (StartupJarElement jarElement : elements) {
+                    fileWatchService.registerWaitConfiguration(jarElement);
+                }
+            }
+        };
+    }
+}
