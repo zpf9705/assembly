36f3e68fee7b1d68f28e456f65e3c5c27f7b2e93
zhangpengfei <929160069@qq.com>
2025-08-12 14:26:04 +0800

    3.0.1

--- interest-extension/file-watch-application-startup/src/main/java/top/osjf/filewatch/application/startup/StartupJarElement.java
+++ interest-extension/file-watch-application-startup/src/main/java/top/osjf/filewatch/application/startup/StartupJarElement.java
@@ -17,7 +17,7 @@
 
 package top.osjf.filewatch.application.startup;
 
-import top.osjf.filewatch.BindWaitConfiguration;
+import top.osjf.filewatch.BindingConfiguration;
 import top.osjf.filewatch.FileWatchPath;
 import top.osjf.filewatch.TriggerKind;
 
@@ -34,7 +34,7 @@ import java.util.concurrent.TimeUnit;
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 3.0.1
  */
-public class StartupJarElement extends BindWaitConfiguration {
+public class StartupJarElement extends BindingConfiguration {
 
     private static final long serialVersionUID = 8028606232869488345L;
 
@@ -107,5 +107,10 @@ public class StartupJarElement extends BindWaitConfiguration {
 
     public void setUnit(TimeUnit unit) {
         this.unit = unit;
+    }
+
+    @Override
+    public String getPathContext() {
+        return jarFileName;
     }
 }
