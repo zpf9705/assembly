93b83db46d8b4fda1c32885cfd042a10f5d82b60
zhangpengfei <929160069@qq.com>
2025-08-18 14:36:45 +0800

    3.0.1 - rename and permission assignment of cron

--- interest-extension/file-watch/src/main/java/top/osjf/filewatch/AmpleFileWatchListener.java
+++ interest-extension/file-watch/src/main/java/top/osjf/filewatch/AmpleFileWatchListener.java
@@ -17,59 +17,17 @@
 
 package top.osjf.filewatch;
 
-import java.nio.file.Path;
 import java.nio.file.WatchEvent;
 
 /**
  * Abstract class of {@link FileWatchListener} that {@link #supports(WatchEvent)}
- * and {@link #onWatchEvent(WatchEvent)} method param must be {@link AmapleWatchEvent}
+ * and {@link #onWatchEvent(WatchEvent)} method param be {@link AmapleWatchEvent}
  * to execute internal methods.
  *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 3.0.1
+ *
+ * @see AmapleWatchEvent
  */
-public abstract class AmpleFileWatchListener implements FileWatchListener {
-    /**
-     * {@inheritDoc}
-     * Be {@link AmapleWatchEvent} to execute {@link #supportsInternal(AmapleWatchEvent)}.
-     */
-    @Override
-    public final boolean supports(WatchEvent<Path> event) {
-        if (instanceofAmapleWatchEvent(event)) {
-            return supportsInternal((AmapleWatchEvent) event);
-        }
-        return false;
-    }
-    /**
-     * {@inheritDoc}
-     * Be {@link AmapleWatchEvent} to execute {@link #onWatchEventInternal(AmapleWatchEvent)}.
-     */
-    @Override
-    public final void onWatchEvent(WatchEvent<Path> event) {
-        if (instanceofAmapleWatchEvent(event)) {
-            onWatchEventInternal((AmapleWatchEvent) event);
-        }
-    }
-
-    /**
-     * Is instanceof {@link AmapleWatchEvent}?
-     * @param event the given {@link WatchEvent}.
-     * @return {@code true} instanceof {@link AmapleWatchEvent},{@code false} otherwise.
-     */
-    private boolean instanceofAmapleWatchEvent(WatchEvent<Path> event) {
-        return event instanceof AmapleWatchEvent;
-    }
-
-    /**
-     * Internal method of {@link #supports(WatchEvent)}.
-     * @param event the converted {@link AmapleWatchEvent}.
-     * @return {@link #supports(WatchEvent)}
-     */
-    protected abstract boolean supportsInternal(AmapleWatchEvent event);
-
-    /**
-     * Internal method of {@link #onWatchEvent(WatchEvent)}.
-     * @param event the converted {@link AmapleWatchEvent}.
-     */
-    protected abstract void onWatchEventInternal(AmapleWatchEvent event);
+public abstract class AmpleFileWatchListener implements FileWatchListener<AmapleWatchEvent> {
 }
