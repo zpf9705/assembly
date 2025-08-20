3fb9f1bf5b9bc726f5978d0ec2b0c66f4e62e982
zhangpengfei <929160069@qq.com>
2025-08-20 13:34:47 +0800

    3.0.1 - rename and permission assignment of cron

---
+++ assembly-spring-boot-autoconfigure/src/main/java/top/osjf/spring/autoconfigure/filewatch/config/refresh/ConfigRefreshedUtils.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2025-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package top.osjf.spring.autoconfigure.filewatch.config.refresh;
+
+import org.springframework.boot.env.PropertiesPropertySourceLoader;
+import org.springframework.boot.env.PropertySourceLoader;
+import org.springframework.boot.env.YamlPropertySourceLoader;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Config refreshed utils.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 3.0.1
+ *
+ * @see PropertySourceLoader
+ * @see org.springframework.boot.env.YamlPropertySourceLoader
+ * @see org.springframework.boot.env.PropertiesPropertySourceLoader
+ */
+public final class ConfigRefreshedUtils {
+
+    /** Yaml config resolver {@code PropertySourceLoader}.*/
+    private static final PropertySourceLoader YAML_SOURCE_LOADER = new YamlPropertySourceLoader();
+
+    /** Properties config resolver {@code PropertySourceLoader}.*/
+    private static final PropertySourceLoader PROPERTIES_SOURCE_LOADER = new PropertiesPropertySourceLoader();
+
+    /**
+     * Return the {@link PropertySourceLoader} static instance that is suitable for parsing based on
+     * the file name type (determined by the suffix).
+     * @param fileName the given {@code fileName}.
+     * @return Specify the file type to support parsing of {@link PropertySourceLoader} static instances.
+     * @see #isYamlFile(String)
+     * @see #isPropertiesFile(String)
+     */
+    public static PropertySourceLoader getPropertySourceLoader(String fileName) {
+        if (isYamlFile(fileName)) {
+            return YAML_SOURCE_LOADER;
+        }
+        else if (isPropertiesFile(fileName)) {
+            return PROPERTIES_SOURCE_LOADER;
+        }
+        throw new UnsupportedOperationException("Unsupported file type");
+    }
+
+    /**
+     * Check is supported config file.
+     * @param fileName the given {@code fileName}.
+     * @return {@code true} is supported config file,{@code false} otherwise.
+     */
+    public static boolean isConfigFile(String fileName) {
+        return isYamlFile(fileName) || isPropertiesFile(fileName);
+    }
+
+    /**
+     * Check is yaml file.
+     * @param fileName the given {@code fileName}.
+     * @return {@code true} is yaml file,{@code false} otherwise.
+     */
+    public static boolean isYamlFile(String fileName) {
+        requireNonNull(fileName, "fileName");
+        return fileName.endsWith(".yml") || fileName.endsWith(".yaml");
+    }
+
+    /**
+     * Check is properties file.
+     * @param fileName the given {@code fileName}.
+     * @return {@code true} is properties file,{@code false} otherwise.
+     */
+    public static boolean isPropertiesFile(String fileName) {
+        requireNonNull(fileName, "fileName");
+        return fileName.endsWith(".properties");
+    }
+}
