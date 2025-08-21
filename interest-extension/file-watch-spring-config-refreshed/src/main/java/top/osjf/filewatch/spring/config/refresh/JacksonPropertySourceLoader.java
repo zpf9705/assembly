3d2c321a021f65acc4dcaac1fc0a23448d0bf79b
zhangpengfei <929160069@qq.com>
2025-08-21 09:37:04 +0800

    3.0.1 - rename and permission assignment of cron

---
+++ interest-extension/file-watch-spring-config-refreshed/src/main/java/top/osjf/filewatch/spring/config/refresh/JacksonPropertySourceLoader.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2025-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package top.osjf.filewatch.spring.config.refresh;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.env.PropertySourceLoader;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.ClassUtils;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Strategy to load '.json' files into a {@link PropertySource}.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 3.0.1
+ */
+public class JacksonPropertySourceLoader implements PropertySourceLoader {
+
+    private static final String[] FILE_EXTENSIONS = new String[]{"json"};
+
+    private final ObjectMapper objectMapper;
+
+    /**
+     * Constructs an empty {@code JacksonPropertySourceLoader} to give a default {@link ObjectMapper}.
+     */
+    public JacksonPropertySourceLoader() {
+        this(new ObjectMapper());
+    }
+
+    /**
+     * Constructs a {@code JacksonPropertySourceLoader} with the given {@link ObjectMapper}.
+     * @param objectMapper the given {@code ObjectMapper}.
+     */
+    public JacksonPropertySourceLoader(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public String[] getFileExtensions() {
+        return FILE_EXTENSIONS;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<PropertySource<?>> load(String name, Resource resource) throws IOException {
+        if (!ClassUtils.isPresent("com.fasterxml.jackson.databind.ObjectMapper", getClass().getClassLoader())) {
+            throw new IllegalStateException(
+                    "Attempted to load " + name + " but jackson.ObjectMapper was not found on the classpath");
+        }
+        if (!resource.exists()) {
+            return Collections.emptyList();
+        }
+        Map<String, Object> source;
+        try {
+            source = objectMapper.readValue(resource.getInputStream(), Map.class);
+        }
+        catch (JsonParseException | JsonMappingException ex) {
+            throw new IOException(ex.getMessage(), ex);
+        }
+        return Collections.singletonList(new MapPropertySource(name, source));
+    }
+}
