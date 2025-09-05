f819ce9b5db7329bfe7d022d2f4e4fc44bf520f8
zhangpengfei <929160069@qq.com>
2025-09-05 13:54:23 +0800

    3.0.1 - rename and permission assignment of cron

--- assembly-simplified-spring-optimize/spring-aware/src/main/java/top/osjf/optimize/aware/BeanAware.java
+++ assembly-simplified-spring-optimize/spring-aware/src/main/java/top/osjf/optimize/aware/BeanAware.java
@@ -28,9 +28,67 @@ import org.springframework.beans.factory.Aware;
  * implemented by components that need to be aware of and obtain references to specific
  * bean instances during initialization phase.
  *
+ * <p><strong>Important Restrictions:</strong>
+ * <ul>
+ *   <li><b>Container infrastructure types (ApplicationContext/BeanFactory) are NOT supported</b></li>
+ *   <li>For container access, implement {@link org.springframework.context.ApplicationContextAware}
+ *       or {@link org.springframework.beans.factory.BeanFactoryAware} instead</li>
+ *   <li>Generic type {@code T} must represent an application-managed bean</li>
+ * </ul>
+ *
+ * <h2>Usage Examples</h2>
+ *
+ * <h3>Basic Implementation</h3>
+ * <pre class="code">
+ * // 1. Define application bean
+ * public class PaymentService {
+ *     public void processPayment() {
+ *         // business logic
+ *     }
+ * }
+ *
+ * // 2. Implement BeanAware correctly
+ * public class PaymentProcessor implements BeanAware&lt;PaymentService&gt; {
+ *     private PaymentService service;
+ *
+ *     {@literal @}Override
+ *     public void setBean(PaymentService service) {
+ *         this.service = service;
+ *     }
+ *
+ *     public void executePayment() {
+ *         service.processPayment();
+ *     }
+ * }</pre>
+ *
+ * <h3>Spring Configuration</h3>
+ * <pre class="code">
+ * {@literal @}Configuration
+ * public class AppConfig {
+ *     {@literal @}Bean
+ *     public PaymentService paymentService() {
+ *         return new PaymentService();
+ *     }
+ *
+ *     {@literal @}Bean
+ *     public PaymentProcessor paymentProcessor() {
+ *         return new PaymentProcessor();
+ *     }
+ * }</pre>
+ *
+ * <h3>Forbidden Case</h3>
+ * <p><span style="color:red">Warning:</span> This will cause BeanCreationException
+ * <pre class="code">
+ * // ❌ Violates restriction: Using container interface
+ * public class InvalidImpl implements BeanAware&lt;ApplicationContext&gt; {
+ *     public void setBean(ApplicationContext ctx) { ... }
+ * }</pre>
+ *
  * @param <T> the type of bean that the implementing class should be aware of.
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 3.0.1
+ * @see org.springframework.context.ApplicationContextAware
+ * @see org.springframework.beans.factory.BeanFactoryAware
  */
 public interface BeanAware<T> extends Aware {
 
