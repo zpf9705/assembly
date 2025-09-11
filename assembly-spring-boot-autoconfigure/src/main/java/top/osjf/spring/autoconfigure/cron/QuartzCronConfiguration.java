504b90a3e91b53e058cfc304a0fdff5957662d38
zhangpengfei <929160069@qq.com>
2025-09-11 10:53:48 +0800

    3.0.1 - rename and permission assignment of cron

--- assembly-spring-boot-autoconfigure/src/main/java/top/osjf/spring/autoconfigure/cron/QuartzCronConfiguration.java
+++ assembly-spring-boot-autoconfigure/src/main/java/top/osjf/spring/autoconfigure/cron/QuartzCronConfiguration.java
@@ -16,6 +16,7 @@
 
 package top.osjf.spring.autoconfigure.cron;
 
+import org.quartz.Scheduler;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -44,6 +45,7 @@ import java.util.List;
 class QuartzCronConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean(Scheduler.class)
     public SuperiorProperties quartzProperties(ObjectProvider<List<QuartzPropertiesCustomizer>> provider,
                                                CronProperties cronProperties) {
         SuperiorProperties properties = cronProperties.getQuartz().get();
