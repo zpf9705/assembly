cc9240877f4057ea71dd9e216e823a0314a707e6
zhangpengfei <929160069@qq.com>
2025-09-29 09:49:23 +0800

    3.0.2

---
+++ assembly-simplified-cron/cron-core/src/main/java/top/osjf/cron/core/repository/RunTimes.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2025-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package top.osjf.cron.core.repository;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+
+/**
+ * This annotation {@code RunTimes} is used to indicate the number of times the
+ * scheduled task of the annotated method should be executed at runtime, which
+ * can be defined by setting the value attribute, with a default of 1 execution.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 3.0.2
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(METHOD)
+public @interface RunTimes {
+    /**
+     * Specifies the number of times the method should be executed. Default is 1.
+     * Users can customize this value, e.g., {@code @RunTimes(3)} means the method should run 3 times.
+     * @return Execution Times.
+     */
+    int value() default 1;
+}
