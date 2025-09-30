a04356b856f67d65ef0445e634e8e8e7887743f0
zhangpengfei <929160069@qq.com>
2025-09-30 11:13:56 +0800

    3.0.2

---
+++ assembly-simplified-cron/cron-core/src/main/java/top/osjf/cron/core/repository/ExecuteTimeoutThreadPoolExecutor.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2025-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package top.osjf.cron.core.repository;
+
+import top.osjf.cron.core.lang.NotNull;
+import top.osjf.cron.core.lifecycle.SuperiorProperties;
+
+import java.util.concurrent.Future;
+
+/**
+ * A thread pool executor extended from {@link SuperiorProperties ParsedThreadPoolExecutor},
+ * used to support timeout monitoring for specific tasks (tasks that implement the
+ * {@link TimeoutMonitoringRunnable} interface).
+ * When the submitted task is of type {@link TimeoutMonitoringRunnable}, the executor wraps
+ * it as a task that can monitor execution time and controls its timeout through the internal
+ * {@link Future} mechanism. If the task execution time exceeds the preset threshold, the
+ * corresponding monitoring behavior can be triggered according to the configuration.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 3.0.2
+ */
+public class ExecuteTimeoutThreadPoolExecutor extends SuperiorPropertiesParsedThreadPoolExecutor {
+
+    /**
+     * Creates a new {@code ExecuteTimeoutThreadPoolExecutor} with the given initial
+     * parameter.
+     * @param properties the initial {@link SuperiorProperties}.
+     */
+    public ExecuteTimeoutThreadPoolExecutor(SuperiorProperties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void execute(@NotNull Runnable command) {
+
+        if (command instanceof TimeoutMonitoringRunnable) {
+            TimeoutMonitoringRunnable tr = (TimeoutMonitoringRunnable) command;
+            Future<?> future = submit(tr.getReal());
+            tr.get(future);
+        } else {
+            super.execute(command);
+        }
+    }
+}
