91d0d9f687f48ff2cc198587359839125ef9fcad
zhangpengfei <929160069@qq.com>
2025-10-10 14:01:43 +0800

    3.0.2

---
+++ assembly-simplified-cron/cron-quartz/src/main/java/top/osjf/cron/quartz/repository/JobConstants.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2025-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package top.osjf.cron.quartz.repository;
+
+import org.quartz.JobDetail;
+import org.quartz.utils.Key;
+
+/**
+ * About {@link org.quartz.Job} help constants.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 3.0.2
+ */
+public interface JobConstants {
+
+    /**
+     * The property name of {@link Runnable} in {@link JobDetail#getJobDataMap()}.
+     */
+    String RUNNABLE_PROPERTY = "RUNNABLE_PROPERTY";
+
+    /**
+     * The property name of job id in {@link JobDetail#getJobDataMap()}.
+     */
+    String ID_PROPERTY = "ID_PROPERTY";
+
+    /**
+     * The default group for scheduling entities, with the value "DEFAULT".
+     */
+    String DEFAULT_GROUP = Key.DEFAULT_GROUP;
+}
