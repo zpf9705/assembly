bf4829887fcc16cc888c4749025798dd1149645d
zhangpengfei <929160069@qq.com>
2025-10-10 14:48:33 +0800

    3.0.2

--- assembly-simplified-cron/cron-quartz/src/main/java/top/osjf/cron/quartz/repository/MethodProviderRunnable.java
+++ assembly-simplified-cron/cron-quartz/src/main/java/top/osjf/cron/quartz/repository/MethodProviderRunnable.java
@@ -25,6 +25,7 @@ import java.lang.reflect.Method;
  *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 1.0.3
+ * @deprecated 3.0.2
  */
 @Deprecated
 public interface MethodProviderRunnable extends Runnable {
