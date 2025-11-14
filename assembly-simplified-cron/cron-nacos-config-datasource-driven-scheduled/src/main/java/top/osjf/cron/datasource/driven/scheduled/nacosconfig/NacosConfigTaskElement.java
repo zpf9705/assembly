559cf4f7cd517447df6c7fda0c3a2a77cacdb356
zhangpengfei <929160069@qq.com>
2025-11-14 10:35:23 +0800

    3.0.2

---
+++ assembly-simplified-cron/cron-nacos-config-datasource-driven-scheduled/src/main/java/top/osjf/cron/datasource/driven/scheduled/nacosconfig/NacosConfigTaskElement.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2025-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package top.osjf.cron.datasource.driven.scheduled.nacosconfig;
+
+import top.osjf.cron.datasource.driven.scheduled.DefaultTaskElement;
+
+/**
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 3.0.2
+ */
+public class NacosConfigTaskElement extends DefaultTaskElement {
+
+    private static final long serialVersionUID = 2576911210334659473L;
+}
