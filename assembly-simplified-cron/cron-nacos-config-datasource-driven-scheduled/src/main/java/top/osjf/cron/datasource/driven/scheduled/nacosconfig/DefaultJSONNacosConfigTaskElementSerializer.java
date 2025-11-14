98db0555bb9348b03835268755a57bfc7f83c711
zhangpengfei <929160069@qq.com>
2025-11-14 09:40:43 +0800

    3.0.2

---
+++ assembly-simplified-cron/cron-nacos-config-datasource-driven-scheduled/src/main/java/top/osjf/cron/datasource/driven/scheduled/nacosconfig/DefaultJSONNacosConfigTaskElementSerializer.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2025-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package top.osjf.cron.datasource.driven.scheduled.nacosconfig;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 3.0.2
+ */
+public class DefaultJSONNacosConfigTaskElementSerializer implements NacosConfigTaskElementSerializer {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String serialize(List<NacosConfigTaskElement> elements) throws IOException {
+        return objectMapper.writeValueAsString(elements);
+    }
+
+    @Override
+    public List<NacosConfigTaskElement> deserialize(String configInfo) throws IOException {
+        return objectMapper.readValue(configInfo, new TypeReference<List<NacosConfigTaskElement>>() { });
+    }
+
+    @Override
+    public ConfigFormat getConfigFormat() {
+        return ConfigFormat.JSON;
+    }
+}
