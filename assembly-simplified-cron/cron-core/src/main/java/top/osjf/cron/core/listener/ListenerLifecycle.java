d94ba74dbd212d619b62190303dc6eafc47d203f
zhangpengfei <929160069@qq.com>
2026-06-17 11:03:19 +0800

    3.0.2

--- assembly-simplified-cron/cron-core/src/main/java/top/osjf/cron/core/listener/ListenerLifecycle.java
+++ assembly-simplified-cron/cron-core/src/main/java/top/osjf/cron/core/listener/ListenerLifecycle.java
@@ -79,7 +79,8 @@ public enum ListenerLifecycle {
         if (listenerContext != null) {
             try {
                 for (CronListener cronListener : collector.getCronListeners()) {
-                    consumer.accept(cronListener, listenerContext, e);
+                    doListenerCallback(cronListener,
+                            () -> consumer.accept(cronListener, listenerContext, e));
                 }
             }
             finally {
@@ -87,6 +88,29 @@ public enum ListenerLifecycle {
                     CONTEXT_LOCAL.remove();
                 }
             }
+        }
+    }
+
+    /**
+     * Implement the callback logic of the scheduled task listener, automatically distinguishing
+     * between synchronous and asynchronous execution modes
+     *
+     * <p>If the current listener is an asynchronous type listener of {@link AsyncCronListener},
+     * the callback will be executed asynchronously using the thread pool provided by the listener
+     * itself;
+     * The ordinary {@link CronListener} listener directly executes the callback logic synchronously
+     * in the current scheduling thread.
+     * @param listener          the instance of scheduled task listener.
+     * @param listenerCallback  the listener callback task to be executed includes callback methods
+     *                          enclosed in {@link CronListener},such as {@link CronListener#start}.
+     * @since 3.0.2
+     */
+    public static void doListenerCallback(CronListener listener, Runnable listenerCallback) {
+        if (listener instanceof AsyncCronListener) {
+            ((AsyncCronListener) listener).get().execute(listenerCallback);
+        }
+        else {
+            listenerCallback.run();
         }
     }
 }
