74d12d3c848e86864fa5b6f9afbb7392bc9929ab
zhangpengfei <929160069@qq.com>
2026-06-22 13:32:06 +0800

    commons

--- assembly-simplified-cron/cron-spring-quartz/src/main/java/top/osjf/cron/spring/quartz/SpringRunnableJobFactory.java
+++ assembly-simplified-cron/cron-spring-quartz/src/main/java/top/osjf/cron/spring/quartz/SpringRunnableJobFactory.java
@@ -28,7 +28,7 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import top.osjf.cron.core.lang.NotNull;
+import top.osjf.commons.lang.NotNull;
 import top.osjf.cron.quartz.repository.RunnableJob;
 import top.osjf.cron.quartz.repository.RunnableJobFactory;
 
