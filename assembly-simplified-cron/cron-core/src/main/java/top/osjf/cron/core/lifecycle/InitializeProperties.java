bf474916427ff49ebe041372987fbba415712463
zhangpengfei <929160069@qq.com>
2026-06-22 09:27:37 +0800

    commons

--- assembly-simplified-cron/cron-core/src/main/java/top/osjf/cron/core/lifecycle/InitializeProperties.java
+++ assembly-simplified-cron/cron-core/src/main/java/top/osjf/cron/core/lifecycle/InitializeProperties.java
@@ -17,7 +17,7 @@
 
 package top.osjf.cron.core.lifecycle;
 
-import top.osjf.cron.core.lang.Nullable;
+import top.osjf.commons.lang.Nullable;
 
 import java.io.InputStream;
 import java.util.Locale;
--- assembly-simplified-cron/cron-core/src/main/java/top/osjf/cron/core/lifecycle/PropertiesInitializeAble.java
+++ assembly-simplified-cron/cron-core/src/main/java/top/osjf/cron/core/lifecycle/PropertiesInitializeAble.java
@@ -19,7 +19,7 @@ package top.osjf.cron.core.lifecycle;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import top.osjf.cron.core.lang.Nullable;
+import top.osjf.commons.lang.Nullable;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
