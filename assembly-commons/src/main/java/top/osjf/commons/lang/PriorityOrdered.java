797b9211b6c136b96d7e6409223684ae5c4ffa3c
zhangpengfei <929160069@qq.com>
2026-06-23 17:27:55 +0800

    commons

---
+++ assembly-commons/src/main/java/top/osjf/commons/lang/PriorityOrdered.java
@@ -0,0 +1,31 @@
+package top.osjf.commons.lang;
+
+/**
+ * This class was copied from {@code org.springframework.core}, with minor modifications
+ * and adaptations. I would like to express my sincere gratitude here!
+ *
+ * Extension of the {@link Ordered} interface, expressing a <em>priority</em>
+ * ordering: {@code PriorityOrdered} objects are always applied before
+ * <em>plain</em> {@link Ordered} objects regardless of their order values.
+ *
+ * <p>When sorting a set of {@code Ordered} objects, {@code PriorityOrdered}
+ * objects and <em>plain</em> {@code Ordered} objects are effectively treated as
+ * two separate subsets, with the set of {@code PriorityOrdered} objects preceding
+ * the set of <em>plain</em> {@code Ordered} objects and with relative
+ * ordering applied within those subsets.
+ *
+ * <p>This is primarily a special-purpose interface, used within the framework
+ * itself for objects where it is particularly important to recognize
+ * <em>prioritized</em> objects first, potentially without even obtaining the
+ * remaining objects.
+ *
+ * <p>Note: {@code PriorityOrdered} post-processor beans are initialized in
+ * a special phase, ahead of other post-processor beans. This subtly
+ * affects their autowiring behavior: they will only be autowired against
+ * beans which do not require eager initialization for type matching.
+ *
+ * @see OrderComparator
+ * @see Ordered
+ */
+public interface PriorityOrdered extends Ordered {
+}
