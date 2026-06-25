8af0621aa0546765cc0f77fddbd0f91271536bac
zhangpengfei <929160069@qq.com>
2026-06-25 11:20:41 +0800

    commons

---
+++ assembly-simplified-cron/cron-cron4j/src/main/java/top/osjf/cron/cron4j/repository/ControllableRunnableTask.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2026-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package top.osjf.cron.cron4j.repository;
+
+import it.sauronsoftware.cron4j.InvalidPatternException;
+import it.sauronsoftware.cron4j.Task;
+import it.sauronsoftware.cron4j.TaskExecutionContext;
+
+/**
+ * {@code Runnable} type controllable scheduled task wrapper implementation class.
+ * <p>
+ * Based on encapsulating native {@link Runnable} into a framework-unified {@link Task}
+ * task object, some functions (such as {@link #canBeStopped()}, {@link #canBePaused()}
+ * and so on) will be overridden to achieve controllable effects on the task's running
+ * process or other aspects.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 3.0.2
+ */
+class ControllableRunnableTask extends Task {
+
+    /**
+     * The wrapped runnable object.
+     */
+    private final Runnable runnable;
+
+    /**
+     * Builds the task.
+     *
+     * @param runnable
+     *            The wrapped Runnable object.
+     * @throws InvalidPatternException
+     *             If the supplied pattern is not valid.
+     */
+    public ControllableRunnableTask(Runnable runnable) throws InvalidPatternException {
+        this.runnable = runnable;
+    }
+
+    /**
+     * Returns the wrapped Runnable object.
+     *
+     * @return The wrapped Runnable object.
+     */
+    public Runnable getRunnable() {
+        return runnable;
+    }
+
+    /**
+     * @return Returning {@code true} indicates that this task can be interrupted during runtime.
+     */
+    @Override
+    public boolean canBeStopped() {
+        return true;
+    }
+
+    /**
+     * Implements {@link Task#execute(TaskExecutionContext)}, launching the
+     * {@link Runnable#run()} method on the wrapped object.
+     */
+    public void execute(TaskExecutionContext context) {
+        runnable.run();
+    }
+
+    /**
+     * Overrides {@link Object#toString()}.
+     */
+    public String toString() {
+        StringBuffer b = new StringBuffer();
+        b.append("Task[");
+        b.append("runnable=");
+        b.append(runnable);
+        b.append("]");
+        return b.toString();
+    }
+}
