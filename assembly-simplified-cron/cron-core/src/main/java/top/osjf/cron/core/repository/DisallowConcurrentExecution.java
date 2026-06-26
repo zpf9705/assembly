898d470979be0ce00148c451e046dd02926a1117
zhangpengfei <929160069@qq.com>
2026-06-26 09:21:24 +0800

    commons

---
+++ assembly-simplified-cron/cron-core/src/main/java/top/osjf/cron/core/repository/DisallowConcurrentExecution.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2026-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package top.osjf.cron.core.repository;
+
+import java.lang.annotation.*;
+
+/**
+ * Annotations that do not allow concurrent execution of tasks.
+ *
+ * <p>Used in the method of timed tasks, it marks that the current timed task does
+ * not allow concurrent execution. When the previous task is not completed, the next
+ * triggered task will skip execution directly, avoiding problems such as data confusion,
+ * resource competition, and duplicate business processing caused by multi-threaded
+ * concurrent execution of the same task.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 3.0.2
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface DisallowConcurrentExecution {
+
+}
