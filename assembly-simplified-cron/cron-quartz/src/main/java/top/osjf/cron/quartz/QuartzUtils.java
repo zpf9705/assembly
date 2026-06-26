0455dec3b3ddbfab6abea3d89c87e5c1e92a7036
zhangpengfei <929160069@qq.com>
2026-06-26 16:38:19 +0800

    commons

--- assembly-simplified-cron/cron-quartz/src/main/java/top/osjf/cron/quartz/QuartzUtils.java
+++ assembly-simplified-cron/cron-quartz/src/main/java/top/osjf/cron/quartz/QuartzUtils.java
@@ -17,17 +17,11 @@
 
 package top.osjf.cron.quartz;
 
-import org.quartz.Job;
 import org.quartz.JobKey;
 import org.quartz.Trigger;
 import org.quartz.impl.triggers.CronTriggerImpl;
 import top.osjf.commons.lang.NotNull;
-import top.osjf.commons.util.Assert;
 import top.osjf.cron.core.util.GsonUtils;
-import top.osjf.cron.quartz.repository.QuartzCronTaskRepository;
-
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Coordinate Quartz's easy-to-use tool class and provide relevant
@@ -38,35 +32,6 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public abstract class QuartzUtils {
 
-    @Deprecated
-    private static final Map<String, JobKey> JOB_KEY_MAP = new ConcurrentHashMap<>(16);
-
-    /**
-     * Returns a unique identity string formatted according to {@link JobKey}.
-     *
-     * @param jobKey the input resolve {@link JobKey}.
-     * @return Tag {@link Job} as a unique identity string.
-     * @throws NullPointerException if input {@code JobKey} is {@literal null}.
-     */
-    @Deprecated
-    public static String getJobIdentity(JobKey jobKey) {
-        String id = jobKey.getName() + "@" + jobKey.getGroup();
-        JOB_KEY_MAP.putIfAbsent(id, jobKey);
-        return id;
-    }
-
-    /**
-     * Returns a {@link JobKey} string formatted according to input id.
-     *
-     * @param id the input id.
-     * @return the cache {@link JobKey}.
-     * @throws NullPointerException if input {@code id} is {@literal null}.
-     */
-    @Deprecated
-    public static JobKey getJobKey(String id) {
-        return JOB_KEY_MAP.getOrDefault(id, null);
-    }
-
     /**
      * Calls {@link JobKey#toString()} as id.
      * @param jobKey the input {@link JobKey}
@@ -74,22 +39,6 @@ public abstract class QuartzUtils {
      */
     public static String jobKeyAsId(@NotNull JobKey jobKey) {
         return jobKey.toString();
-    }
-
-    /**
-     * Resolves input id as {@link JobKey}.
-     * @param id the input id.
-     * @return {@link JobKey} of resolve result.
-     * @see #jobKeyAsId(JobKey)
-     */
-    public static JobKey resolveIdAsJobKey(@NotNull String id) {
-        /**
-         * {@link QuartzCronTaskRepository#doRegisterInternal(String, JobKeyWrapperdRunnable)}
-         * {@link JobKey#toString() getGroup() + '.' + getName()}
-         */
-        String[] idArray = id.split("\\.");
-        Assert.isTrue(idArray.length == 2, "Illegal ID");
-        return new JobKey(idArray[1], idArray[0]);
     }
 
     /**
