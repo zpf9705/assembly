dd660954c1a63104ee32ec5762727884d4e5f3a1
zhangpengfei <929160069@qq.com>
2026-06-29 17:36:51 +0800

    commons

---
+++ assembly-spring-boot-autoconfigure/src/main/java/top/osjf/spring/autoconfigure/cron/CronMicrometerConfiguration.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2026-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package top.osjf.spring.autoconfigure.cron;
+
+import io.micrometer.core.aop.CountedAspect;
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import top.osjf.cron.core.micrometer.RepositoryGaugeIndicatorRegistrant;
+import top.osjf.cron.core.micrometer.RepositoryTagsBasedOnJoinPointFunction;
+import top.osjf.cron.core.repository.CronTaskRepository;
+
+/**
+ * {@link Configuration Configuration} for cron task micrometer monitoring capability.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 3.0.2
+ */
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnClass({MeterRegistry.class, CountedAspect.class, TimedAspect.class})
+@ConditionalOnBean({ MeterRegistry.class })
+class CronMicrometerConfiguration {
+
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> cronTaskMeterRegistryCustomizer(CronTaskRepository cronTaskRepository)
+    {
+        return registry -> new RepositoryGaugeIndicatorRegistrant(registry, cronTaskRepository).doRegister();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public RepositoryTagsBasedOnJoinPointFunction repositoryTagsBasedOnJoinPointFunction() {
+        return new RepositoryTagsBasedOnJoinPointFunction();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public CountedAspect countedAspect(MeterRegistry meterRegistry,
+                                       RepositoryTagsBasedOnJoinPointFunction repositoryTagsBasedOnJoinPointFunction) {
+        return new CountedAspect(meterRegistry, repositoryTagsBasedOnJoinPointFunction);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public TimedAspect timedAspect(MeterRegistry meterRegistry,
+                                   RepositoryTagsBasedOnJoinPointFunction repositoryTagsBasedOnJoinPointFunction) {
+        return new TimedAspect(meterRegistry, repositoryTagsBasedOnJoinPointFunction);
+    }
+}
