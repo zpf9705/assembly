6953926dbb5eced8266ecf54e44c1adc80ce8fe1
zhangpengfei <929160069@qq.com>
2026-06-30 17:37:56 +0800

    commons

---
+++ assembly-simplified-cron/cron-core/src/main/java/top/osjf/cron/core/micrometer/SystemPropertiesTagUtils.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2026-? the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package top.osjf.cron.core.micrometer;
+
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Tags;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The system attribute monitoring tag utility class is used to uniformly parse
+ * the built-in environment placeholders in the system.
+ *
+ * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
+ * @since 3.0.2
+ */
+public abstract class SystemPropertiesTagUtils {
+
+    /** System tag template constant: Key is fixed tag name, Value is environment variable placeholder expression */
+    private static final Tags systemTags
+            = Tags.of(
+            "os.name", "${os.name}", "java.version", "${java.version}", "hostname", "${HOSTNAME:local}"
+    );
+
+    /**
+     * Merge resolved global system tags into source business tags and return the combined tags
+     *
+     * @param source               the original business custom tags.
+     * @param expressionResolver   the placeholder expression resolver.
+     * @return the combined tags of business tags and system tags.
+     */
+    public static Tags mergResolvedSystemTags(Tags source, ExpressionResolver expressionResolver) {
+        return source.and(getResolvedSystemTags(expressionResolver));
+    }
+
+    /**
+     * Resolve placeholder expressions and return system tags with real parsed environment values
+     *
+     * @param expressionResolver the placeholder expression resolver.
+     * @return the resolved system dimension tags.
+     */
+    public static Tags getResolvedSystemTags(ExpressionResolver expressionResolver) {
+
+        List<Tag> tags = new ArrayList<>();
+
+        for (Tag systemTag : systemTags) {
+            String resolvedValue = expressionResolver.resolveExpression(systemTag.getValue());
+            tags.add(Tag.of(systemTag.getKey(), resolvedValue));
+        }
+
+        return Tags.of(tags);
+    }
+}
