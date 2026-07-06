743078e72d2dd88d0ca4b47b9f4b13e21afb21f9
zhangpengfei <929160069@qq.com>
2026-07-06 14:50:19 +0800

    commons

--- assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/datasource/driven/scheduled/ScheduledDrivenPropertyKey.java
+++ assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/datasource/driven/scheduled/ScheduledDrivenPropertyKey.java
@@ -20,38 +20,85 @@ package top.osjf.cron.spring.datasource.driven.scheduled;
 import top.osjf.cron.spring.CronTaskPropertyKey;
 
 /**
+ * Constant keys for scheduled driven configuration properties
+ *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 3.0.2
  */
 public interface ScheduledDrivenPropertyKey {
 
+    /**
+     * Root prefix of all scheduled driven configurations
+     */
     String PREFIX = CronTaskPropertyKey.PREFIX + ".scheduled-driven";
 
+    /**
+     * Prefix for external file data source configurations
+     */
     String PREFIX_EXTERNAL = PREFIX + ".external";
 
+    /**
+     * Base directory path of external configuration files
+     */
     String KEY_BASE_DIR = PREFIX_EXTERNAL + ".base-dir";
 
+    /**
+     * Filename of external configuration file
+     */
     String KEY_CONFIG_FILE_NAME = PREFIX_EXTERNAL + ".config-file-name";
 
+    /**
+     * Prefix for Nacos config data source configurations
+     */
     String PREFIX_NACOS = PREFIX + ".nacos-config";
 
+    /**
+     * Nacos server address
+     */
     String KEY_NACOS_SERVER_ADDR = PREFIX_NACOS + ".server-addr";
 
+    /**
+     * Nacos configuration group id
+     */
     String KEY_NACOS_GROUP_ID = PREFIX_NACOS + ".group-id";
 
+    /**
+     * Nacos configuration data id
+     */
     String KEY_NACOS_DATA_ID = PREFIX_NACOS + ".data-id";
 
+    /**
+     * Format of Nacos configuration content
+     */
     String KEY_NACOS_CONFIG_FORMAT = PREFIX_NACOS + ".config-format";
 
-    String PREFIX_REDIS = "spring.schedule.cron.scheduled-driven.redis";
+    /**
+     * Prefix for Redis data source configurations
+     */
+    String PREFIX_REDIS = PREFIX + ".redis";
 
+    /**
+     * Redis rule storage key
+     */
     String KEY_RULE_KEY = PREFIX_REDIS + ".rule-key";
 
+    /**
+     * Redis publish/subscribe channel name
+     */
     String KEY_CHANNEL = PREFIX_REDIS + ".channel";
 
+    /**
+     * Format of Redis stored configuration content
+     */
     String KEY_CONFIG_FORMAT = PREFIX_REDIS + ".config-format";
 
+    /**
+     * Custom logger name for scheduled driven module
+     */
     String KEY_LOGGER_NAME = PREFIX + ".logger-name";
 
+    /**
+     * Interval for task monitor inspection
+     */
    String KEY_MONITOR_CHECK_INTERNAL = PREFIX + ".monitor-check-internal";
 }
