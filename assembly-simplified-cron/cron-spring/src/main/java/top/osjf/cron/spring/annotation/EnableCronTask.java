6b87d26ad3ef02815697918154ef4a7f22d46e05
zhangpengfei <929160069@qq.com>
2026-07-06 13:30:30 +0800

    commons

--- assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/annotation/EnableCronTask.java
+++ assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/annotation/EnableCronTask.java
@@ -18,12 +18,29 @@
 package top.osjf.cron.spring.annotation;
 
 import org.springframework.context.annotation.Import;
+import top.osjf.cron.core.repository.CronTaskRepository;
 
 import java.lang.annotation.*;
 
 /**
+ * Enables support for scanning {@link Cron} and {@link Crones} annotations
+ * on Spring components to register custom cron task definitions.
+ *
+ * <p>To be used together with {@link CronTaskRepository} for task lifecycle management.
+ * When present on any {@code @Configuration} class, the framework automatically registers
+ * a {@link CronAnnotationPostProcessor} to detect annotated methods and load cron tasks
+ * after the application context has been fully refreshed.
+ *
+ * <p>This annotation imports {@link CronTaskConfiguration} to register the internal
+ * annotation post processor bean infrastructure.
+ *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
  * @since 3.0.2
+ * @see Cron
+ * @see Crones
+ * @see CronTaskConfiguration
+ * @see CronAnnotationPostProcessor
+ * @see CronTaskRepository
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
