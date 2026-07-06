9914eb73f5f9b09d296a9b79c8462aa5c8b4913b
zhangpengfei <929160069@qq.com>
2026-07-06 14:32:17 +0800

    commons

--- assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/datasource/driven/scheduled/SubstituteConfigFormat.java
+++ assembly-simplified-cron/cron-spring/src/main/java/top/osjf/cron/spring/datasource/driven/scheduled/SubstituteConfigFormat.java
@@ -22,7 +22,7 @@ package top.osjf.cron.spring.datasource.driven.scheduled;
  * {@link top.osjf.cron.datasource.driven.scheduled.serialization.ConfigFormat}.
  *
  * @author <a href="mailto:929160069@qq.com">zhangpengfei</a>
- * @since 3.0.1
+ * @since 3.0.2
  */
 public enum SubstituteConfigFormat {
 
